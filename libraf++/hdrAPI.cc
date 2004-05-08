cf472fb36f1be45f4ab4065f302d3450e5ff1f20
Chris Webster <cjw@ucar.edu>
2004-05-08 11:22:02 +0000

    Remove wasinger test stuff.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@161 640d5228-2204-0410-8d71-8f46fa6850b4

--- libraf++/hdrAPI.cc
+++ libraf++/hdrAPI.cc
@@ -67,7 +67,7 @@ void Header::readHeader(const char fileName[])
     {
     if ((fd = open(fileName, O_RDONLY)) == (-1))
       {
-      std::cerr << "Header: can't open " << fileName << " fatal, errno = " << errno << ".\n";
+      cerr << "Header: can't open " << fileName << " fatal, errno = " << errno << ".\n";
       exit(1);
       }
     }
@@ -79,13 +79,13 @@ void Header::readHeader(const char fileName[])
 
     if (drv->Read(buff) < len)
       {
-      std::cerr << "Header: read problem.\n";
+      cerr << "Header: read problem.\n";
       exit(1);
       }
 
     if (strncmp(FIRST_REC_STRING, buff, len) != 0)
       {
-      std::cerr << "Header: not an ADS tape, fatal.\n";
+      cerr << "Header: not an ADS tape, fatal.\n";
       exit(1);
       }
 
@@ -94,71 +94,13 @@ void Header::readHeader(const char fileName[])
     }
   else
     {
-    // Legacy support: header files on tape stored an identifying string at the
-    // beginning of the file.  If it exists then read past it to access the Fl
-    // block.  Otherwise rewind the file pointer.
-    read(fd, buff, 40);
+    read(fd, buff, 20);
+
     if (strncmp(buff, FIRST_REC_STRING, strlen(FIRST_REC_STRING)) != 0)
       lseek(fd, 0, 0);
 
-    if (strncmp(buff, FIRST_XML_STRING, strlen(FIRST_XML_STRING)) != 0)
-      {
-	// Read binary header file into the buffer.
-	nBytes = read(fd, buff, sizeof(Fl));
-	nBytes += read(fd, &buff[sizeof(Fl)], ntohl(header->thdrlen));
-      }
-/*
-    else
-      {
-	// Close the file and re-open as an XML document.
-	close(fd);
-	TiXmlDocument doc( fileName );
-	if (!doc.LoadFile())
-	  {
-	    std::cerr << "Could not load file '" << fileNamee
-		      << "'. Error='"<< doc.ErrorDesc() << "'. fatal.\n";
-	    exit(1);
-	  }
-// 	TiXmlHandle docHandle( & );
-
-// 	TiXmlNode* node = doc.FirstChild( "header" );
-// 	assert
-
-// 	for( child; child; child=child->NextSiblingElement() )
-// 	  {
-// 	    // do something
-// 	  }
-
-
-	// Parse the ascii XML file into the binary buffer.
-	TiXmlNode* node = 0;
-	TiXmlElement* ele = 0;
-	
-	node = doc.FirstChild( "Fl" );
-	assert( node );
-	ele = node->FirstChildElement();
-	assert( node );
-
-        Fl->thdr     = (str8)  asdf("thdr");     // I.D. "THDR"
-        Fl->item_len = (long)  asdf("item_len"); // Length of this struct in bytes
-        Fl->version  = (str8)  asdf("version");  // header version number
-        Fl->prnum    = (str8)  asdf("prnum");    // project number
-        Fl->fltnum   = (str8)  asdf("fltnum");   // flight number
-        Fl->tpnum    = (str8)  asdf("tpnum");    // tape number
-        Fl->date     = (str12) asdf("date");     // "mm/dd/yyyy
-        Fl->time     = (str12) asdf("time");     // "hh:mm:ss"
-        Fl->tzone    = (str4)  asdf("tzone");    // i.e. "MST"
-        Fl->acraft   = (str8)  asdf("acraft");   // i.e. "N308D"
-        Fl->n_items  = (long)  asdf("n_items");  // # of items excluding this one
-        Fl->lrlen    = (long)  asdf("lrlen");    // logical record length
-        Fl->lrppr    = (long)  asdf("lrppr");    // logical records per physical rec
-        Fl->thdrlen  = (long)  asdf("thdrlen");  // length of this tape header
-        Fl->ads_type = (str8)  asdf("ads_type"); // data system type
-	
-	
-
-      }
-*/
+    nBytes = read(fd, buff, sizeof(Fl));
+    nBytes += read(fd, &buff[sizeof(Fl)], ntohl(header->thdrlen));
     close(fd);
     }
 
@@ -171,7 +113,7 @@ void Header::readHeader(const char fileName[])
    */
   if (strcmp(TAPEHDR_STR, header->thdr) != 0)
     {
-    std::cerr << "Header: Bad header, fatal [" << header->thdr << "].\n";
+    cerr << "Header: Bad header, fatal [" << header->thdr << "].\n";
     exit(1);
     }
 
@@ -187,19 +129,19 @@ Header::Header(TapeDrive &drv)
 
   if ((nBytes = drv.Read(buff)) < len)
     {
-    std::cerr << stderr, "Header: read problem.\n";
+    cerr << stderr, "Header: read problem.\n";
     exit(1);
     }
 
   if (strncmp(FIRST_REC_STRING, buff, len) !=0)
     {
-    std::cerr << "Header: not an ADS tape, fatal.\n";
+    cerr << "Header: not an ADS tape, fatal.\n";
     exit(1);
     }
 
   if ((nBytes = drv.Read(buff)) == 0)
     {
-    std::cerr << "Header: empty tape.\n";
+    cerr << "Header: empty tape.\n";
     exit(1);
     }
 
@@ -211,7 +153,7 @@ Header::Header(TapeDrive &drv)
    */
   if (strcmp(TAPEHDR_STR, header->thdr) != 0)
     {
-    std::cerr << "Header: Bad header, fatal.\n";
+    cerr << "Header: Bad header, fatal.\n";
     exit(1);
     }
 
