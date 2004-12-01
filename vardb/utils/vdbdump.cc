87bd23e2caa1feeadecba7a92f54f18c26dfe50c
Janet Scannell <anstett@ucar.edu>
2004-12-01 21:18:32 +0000

    Modify vdbdump program to add option for generating output for ACtracker use. Change to a .cc program and add Makefile for vdbdump program.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@904 640d5228-2204-0410-8d71-8f46fa6850b4

--- vardb/utils/vdbdump.cc
+++ vardb/utils/vdbdump.cc
@@ -22,59 +22,73 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1993
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <netinet/in.h> // htonl macros.
 
 #include "vardb.h"
 
 extern long	VarDB_nRecords;
+bool tracker = false;
 
 /* -------------------------------------------------------------------- */
 main(int argc, char *argv[])
 {
-  int		i;
+  int		i = 1;
 
-  if (argc != 2)
+  if (argc < 2)
     {
-    fprintf(stderr, "Usage: vdbdump [proj_num | VarDB_filename]\n");
-    exit(1);
+    fprintf(stderr, "Usage: vdbdump [-a] [proj_num | VarDB_filename]\n");
+    return(1);
     }
+  if (strcmp(argv[i], "-a") == 0) {
+     tracker = true;
+     i++;
+  }
 
-  if (InitializeVarDB(argv[1]) == ERR)
+  if (InitializeVarDB(argv[i]) == ERR)
     {
     fprintf(stderr, "vdbdump: Initialize failure.\n");
-    exit(1);
+    return(1);
     }
 
 
   printf("Version %d, with %d records.\n", ntohl(VarDB_Hdr.Version),
 						VarDB_nRecords);
 
-  for (i = 0; i < VarDB_nRecords; ++i)
-    {
-    printf("%-12.12s %-6.6s %-50.50s",	((struct var_v2 *)VarDB)[i].Name,
-	((struct var_v2 *)VarDB)[i].Units, ((struct var_v2 *)VarDB)[i].Title);
+  for (i = 0; i < VarDB_nRecords; ++i) {
+     if (tracker) {
+        printf("%-12.12s%-8.8s%-60.60s", ((struct var_v2 *)VarDB)[i].Name,
+               ((struct var_v2 *)VarDB)[i].Units, ((struct var_v2 *)VarDB)[i].Title);
+     }
+     else {
+        printf("%-12.12s %-6.6s %-50.50s", ((struct var_v2 *)VarDB)[i].Name,
+               ((struct var_v2 *)VarDB)[i].Units, ((struct var_v2 *)VarDB)[i].Title);
+     }
 
-    if (ntohl(((struct var_v2 *)VarDB)[i].type) == FLOATING)
-      {
+    if (ntohl(((struct var_v2 *)VarDB)[i].type) == FLOATING) {
       ulong	p;
       float	*rc = (float *)&p;
 
       p = ntohl(*((int *)&(((struct var_v2 *)VarDB)[i].FloatRange)));
-
-      printf("%6.1f\n", *rc);
+      if (!tracker) {
+         printf("%6.1f\n", *rc);
       }
-    else
-      {
+    }
+    else {
       ulong	p0, p1;
       float	*rc0 = (float *)&p0, *rc1 = (float *)&p1;
 
       p0 = ntohl(*((int *)&(((struct var_v2 *)VarDB)[i].FixedRange[0])));
       p1 = ntohl(*((int *)&(((struct var_v2 *)VarDB)[i].FixedRange[1])));
-
-      printf("%6.1f %6.1f\n", *rc0, *rc1);
+      if (!tracker) {
+         printf("%6.1f %6.1f\n", *rc0, *rc1);
       }
     }
+    if (tracker) {
+       printf("\n");
+    }
+  }
 
   ReleaseVarDB();
 
