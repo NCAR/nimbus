0c692dd4c6bdd9a975b9de6a8b0955e5b04904d4
Chris Webster <cjw@ucar.edu>
2005-04-29 23:47:54 +0000

    Fix ATWH which was clandestinely back filling TTWH.  Add intermediary variable TTWHC. Fix modulo un-ravel in phase_shift.c. Fix recent new netCDF attrs, TimeLag & DespikeSlope, shouldn't be added if despiking/phase-shift are turned off.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@1916 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/atfh.c
+++ src/amlib/std/atfh.c
@@ -17,6 +17,8 @@
 extern NR_TYPE	recfrh;
 extern NR_TYPE	tfher1,tfher2;	/* set in initAC.c */
 
+static NR_TYPE atfh[nFeedBackTypes] = { 0.0, 0.0 };
+
 /* -------------------------------------------------------------------- */
 void atfhInit(DERTBL *varp)
 {
@@ -24,45 +26,55 @@ void atfhInit(DERTBL *varp)
 }	/* END CONSTRUCTOR */
 
 /* -------------------------------------------------------------------- */
+void sttwhc(DERTBL *varp)
+{
+  NR_TYPE zee, tth, xmach2, psxc;
+  static bool firstTime[nFeedBackTypes] = { true, true };
+
+  tth = GetSample(varp, 0);
+  xmach2 = GetSample(varp, 1);
+  psxc = GetSample(varp, 2);
+
+  if (firstTime[FeedBack])
+  {
+    firstTime[FeedBack] = false;
+    atfh[FeedBack] = tth;
+  }
+
+  if (tth < -273.15)
+    tth = -273.15;
+
+  if (xmach2 < 0.0)
+  {
+    LogStdMsg("atfh.c: xmach2 went negative!\n");
+    xmach2 = 0.0;
+  }
+
+  zee = 0.269589 * psxc * sqrt((double)xmach2) / (atfh[FeedBack] + 273.16);
+
+  if (zee < 0.1)
+    zee = 0.99;
+
+  tth -= (NR_TYPE)pow(	(double)10.0,
+			(double)tfher1 * log10((double)zee) + tfher2);
+
+  if (tth < -273.15)
+    tth = -273.15;
+
+  PutSample(varp, tth);
+}
+
+/* -------------------------------------------------------------------- */
 void satfh(DERTBL *varp)
 {
-	NR_TYPE		ttfh, zee, xmach2, psxc;
-	static NR_TYPE	atfh[nFeedBackTypes];
-	static bool	firstTime[nFeedBackTypes] = { TRUE, TRUE };
+  NR_TYPE ttfh, xmach2;
 
-	ttfh	= GetSample(varp, 0);
-	xmach2	= GetSample(varp, 1);
-	psxc	= GetSample(varp, 2);
+  ttfh = GetSample(varp, 0);
+  xmach2 = GetSample(varp, 1);
 
-	if (firstTime[FeedBack])
-		{
-		firstTime[FeedBack] = FALSE;
-		atfh[FeedBack] = ttfh;
-		}
+  atfh[FeedBack] = AMBIENT(ttfh, (NR_TYPE)recfrh, xmach2);
 
-	if (ttfh < -273.15)
-		ttfh = -273.15;
-
-	zee = 0.269589 * psxc * sqrt((double)xmach2) / (atfh[FeedBack] +273.16);
-
-	if (zee < 0.1)
-		zee = 0.99;
-
-	ttfh -= (NR_TYPE)pow(	(double)10.0,
-				(double)tfher1 * log10((double)zee) + tfher2);
-
-	if (ttfh < -273.15)
-		ttfh = -273.15;
-
-	atfh[FeedBack] = AMBIENT(ttfh, (NR_TYPE)recfrh, xmach2);
-
-
-	if (FeedBack == LOW_RATE_FEEDBACK)
-		AveragedData[varp->depend_LRindex[0]] = ttfh;
-	else
-		HighRateData[varp->depend_HRindex[0]+SampleOffset] = ttfh;
-
-	PutSample(varp, atfh[FeedBack]);
+  PutSample(varp, atfh[FeedBack]);
 
 }	/* END SATFH */
 
