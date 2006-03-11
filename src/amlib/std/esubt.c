1cdcba000b9264b2c83fc1627f8973b13fd2b82d
Chris Webster <cjw@ucar.edu>
2006-03-11 19:09:15 +0000

    Allow Total Temp recovery factors, hardwired, to be overridden in proj/###/Defaults file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@3244 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/esubt.c
+++ src/amlib/std/esubt.c
@@ -31,41 +31,39 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1992
 
 static const double	Ts = 373.16, To = 273.16;
 
-double esubt(double, double);
+/* -------------------------------------------------------------------- */
+double esubt(double temperature, double pressure)
+{
+  double Tk = temperature + To;
+  double fw, ew;
+
+  if (Tk < 1.0)
+    Tk = 1.0;
+
+  ew = pow(10.0, (
+	-7.90298 * ((Ts / Tk) - 1.0)
+	+5.02808 * log10(Ts / Tk)
+	-1.3816e-7 * (pow(10.0, 11.334 * (1.0 - Tk / Ts)) - 1.0)
+	+8.1328e-3 * (pow(10.0, -3.49149 * (Ts / Tk - 1.0)) - 1.0))
+	+log10(1013.246) );
+
+  /* Arden Buck's pressure enhancement factor.
+  */
+  fw = 1.0007 + (3.46e-6 * pressure);
+
+  return(ew * fw);
+
+}	/* END ESUBT */
 
 /* -------------------------------------------------------------------- */
 void sedpc(DERTBL *varp)
 {
-	NR_TYPE	dpxc, psxc;
+  NR_TYPE	dpxc, psxc;
 
-	dpxc = GetSample(varp, 0);
-	psxc = GetSample(varp, 1);
+  dpxc = GetSample(varp, 0);
+  psxc = GetSample(varp, 1);
  
-	PutSample(varp, (NR_TYPE)esubt(dpxc, psxc));
+  PutSample(varp, (NR_TYPE)esubt(dpxc, psxc));
 }
-
-/* -------------------------------------------------------------------- */
-double esubt(double temperature, double pressure)
-{
-	double	Tk = temperature + To;
-	double	fw, ew;
-
-	if (Tk < 1.0)
-		Tk = 1.0;
-
-	ew = pow(10.0, (
-		-7.90298 * ((Ts / Tk) - 1.0)
-		+5.02808 * log10(Ts / Tk)
-		-1.3816e-7 * (pow(10.0, 11.334 * (1.0 - Tk / Ts)) - 1.0)
-		+8.1328e-3 * (pow(10.0, -3.49149 * (Ts / Tk - 1.0)) - 1.0))
-		+log10(1013.246) );
-
-	/* Arden Buck's pressure enhancement factor.
-	 */
-	fw = 1.0007 + (3.46e-6 * pressure);
-
-	return(ew * fw);
-
-}	/* END ESUBT */
 
 /* END ESUBT.C */
