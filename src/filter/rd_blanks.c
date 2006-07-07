4ca07fc2d30538067fd75f5972d43db0e3cf14b6
Chris Webster <cjw@ucar.edu>
2006-07-07 17:11:45 +0000

    Allow blanking all variables with a single entry in the blanks out file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@3348 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/filter/rd_blanks.c
+++ src/filter/rd_blanks.c
@@ -19,6 +19,7 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2006
 #include "nimbus.h"
 #include "decode.h"
 
+static void clear_sacred_variables();
 
 /* -------------------------------------------------------------------- */
 void ReadBlankOuts()
@@ -50,15 +51,57 @@ void ReadBlankOuts()
 
     std::pair<int, int> tm(sTime[3], eTime[3]);
 
-    if ((index = SearchTable(raw, target)) != ERR)
-      raw[index]->blank_out.push_back(tm);
+    if (strcmp(target, "ALL") == 0)
+    {
+      printf("Blanking out all raw variables for %d %d\n", sTime[3], eTime[3]);
+      for (size_t j = 0; j < raw.size(); ++j)
+        raw[j]->blank_out.push_back(tm);
 
-    if ((index = SearchTable(derived, target)) != ERR)
-      derived[index]->blank_out.push_back(tm);
+//      for (size_t j = 0; j < derived.size(); ++j)
+//        derived[j]->blank_out.push_back(tm);
+    }
+    else
+    {
+      if ((index = SearchTable(raw, target)) != ERR)
+        raw[index]->blank_out.push_back(tm);
+
+      if ((index = SearchTable(derived, target)) != ERR)
+        derived[index]->blank_out.push_back(tm);
+    }
   }
 
   FreeTextFile(bo);
 
+  clear_sacred_variables();
+
 }	/* END READBLANKOUTS */
+
+/* -------------------------------------------------------------------- */
+static void check_var(var_base *var)
+{
+  if (strcmp(var->name, "YEAR") == 0 ||
+      strcmp(var->name, "MONTH") == 0 ||
+      strcmp(var->name, "DAY") == 0 ||
+      strcmp(var->name, "HOUR") == 0 ||
+      strcmp(var->name, "MINUTE") == 0 ||
+      strcmp(var->name, "ONE") == 0 ||
+      strcmp(var->name, "ZERO") == 0 ||
+      strcmp(var->name, "SECOND") == 0)
+  {
+    char msg[128];
+    sprintf(msg, "Blankout can not apply to variable %s, clearing.\n", var->name);
+    LogMessage(msg);
+    var->blank_out.clear();
+  }
+}
+
+static void clear_sacred_variables()
+{
+  for (size_t j = 0; j < raw.size(); ++j)
+    check_var(raw[j]);
+
+  for (size_t j = 0; j < derived.size(); ++j)
+    check_var(derived[j]);
+}
 
 /* END RD_BLANKS.C */
