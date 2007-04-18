e8e7f0de048dfb14d0d83f6b18602f89f8bda8c9
Chris Webster <cjw@ucar.edu>
2007-04-18 12:24:27 +0000

    NOAA AOC updates to this file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@3609 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/xlate/gpsPps.c
+++ src/amlib/xlate/gpsPps.c
@@ -5,7 +5,8 @@ OBJECT NAME:	gpsPps.c
 FULL NAME:	Convert Collins PPS GPS block data
 
 ENTRY POINTS:	xlgptime(), xlgplat(), xlgplon(), xlgpalt(), xlgpvns(),
-		xlgpvew(), xlgpvspd()
+		xlgpvew(), xlgpvspd(), xlgpherr(), xlgpverr(),
+		xlgpsat1(), xlgpsat2(), xlgpsat3(), xlgpsat4(),
 
 DESCRIPTION:	Collins GPS data is stored in DEC floating point.
 
@@ -28,50 +29,94 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1996
 /* -------------------------------------------------------------------- */
 void xlgptime(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-  *np = ((Ppsgps_blk *)p)->utc_time;
+  *np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->utc_time);
 
 }	/* END XLGPTIME */
 
 /* -------------------------------------------------------------------- */
 void xlgplat(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->lat / 4.0 * RAD_DEG;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->lat) / 4.0 * RAD_DEG;
 
 }	/* END XLGPLAT */
 
 /* -------------------------------------------------------------------- */
 void xlgplon(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->lon / 4.0 * RAD_DEG;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->lon) / 4.0 * RAD_DEG;
 
 }	/* END XLGPLON */
 
 /* -------------------------------------------------------------------- */
 void xlgpalt(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->alt_msl / 4.0;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->alt_msl) / 4.0;
 
 }	/* END XLGPALT */
 
 /* -------------------------------------------------------------------- */
 void xlgpvns(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->vel_north / 4.0;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->vel_north) / 4.0;
 
 }	/* END XLGPVNS */
 
 /* -------------------------------------------------------------------- */
 void xlgpvew(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->vel_east / 4.0;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->vel_east) / 4.0;
 
 }	/* END XLGPVEW */
 
 /* -------------------------------------------------------------------- */
 void xlgpvspd(RAWTBL *varp, void *p, NR_TYPE *np)
 {
-	*np = ((Ppsgps_blk *)p)->vel_up / 4.0;
+	*np = (NR_TYPE)ntohf(((Ppsgps_blk *)p)->vel_up) / 4.0;
 
 }	/* END XLGPVSPD */
+
+
+/* -------------------------------------------------------------------- */
+void xlgpherr(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)((Ppsgps_blk *)p)->err_horiz;
+
+}	/* END XLGPHERR */
+
+/* -------------------------------------------------------------------- */
+void xlgpverr(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)((Ppsgps_blk *)p)->err_vert;
+
+}	/* END XLGPVERR */
+
+/* -------------------------------------------------------------------- */
+void xlgpsat1(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)( ((Ppsgps_blk *)p)->ch1_stat1 & 0x1F );
+
+}	/* END XLGPSAT1 */
+
+/* -------------------------------------------------------------------- */
+void xlgpsat2(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)( ((Ppsgps_blk *)p)->ch2_stat1 & 0x1F );
+
+}	/* END XLGPSAT2 */
+
+/* -------------------------------------------------------------------- */
+void xlgpsat3(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)( ((Ppsgps_blk *)p)->ch3_stat1 & 0x1F );
+
+}	/* END XLGPSAT3 */
+
+/* -------------------------------------------------------------------- */
+void xlgpsat4(RAWTBL *varp, void *p, NR_TYPE *np)
+{
+	*np = (NR_TYPE)( ((Ppsgps_blk *)p)->ch4_stat1 & 0x1F );
+
+}	/* END XLGPSAT4 */
+
 
 /* END GPSPPS.C */
