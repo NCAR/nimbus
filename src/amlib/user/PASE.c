104b5baab530d9b94eac689ed772949861cc915a
Chris Webster <cjw@ucar.edu>
2007-08-06 03:06:29 +0000

    Hawaii flow correction calculation.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@3740 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/user/PASE.c
+++ src/amlib/user/PASE.c
@@ -17,6 +17,8 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2007
 #include "nimbus.h"
 #include "amlib.h"
 
+static NR_TYPE d[3] = { 3.17, 6.17, 5.16 };
+
 /* -------------------------------------------------------------------- */
 void sScatter(DERTBL *varp)
 {
@@ -27,5 +29,21 @@ void sScatter(DERTBL *varp)
   PutSample(varp, pow(10.0, Vdc / 2.0));
 
 }  /* END SFUNC */
+
+
+/* -------------------------------------------------------------------- */
+void ssteve(DERTBL *varp)
+{
+  NR_TYPE slpm, atx, psx, tasx, out;
+
+  slpm = GetSample(varp, 0);
+  psx = GetSample(varp, 1);
+  atx = GetSample(varp, 2);
+  tasx = GetSample(varp, 3);
+
+  out = slpm * (1013.25 / psx) * ((atx+273.15) / 298) * 66.6666 / (M_PI * d[0] * d[0] * tasx);
+
+  PutSample(varp, out);
+}
 
 /* END PASE.C */
