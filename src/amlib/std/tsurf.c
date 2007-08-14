5384548ba02295da6f8a49d05d8ac1d1bd565305
Chris Webster <cjw@ucar.edu>
2007-08-14 00:47:20 +0000

    New Corrected Sea Surface Temperature.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@3752 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ src/amlib/std/tsurf.c
@@ -0,0 +1,30 @@
+#include "nimbus.h"
+#include "amlib.h"
+
+static NR_TYPE TSURF_CAL = 0.001933;
+
+/* -------------------------------------------------------------------- */
+void initTsurf(var_base *varp)
+{
+  NR_TYPE  *tmp;
+  if ((tmp = GetDefaultsValue("TSURF_CAL", varp->name)) == NULL)
+  {
+    sprintf(buffer, "Value set to %f in AMLIB function initPsurf.\n", TSURF_CAL);
+    LogMessage(buffer);
+  }
+  else
+    TSURF_CAL = tmp[0];
+
+}
+
+/* -------------------------------------------------------------------- */
+void stsurf(DERTBL *varp)
+{
+  NR_TYPE  rstb, galt;
+
+  rstb  = GetSample(varp, 0);
+  galt  = GetSample(varp, 1);
+
+  PutSample(varp, rstb + (TSURF_CAL * galt));
+
+}
