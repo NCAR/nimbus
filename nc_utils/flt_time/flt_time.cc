2e9b52c59051fac74f64ab63f269c43f0921a621
Chris Webster <cjw@ucar.edu>
2007-09-30 17:30:24 +0000

    Utility program to print out take-off and landing time of a flight from a netCDF file.  Default criteria is true airspeed above 25 m/s.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@3769 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ nc_utils/flt_time/flt_time.cc
@@ -0,0 +1,136 @@
+#include <cstdlib>
+#include <ctime>
+
+#include <netcdfcpp.h>
+
+
+NcValues *
+getData(NcFile * file, const char * name)
+{
+  NcVar * var = file->get_var(name);
+
+  if (var == 0 || var->is_valid() == false)
+  {
+    std::cerr << "No variable " << name << "?" << std::endl;
+    return 0;
+  }
+
+  NcValues * data = var->values();
+
+  if (data == 0)
+  {
+    std::cerr << "No data for variable " << name << "?" << std::endl;
+    return 0;
+  }
+
+  return data;
+}
+
+
+time_t
+getStartTime(NcVar * time_var)
+{
+  NcAtt * unit_att = time_var->get_att("units");
+  NcAtt * frmt_att = time_var->get_att("strptime_format");
+
+  if (unit_att == 0 || unit_att->is_valid() == false)
+  {
+    std::cerr << "Units not present for time variable.\n";
+    return 0;
+  }
+
+  char format[1000];
+  if (frmt_att == 0 || frmt_att->is_valid() == false)
+    strcpy(format, "seconds since %F %T %z");
+  else
+    strcpy(format, frmt_att->as_string(0));
+
+  struct tm StartFlight;
+  strptime(unit_att->as_string(0), format, &StartFlight);
+
+  time_t start_t = mktime(&StartFlight);
+  if (start_t <= 0)
+    std::cerr << "Problem decoding units from Time variable, _start = " << start_t << std::endl;
+
+  return start_t;
+}
+
+
+int
+main(int argc, char *argv[])
+{
+  int indx = 1;
+  float tas_cutoff = 25.0;	// Default.
+
+
+  // Check arguments / usage.
+  if (argc < 2)
+  {
+    std::cerr << "Print out take-off and landing times from a netCDF file.\nUses true airspeed above 25 m/s (use -t to change).\n\n";
+    std::cerr << "Usage: flt_time [-t value] netcdf_file\n";
+    exit(1);
+  }
+
+  if (strcmp(argv[indx], "-t") == 0)
+  {
+    ++indx;
+    tas_cutoff = atof(argv[indx++]);
+  }
+
+  // keep program from exiting, if netCDF API doesn't find something.
+  NcError * ncErr = new NcError(NcError::silent_nonfatal);
+
+  // Open NetCDF file
+  NcFile * ncFile = new NcFile(argv[indx], NcFile::ReadOnly);
+  if (ncFile->is_valid() == false)
+  {
+    std::cerr << "Could not open NetCDF file '" << argv[indx] << "' for reading.\n";
+    return 1;
+  }
+  
+  NcAtt * flight = ncFile->get_att("FlightNumber");
+  std::cout << argv[indx] << ":" << flight->as_string(0) << ":\n";
+
+  NcValues * time_data = getData(ncFile, "Time");
+  NcValues * tas_data = getData(ncFile, "TASX");
+
+  if (time_data == 0 || tas_data == 0)
+    return 1;
+
+  // Get flight start time.
+  NcVar * time_var = ncFile->get_var("Time");
+  time_t start_t = getStartTime(time_var);
+
+  if (start_t <= 0)
+  {
+    std::cerr << "Invalid start time, fatal.\n";
+    return 1;
+  }
+
+
+  // Locate Start of Flight
+  long i;
+  for (i = 0; i < time_var->num_vals(); ++i)
+    if (tas_data->as_float(i) > tas_cutoff)
+    {
+      time_t x = start_t + i;
+      std::cout << "   " << ctime(&x);
+//std::cout << "take-off indx = " << x << " " << i << std::endl;
+      break;
+    }
+
+  // Locate End of Flight
+  for (; i < time_var->num_vals(); ++i)
+    if (tas_data->as_float(i) < tas_cutoff && tas_data->as_float(i) != -32767.0)
+    {
+      time_t x = start_t + i;
+
+      std::cout << "   " << ctime(&x);
+//std::cout << "landing indx = " << x << " " << i << std::endl;
+      break;
+    }
+
+  delete time_data;
+  delete tas_data;
+  delete ncFile;
+}
