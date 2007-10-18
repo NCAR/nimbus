351f485901efa577f8ac113853704a67ebcb0fd3
John Wasinger <wasinger@ucar.edu>
2007-10-18 21:20:47 +0000

    There was a path change to the javac.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@3787 640d5228-2204-0410-8d71-8f46fa6850b4

--- groundfeed/src/build.sh
+++ groundfeed/src/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/java/jdk/bin/javac -classpath ../lib/getopt.jar Groundfeed/*java
+/usr/java/latest/bin/javac -classpath ../lib/getopt.jar Groundfeed/*java
 jar cvf ../lib/groundfeed.jar Groundfeed/*class
