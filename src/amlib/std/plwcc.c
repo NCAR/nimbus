d29d84822d4634884d49a2f5f410bf59f9bb6dc8
Chris Webster <cjw@ucar.edu>
2008-12-31 21:00:11 +0000

    Add running average smoothing.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@4308 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/plwcc.c
+++ src/amlib/std/plwcc.c
@@ -26,26 +26,26 @@ void plwccInit(var_base *varp)
   NR_TYPE	*tmp;
 
   if ((tmp = GetDefaultsValue("TWIRE_PMS", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twire[0]);
     LogMessage(buffer);
-    }
+  }
   else
     twire[0] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_TASFAC", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", tasFac[0]);
     LogMessage(buffer);
-    }
+  }
   else
     tasFac[0] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_DIAM", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twireDiam[0]);
     LogMessage(buffer);
-    }
+  }
   else
     twireDiam[0] = tmp[0];
 
@@ -57,26 +57,26 @@ void plwcc1Init(var_base *varp)
   NR_TYPE	*tmp;
 
   if ((tmp = GetDefaultsValue("TWIRE_PMS1", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twire[1]);
     LogMessage(buffer);
-    }
+  }
   else
     twire[1] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_TASFAC1", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", tasFac[1]);
     LogMessage(buffer);
-    }
+  }
   else
     tasFac[1] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_DIAM1", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twireDiam[1]);
     LogMessage(buffer);
-    }
+  }
   else
     twireDiam[1] = tmp[0];
 
@@ -88,26 +88,26 @@ void plwcc2Init(var_base *varp)
   NR_TYPE	*tmp;
 
   if ((tmp = GetDefaultsValue("TWIRE_PMS2", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twire[2]);
     LogMessage(buffer);
-    }
+  }
   else
     twire[2] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_TASFAC2", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", tasFac[2]);
     LogMessage(buffer);
-    }
+  }
   else
     tasFac[2] = tmp[0];
 
   if ((tmp = GetDefaultsValue("TWIRE_DIAM2", varp->name)) == NULL)
-    {
+  {
     sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twireDiam[2]);
     LogMessage(buffer);
-    }
+  }
   else
     twireDiam[2] = tmp[0];
 
@@ -116,78 +116,128 @@ void plwcc2Init(var_base *varp)
 /* -------------------------------------------------------------------- */
 void splwcc(DERTBL *varp)
 {
-  NR_TYPE  plwc, tasx, atx, psxc;
+  NR_TYPE plwc, tasx, atx, psxc, concf;
+  NR_TYPE prev_lwc, plwcc;
+
+  static const int nSeconds = 5;
+  static int buffIndex = 0;
+  static NR_TYPE lwc_buffer[nSeconds], lwc_sum;
 
   plwc  = GetSample(varp, 0);
   tasx  = GetSample(varp, 1);
   atx   = GetSample(varp, 2);
   psxc  = GetSample(varp, 3);
  
-  /* Blow-up protection:  output zero while on ground (TASX < 30.0 m/s)
-       installed by Ron Ruth  18 October 2001 */
-
   if (tasx < 30.0)
-    {
-    plwc = 0.0;
-    PutSample(varp, plwc);
-    }
-  else
-    {
-    tasx *= tasFac[0];
-    PutSample(varp, kinglwcc(plwc, tasx, atx, psxc, twire[0], twireDiam[0]));
-    }
+  {
+    PutSample(varp, 0.0);
+    return;
+  }
 
-}  /* END SPLWCC */
+  tasx *= tasFac[0];
+  plwcc = kinglwcc(plwc, tasx, atx, psxc, twire[0], twireDiam[0]);
+
+  if (varp->ndep >= 6)
+  {
+    concf = GetSample(varp, 4);
+
+    if (concf < 0.25)
+    {
+      if (++buffIndex >= nSeconds)
+        buffIndex = 0;
+  
+      prev_lwc = lwc_buffer[buffIndex];
+      lwc_buffer[buffIndex] = -plwcc;
+      lwc_sum -= prev_lwc;
+      lwc_sum += -plwcc;
+    }
+  }
+
+  PutSample(varp, plwcc + lwc_sum / nSeconds);
+}
 
 /* -------------------------------------------------------------------- */
 void splwcc1(DERTBL *varp)
 {
-  NR_TYPE  plwc, tasx, atx, psxc;
+  NR_TYPE  plwc, tasx, atx, psxc, concf;
+  NR_TYPE prev_lwc, plwcc;
+
+  static const int nSeconds = 10;
+  static int buffIndex = 0;
+  static NR_TYPE lwc_buffer[nSeconds], lwc_sum;
 
   plwc  = GetSample(varp, 0);
   tasx  = GetSample(varp, 1);
   atx   = GetSample(varp, 2);
   psxc  = GetSample(varp, 3);
  
-  /* Blow-up protection:  output zero while on ground (TASX < 30.0 m/s)
-       installed by Ron Ruth  18 October 2001 */
-
   if (tasx < 30.0)
-    {
-    plwc = 0.0;
-    PutSample(varp, plwc);
-    }
-  else
-    {
-    tasx *= tasFac[1];
-    PutSample(varp, kinglwcc(plwc, tasx, atx, psxc, twire[1], twireDiam[1]));
-    }
+  {
+    PutSample(varp, 0.0);
+    return;
+  }
+  
+  tasx *= tasFac[1];
+  plwcc = kinglwcc(plwc, tasx, atx, psxc, twire[1], twireDiam[1]);
 
-}  /* END SPLWCC1 */
+  if (varp->ndep >= 6)
+  { 
+    concf = GetSample(varp, 4);
+    
+    if (concf < 0.25)
+    {
+      if (++buffIndex >= nSeconds)
+        buffIndex = 0;
+
+      prev_lwc = lwc_buffer[buffIndex];
+      lwc_buffer[buffIndex] = -plwcc;
+      lwc_sum -= prev_lwc;
+      lwc_sum += -plwcc;
+    }
+  }
+  
+  PutSample(varp, plwcc + lwc_sum / nSeconds);
+}
+
 /* -------------------------------------------------------------------- */
 void splwcc2(DERTBL *varp)
 {
-  NR_TYPE  plwc, tasx, atx, psxc;
+  NR_TYPE  plwc, tasx, atx, psxc, concf;
+  NR_TYPE prev_lwc, plwcc;
+
+  static const int nSeconds = 10;
+  static int buffIndex = 0;
+  static NR_TYPE lwc_buffer[nSeconds], lwc_sum;
 
   plwc  = GetSample(varp, 0);
   tasx  = GetSample(varp, 1);
   atx   = GetSample(varp, 2);
   psxc  = GetSample(varp, 3);
  
-  /* Blow-up protection:  output zero while on ground (TASX < 30.0 m/s)
-       installed by Ron Ruth  18 October 2001 */
-
   if (tasx < 30.0)
-    {
-    plwc = 0.0;
-    PutSample(varp, plwc);
-    }
-  else
-    {
-    tasx *= tasFac[2];
-    PutSample(varp, kinglwcc(plwc, tasx, atx, psxc, twire[2], twireDiam[2]));
-    }
+  {
+    PutSample(varp, 0.0);
+    return;
+  }
+  
+  tasx *= tasFac[2];
+  plwcc = kinglwcc(plwc, tasx, atx, psxc, twire[2], twireDiam[2]);
 
-}  /* END SPLWCC1 */
+  if (varp->ndep == 5)
+  { 
+    concf = GetSample(varp, 4);
+    
+    if (concf < 0.25)
+    {
+      if (++buffIndex >= nSeconds)
+        buffIndex = 0;
 
-/* END PLWCC.C */
+      prev_lwc = lwc_buffer[buffIndex];
+      lwc_buffer[buffIndex] = -plwcc;
+      lwc_sum -= prev_lwc;
+      lwc_sum += -plwcc;
+    }
+  }
+  
+  PutSample(varp, plwcc + lwc_sum / nSeconds);
+}
