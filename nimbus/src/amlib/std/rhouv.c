1cfe1344f4313343b5c691b1b08f749c583d0c4b
Chris Webster <cjw@ucar.edu>
2008-12-31 21:00:11 +0000

    Add running average smoothing.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4308 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/rhouv.c
+++ nimbus/src/amlib/std/rhouv.c
@@ -17,7 +17,7 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2007
 #include "nimbus.h"
 #include "amlib.h"
 
-static NR_TYPE cx_1[4] = { 0.28, -5.9761, -6.0135, 0. };
+static NR_TYPE cx_1[4] = { 0.5, 0.0, 0.0, 0.0 };
 static NR_TYPE  *CX_1;
 
 /* -------------------------------------------------------------------- */
@@ -39,17 +39,37 @@ void rhouvInit(var_base *varp)
 void srhouv(DERTBL *varp)
 {
   NR_TYPE xuvi = GetSample(varp, 0);
-  NR_TYPE rhouv;
+  NR_TYPE rhodt = GetSample(varp, 1);
+  NR_TYPE dpxc = GetSample(varp, 2);
+  NR_TYPE atx = GetSample(varp, 3);
+  NR_TYPE rhouv, prev_uv, diff;
 
-  xuvi = xuvi + CX_1[0];
-  if (xuvi <= 0.0)
-    xuvi = 0.001;
+  static const int nSeconds = 10;
+  static int buffIndex = 0;
+  static NR_TYPE uv_buffer[nSeconds], uv_sum;
 
-   rhouv = (CX_1[1] + CX_1[2]*log(xuvi));
+  rhouv = xuvi;
+  diff = (rhodt - xuvi);
+  if (diff < -0.5)
+    diff = -0.5;
+  if (diff > 0.5)
+    diff = 0.25;
 
-   if (rhouv < 0.01)
-     rhouv = 0.01;
+  if (dpxc < atx)
+  {
+    if (++buffIndex >= nSeconds)
+      buffIndex = 0;
+
+    prev_uv = uv_buffer[buffIndex];
+    uv_buffer[buffIndex] = diff;
+    uv_sum -= prev_uv;
+    uv_sum += diff;
+  }
+
+  rhouv = rhouv +  CX_1[0] * (uv_sum / nSeconds);
+
+  if (rhouv < 0.01)
+    rhouv = 0.01;
 
   PutSample(varp, rhouv);
-
 }
