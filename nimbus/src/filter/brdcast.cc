0e485b5a003378c2290a0a2c2d71df9e4f41b68c
Chris Webster <cjw@ucar.edu>
2009-01-07 14:55:35 +0000

    Split onboard UDP broadcast and ground feed UDP into separate classes.  Add a base class UDP_Base also.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4315 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/filter/brdcast.cc
+++ nimbus/src/filter/brdcast.cc
@@ -4,135 +4,44 @@ OBJECT NAME:	brdcst.cc
 
 FULL NAME:	UDP broadcast.
 
-ENTRY POINTS:	
-
 DESCRIPTION:	Class to produce 1 per second ascii string to broadcast
 		around airplane.  At this time it produces the IWGADTS
 		string.
-
-INPUT:		${PROJ_DIR}/###/asciilist, groundvars
-
-OUTPUT:		[rp|dp]->Broadcast updated.
 
 COPYRIGHT:	University Corporation for Atmospheric Research, 2005-08
 -------------------------------------------------------------------------
 */
 
+#include "nimbus.h"
+#include "decode.h"
 #include "brdcast.h"
 
-#include <sstream>
-#include <bzlib.h>
+#include "UdpSocket.h"
 
-const int Broadcast::RT_UDP_PORT = 31000;
+#include <sstream>
+
 //const std::string Broadcast::RT_UDP_ADDR = "128.117.84.255";
 const std::string Broadcast::RT_UDP_ADDR_1 = "192.168.84.255";
 const std::string Broadcast::RT_UDP_ADDR_2 = "192.168.184.255";
 
-// eol-rt-data.guest.ucar.edu
-const int Broadcast::GRND_UDP_PORT = 31007;
-const std::string Broadcast::GRND_UDP_ADDR = "128.117.188.122";
-
 const size_t Broadcast::RADAR_ALT_INDX = 5;
 
-const size_t Broadcast::GroundFeedDataRate = 5;	// in seconds.
 
 /* -------------------------------------------------------------------- */
-Broadcast::Broadcast()
+Broadcast::Broadcast() : UDP_Base(31000)
 {
   _varList = readFile(BROADCAST);
-  std::string fileName(XMIT_VARS);
-  fileName += ".rt";
-  _groundVarList = readFile(fileName);
 
-  setCoordinatesFrom(_groundVarList);
-
-  _brdcst1 = new UdpSocket(RT_UDP_PORT, RT_UDP_ADDR_1.c_str());
+  _brdcst1 = new UdpSocket(UDP_PORT, RT_UDP_ADDR_1.c_str());
   _brdcst1->openSock(UDP_BROADCAST);
 
-  _brdcst2 = new UdpSocket(RT_UDP_PORT, RT_UDP_ADDR_2.c_str());
+  _brdcst2 = new UdpSocket(UDP_PORT, RT_UDP_ADDR_2.c_str());
   _brdcst2->openSock(UDP_BROADCAST);
-
-  _groundBrdcst = new UdpSocket(GRND_UDP_PORT, GRND_UDP_ADDR.c_str());
-  _groundBrdcst->openSock(UDP_UNBOUND);
-
-  // Initialize GroundSummedData
-  //size_t counter = 0;
-  for (size_t i = 0; i < _groundVarList.size(); ++i)
-  {
-     for (size_t j = 0; j < _groundVarList[i]->Length; j++)
-     {
-       //counter++;
-       GroundSummedData.push_back(0.0);
-     }
-  }
-  //GroundSummedData = new NR_TYPE[counter];
-
 }
 
 /* -------------------------------------------------------------------- */
-void Broadcast::setCoordinatesFrom(const std::vector<var_base *> & list) const
+void Broadcast::BroadcastData(const std::string & timeStamp) 
 {
-  std::vector<var_base *>::const_iterator it;
-  for (it = list.begin(); it != list.end(); ++it)
-  {
-    if (strstr((*it)->name, "LON"))
-      cfg.SetCoordLON((*it)->name);
-    if (strstr((*it)->name, "LAT"))
-      cfg.SetCoordLAT((*it)->name);
-    //  Don't set it to PALTF, we don't want an altitude in feet as the ref.
-    if (strstr((*it)->name, "ALT") && strstr((*it)->name, "ALTF") == 0)
-      cfg.SetCoordALT((*it)->name);
-  }
-}
-
-/* -------------------------------------------------------------------- */
-std::vector<var_base *> Broadcast::readFile(const std::string & fileName) const
-{
-  char	*varList[512];
-  std::vector<var_base *> list;
-
-  ReadTextFile(fileName, varList);
-
-  for (int i = 0; varList[i]; ++i)
-  {
-    char target[BUFSIZ], *p;
-    strcpy(target, varList[i]);
-
-    p = strtok(target, ", \t\n");
-    while (p)
-    {
-      int index;
-
-      if ((index = SearchTable(raw, p)) != ERR)
-      {
-        list.push_back(raw[index]);
-      }
-      else
-      if ((index = SearchTable(derived, p)) != ERR)
-      {
-        list.push_back(derived[index]);
-      }
-      else
-      {
-        char msg[100];
-        sprintf(msg, "brdcast.cc: Variable [%s] not found, substituting ZERO.\n", p);
-        LogMessage(msg);
-        list.push_back(0);
-      }
-      p = strtok(NULL, ", \t\n");
-    }
-  }
-
-  FreeTextFile(varList);
-
-  return list;
-}
-
-/* -------------------------------------------------------------------- */
-void Broadcast::broadcastData(const std::string & timeStamp) 
-{
-  static int rate_cntr = 0;
-
   std::stringstream bcast;
   bcast << "IWG1," << timeStamp;
 
@@ -154,83 +63,6 @@ void Broadcast::broadcastData(const std::string & timeStamp)
   _brdcst2->writeSock(bcast.str().c_str(), bcast.str().length());
   printf(bcast.str().c_str());
 
-  // Send UDP Ground Feed.
-  if (cfg.GroundFeedType() != Config::UDP || rate_cntr++ < 180)  // Don't transmit the first couple minutes.
-    return;
-
-
-  // Sum the AveragedData
-  size_t counter = 0;
-  for (size_t i = 0; i < _groundVarList.size(); ++i)
-  {
-    for (size_t j = 0; j < _groundVarList[i]->Length; j++)
-    {
-      GroundSummedData[counter++] += AveragedData[(_groundVarList[i]->LRstart)+j];
-    }
-  }
-
-  // Only send every so often.
-  if ((rate_cntr % GroundFeedDataRate) != 0)
-    return;
- 
-  std::stringstream groundString;
-  if (cfg.Aircraft() == Config::HIAPER)
-    groundString << "GV";
-  if (cfg.Aircraft() == Config::NRL_P3)
-    groundString << "NRLP3";
-  if (cfg.Aircraft() == Config::C130)
-    groundString << "C130";
-
-  groundString << "," << timeStamp;
-
-  counter = 0;
-  for (size_t i = 0; i < _groundVarList.size(); ++i)
-  {
-    if (_groundVarList[i]->Length > 1) 
-    {
-      groundString << ",'{" << GroundSummedData[counter]/GroundFeedDataRate;
-      GroundSummedData[counter++] = 0.0;
-      for (size_t j = 1; j < _groundVarList[i]->Length; j++) 
-      {
-         groundString << "," << GroundSummedData[counter]/GroundFeedDataRate;
-         GroundSummedData[counter++] = 0.0;
-      }
-      groundString << "}'";
-    } else {
-      groundString << "," << GroundSummedData[counter]/GroundFeedDataRate;
-      GroundSummedData[counter++] = 0.0;
-    }
-  }
-  groundString << "\n";
-
-  // compress the stream before sending it to the ground
-  char buffer[32000];
-  memset(buffer, 0, 32000);
-  unsigned int bufLen = sizeof(buffer);
-  int ret = BZ2_bzBuffToBuffCompress( buffer, &bufLen, (char *) groundString.str().c_str(),
-                                      groundString.str().length(),9,0,0);
-  if (ret < 0) {
-    typedef struct {     // copied from bzlib.c
-      FILE*     handle;
-      char      buf[BZ_MAX_UNUSED];
-      int       bufN;
-      bool      writing;
-      bz_stream strm;
-      int       lastErr;
-      bool      initialisedOk;
-    } bzFile;
-    bzFile b;
-    b.lastErr = ret;
-    int errnum;
-    char msg[100];
-    sprintf(msg, "Failed to compress the ground feed stream: %s\n", BZ2_bzerror(&b, &errnum) );
-    LogMessage(msg);
-    return;
-  }
-  _groundBrdcst->writeSock(buffer, bufLen);
-
-  printf("\ncompressed %d -> %d\n", groundString.str().length(), bufLen);
-  printf("to ground: %s\n", groundString.str().c_str());
 }	// END BROADCASTDATA
 
 // END BRDCAST.CC
---
+++ nimbus/src/filter/grnd_feed.cc
@@ -0,0 +1,160 @@
+/*
+-------------------------------------------------------------------------
+OBJECT NAME:	grnd_feed.cc
+
+FULL NAME:	UDP broadcast for Ground Feed.
+
+ENTRY POINTS:	
+
+DESCRIPTION:	Class to produce 1 per second ascii string to broadcast
+		to ground.
+
+INPUT:		${PROJ_DIR}/###/groundvars
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 2005-08
+-------------------------------------------------------------------------
+*/
+
+#include "nimbus.h"
+#include "decode.h"
+#include "grnd_feed.h"
+
+#include "UdpSocket.h"
+
+#include <sstream>
+#include <bzlib.h>
+
+//const int GroundFeed::GRND_UDP_PORT = 31007;
+// eol-rt-data.guest.ucar.edu
+const std::string GroundFeed::DEST_HOST_ADDR = "128.117.188.122";
+
+
+/* -------------------------------------------------------------------- */
+GroundFeed::GroundFeed(int rate) : UDP_Base(31007), _dataRate(rate)
+{
+  std::string fileName(XMIT_VARS);
+  fileName += ".rt";
+  _varList = readFile(fileName);
+
+  setCoordinatesFrom(_varList);
+
+  _socket = new UdpSocket(UDP_PORT, DEST_HOST_ADDR.c_str());
+  _socket->openSock(UDP_UNBOUND);
+
+  // Initialize GroundSummedData
+  //size_t counter = 0;
+  for (size_t i = 0; i < _varList.size(); ++i)
+  {
+     for (size_t j = 0; j < _varList[i]->Length; j++)
+     {
+       //counter++;
+       _summedData.push_back(0.0);
+     }
+  }
+}
+
+/* -------------------------------------------------------------------- */
+void GroundFeed::setCoordinatesFrom(const std::vector<var_base *> & list) const
+{
+  std::vector<var_base *>::const_iterator it;
+  for (it = list.begin(); it != list.end(); ++it)
+  {
+    if (strstr((*it)->name, "LON"))
+      cfg.SetCoordLON((*it)->name);
+    if (strstr((*it)->name, "LAT"))
+      cfg.SetCoordLAT((*it)->name);
+    // Don't set it to PALTF, we don't want an altitude in feet as the reference.
+    if (strstr((*it)->name, "ALT") && strstr((*it)->name, "ALTF") == 0)
+      cfg.SetCoordALT((*it)->name);
+  }
+}
+
+/* -------------------------------------------------------------------- */
+void GroundFeed::BroadcastData(const std::string & timeStamp) 
+{
+  static int rate_cntr = 0;
+
+  if (cfg.GroundFeedType() != Config::UDP)
+    return;
+
+  if (rate_cntr++ < 120)  // Don't transmit the first couple minutes.
+    return;
+
+  extern NR_TYPE * AveragedData;
+
+  // Accumulate data so we can send an average.
+  size_t counter = 0;
+  for (size_t i = 0; i < _varList.size(); ++i)
+  {
+    for (size_t j = 0; j < _varList[i]->Length; j++)
+    {
+      _summedData[counter++] += AveragedData[(_varList[i]->LRstart)+j];
+    }
+  }
+
+  // Only send every so often.
+  if ((rate_cntr % _dataRate) != 0)
+    return;
+ 
+  std::stringstream groundString;
+  if (cfg.Aircraft() == Config::HIAPER)
+    groundString << "GV";
+  if (cfg.Aircraft() == Config::NRL_P3)
+    groundString << "NRLP3";
+  if (cfg.Aircraft() == Config::C130)
+    groundString << "C130";
+
+  groundString << "," << timeStamp;
+
+  counter = 0;
+  for (size_t i = 0; i < _varList.size(); ++i)
+  {
+    if (_varList[i]->Length > 1) 
+    {
+      groundString << ",'{" << _summedData[counter] / _dataRate;
+      _summedData[counter++] = 0.0;
+      for (size_t j = 1; j < _varList[i]->Length; j++) 
+      {
+         groundString << "," << _summedData[counter] / _dataRate;
+         _summedData[counter++] = 0.0;
+      }
+      groundString << "}'";
+    } else {
+      groundString << "," << _summedData[counter] / _dataRate;
+      _summedData[counter++] = 0.0;
+    }
+  }
+  groundString << "\n";
+
+  // compress the stream before sending it to the ground
+  char buffer[32000];
+  memset(buffer, 0, 32000);
+  unsigned int bufLen = sizeof(buffer);
+  int ret = BZ2_bzBuffToBuffCompress( buffer, &bufLen, (char *) groundString.str().c_str(),
+                                      groundString.str().length(),9,0,0);
+  if (ret < 0) {
+    typedef struct {     // copied from bzlib.c
+      FILE*     handle;
+      char      buf[BZ_MAX_UNUSED];
+      int       bufN;
+      bool      writing;
+      bz_stream strm;
+      int       lastErr;
+      bool      initialisedOk;
+    } bzFile;
+    bzFile b;
+    b.lastErr = ret;
+    int errnum;
+    char msg[100];
+    sprintf(msg, "Failed to compress the ground feed stream: %s\n", BZ2_bzerror(&b, &errnum) );
+    LogMessage(msg);
+    return;
+  }
+  _socket->writeSock(buffer, bufLen);
+
+//  printf("\ncompressed %d -> %d\n", groundString.str().length(), bufLen);
+  printf("GroundFeed: %s\n", groundString.str().c_str());
+
+}	// END BROADCASTDATA
+
+// END BRDCAST.CC
