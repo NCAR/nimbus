84457b674246c28f45141e5dec3fb5966d514c72
Chris Webster <cjw@ucar.edu>
2009-05-25 18:11:31 +0000

    More on renaming.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4420 640d5228-2204-0410-8d71-8f46fa6850b4

--- nc_utils/nc2asc/nc2asc.bat
+++ nc_utils/nc2asc/nc2asc.bat
@@ -2,4 +2,4 @@
 
 set LOCAL_CLASSPATH=.\nc2asc.jar;.\netcdfUI-2.2.22.jar;.\nlog4j-1.2.25.jar;.\commons-logging-1.1.jar
 
-java -cp %LOCAL_CLASSPATH% edu.ucar.eol.nc2Asc.NC2A  %*
+java -cp %LOCAL_CLASSPATH% edu.ucar.eol.nc2asc.NC2A  %*
