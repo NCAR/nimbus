992d6f547c053cd5c5c7771afcc4dde775a5bac7
Ling-Ling Dong <dongl@ucar.edu>
2009-07-29 19:23:55 +0000

    add libary files, build scripts, key, etc. to deploy the program
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4502 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ nc_utils/nc2asc/deploy/classpath.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# set the CLASSPATH to the jar files found in lib/linux
+
+cp=(lib/*.jar )
+OLDIFS=$IFS
+IFS=:
+# This syntax for expanding an array expands to a single word
+# with each member separated by the first character in IFS
+CLASSPATH="${cp[*]}"
+IFS=$OLDIFS
+
+export CLASSPATH
+
