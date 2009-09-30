d03db7600c5b35ffd3599fc540db7fd38cb08d10
Chris Webster <cjw@ucar.edu>
2009-09-30 15:01:21 +0000

    Update to reflect use of manifest in jar file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4597 640d5228-2204-0410-8d71-8f46fa6850b4

--- nc_utils/nc2asc/deploy/nc2asc.sh
+++ nc_utils/nc2asc/deploy/nc2asc.sh
@@ -2,5 +2,4 @@
 
 setenv JNC2ASC /opt/local/lib/nc2asc
 
-java  -cp ${JNC2ASC}/nc2asc.jar:${JNC2ASC}/netcdfUI-2.2.22.jar:${JNC2ASC}/nlog4j-1.2.25.jar:${JNC2ASC}/commons-logging-1.1.jar  edu.ucar.eol.nc2asc.NC2A  $* 
-
+java -jar ${JNC2ASC}/nc2asc.jar $* 
