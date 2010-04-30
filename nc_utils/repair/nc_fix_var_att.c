aef821bfe04a1711790085d835944c552f479930
Chris Webster <cjw@ucar.edu>
2010-04-30 21:04:07 +0000

    Upgrades to bring it current.  Remove the mix of netCDF 2 & 3 calls.  Fix compiler warnings.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4882 640d5228-2204-0410-8d71-8f46fa6850b4

--- nc_utils/repair/nc_fix_var_att.c
+++ nc_utils/repair/nc_fix_var_att.c
@@ -1,8 +1,10 @@
-/* gcc -I/jnet/local/include nc_fix_var_att.c -o nc_fix_var_att -L/jnet/local/lib -lnetcdf
-
- * This program should be custom tailored.
+/* gcc -I/usr/include/netcdf nc_fix_var_att.c -o nc_fix_var_att -lnetcdf
+ *
+ * This is a skeleton program to be custom tailored.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <netcdf.h>
 
@@ -11,14 +13,19 @@ int main(int argc, char *argv[])
   int	fd, id;
   char *att = "seconds since 2005-01-07 13:55:00 ";
 
-  fd = ncopen(argv[1], NC_WRITE);
-  ncredef(fd);
+  if (nc_open(argv[1], NC_WRITE, &fd) != NC_NOERR)
+  {
+    fprintf(stderr, "Can't open [%s]\n", argv[1]);
+    exit(1);
+  }
+
+  nc_redef(fd);
 
   nc_inq_varid(fd, "Time", &id);
   nc_put_att_text(fd, id, "units", strlen(att)+1, att);
 
-  ncendef(fd);
-  ncclose(fd);
+  nc_enddef(fd);
+  nc_close(fd);
 
   return 0;
 }
