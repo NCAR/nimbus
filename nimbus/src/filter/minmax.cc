f34d189e4bcd972a3dfaed90820defa91bec18cb
Chris Webster <cjw@ucar.edu>
2010-06-28 20:05:07 +0000

    Improve reliabilty [and readability].  :)
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4928 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/filter/minmax.cc
+++ nimbus/src/filter/minmax.cc
@@ -21,52 +21,42 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2010
 extern NR_TYPE	*HighRateData, *AveragedData, *SampledData;
 
 /* -------------------------------------------------------------------- */
+static void _findMinMax(var_base * vp, const NR_TYPE * value, size_t n)
+{
+  for (size_t i = 0; i < n; ++i) {
+    if (!isnan(value[i])) {
+      vp->min = std::min(vp->min, value[i]);
+      vp->max = std::max(vp->max, value[i]);
+    }
+  }
+}
+
+/* -------------------------------------------------------------------- */
 void FindMinMax()
 {
-  size_t i, j;
 {
   RAWTBL	*rp;
-  for (i = 0; i < raw.size(); i++)
+  for (size_t i = 0; i < raw.size(); i++)
   {
     rp = raw[i];
     if (rp->OutputRate > 1) {	// Scan raw data.
-      for (j = 0; j < rp->SampleRate * rp->Length; ++j) {
-        if (!isnan(SampledData[rp->SRstart + j])) {
-          rp->min = std::min(rp->min, SampledData[rp->SRstart + j]);
-          rp->max = std::max(rp->max, SampledData[rp->SRstart + j]);
-        }
-      }
+      _findMinMax(rp, &SampledData[rp->SRstart], rp->SampleRate * rp->Length);
     }
     else {
-      for (j = 0; j < rp->Length; ++j) {	// Scan averaged data.
-        if (!isnan(AveragedData[rp->LRstart + j])) {
-          rp->min = std::min(rp->min, AveragedData[rp->LRstart + j]);
-          rp->max = std::max(rp->max, AveragedData[rp->LRstart + j]);
-        }
-      }
+      _findMinMax(rp, &AveragedData[rp->LRstart], rp->Length);
     }
   }
 }
 
 {
   DERTBL	*dp;
-  for (i = 0; i < derived.size(); i++) {
-    if ((dp = ComputeOrder[i])->compute) {
+  for (size_t i = 0; i < derived.size(); i++) {
+    if ((dp = derived[i])->compute) {
       if (dp->OutputRate > 1) {
-        for (j = 0; j < cfg.HRTRate() * dp->Length; ++j) {
-          if (!isnan(HighRateData[dp->HRstart + j])) {
-            dp->min = std::min(dp->min, HighRateData[dp->HRstart + (SampleOffset * dp->Length)]);
-            dp->min = std::min(dp->min, HighRateData[dp->HRstart + (SampleOffset * dp->Length)]);
-          }
-        }
+        _findMinMax(dp, &HighRateData[dp->HRstart], cfg.HRTRate() * dp->Length);
       }
       else {
-        for (j = 0; j < dp->Length; ++j) {
-          if (!isnan(AveragedData[dp->LRstart + j])) {
-            dp->min = std::min(dp->min, AveragedData[dp->LRstart + j]);
-            dp->max = std::max(dp->max, AveragedData[dp->LRstart + j]);
-          }
-        }
+        _findMinMax(dp, &AveragedData[dp->LRstart], dp->Length);
       }
     }
   }
