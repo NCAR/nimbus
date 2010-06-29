8fb58aa5f24df61cf1aa410a202e3d3c6a04f8f3
Chris Webster <cjw@ucar.edu>
2010-06-29 19:29:34 +0000

    Upgrade to Java netCDF 4.1.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@4941 640d5228-2204-0410-8d71-8f46fa6850b4

--- nc_utils/nc2asc/deploy/nc2asc.bat
+++ nc_utils/nc2asc/deploy/nc2asc.bat
@@ -1,5 +1,5 @@
 @echo on
 
-set LOCAL_CLASSPATH=.\nc2asc.jar;.\netcdfUI-2.2.22.jar;.\nlog4j-1.2.25.jar;.\commons-logging-1.1.jar
+set LOCAL_CLASSPATH=.\nc2asc.jar;.\netcdfUI-4.1.jar;.\nlog4j-1.2.25.jar;.\commons-logging-1.1.jar
 
 java -cp %LOCAL_CLASSPATH% edu.ucar.eol.nc2asc.NC2A  %*
