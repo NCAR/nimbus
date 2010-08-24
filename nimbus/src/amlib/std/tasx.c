d367fc0fd3612bf026124bb0f9b54fba3df90246
Chris Webster <cjw@ucar.edu>
2010-08-24 14:57:56 +0000

    Allow a secondary parameter to be passed in.  If the primary is nan, then use the secondary.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5024 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/tasx.c
+++ nimbus/src/amlib/std/tasx.c
@@ -2,26 +2,15 @@
 -------------------------------------------------------------------------
 OBJECT NAME:	tasx.c
 
-FULL NAME:	True Airspeed
-
-ENTRY POINTS:	sfunc()
-
-STATIC FNS:	none
+FULL NAME:	Reference True Airspeed Assignment
 
 DESCRIPTION:	TASX is used by other parameters to compute their values.
 		TASX takes on one of the sampled TAS variables.  This
 		method allows users to set TASX to what ever is working
-		or most accurate.
+		or most accurate.  It can also take a secondary tas to
+		use should the primary go nan.
 
-INPUT:		True Airspeed to use
-
-OUTPUT:		
-
-REFERENCES:	none
-
-REFERENCED BY:	compute.c
-
-COPYRIGHT:	University Corporation for Atmospheric Research, 1993
+COPYRIGHT:	University Corporation for Atmospheric Research, 1993-2010
 -------------------------------------------------------------------------
 */
 
@@ -33,6 +22,10 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1993
 void stasx(DERTBL *varp)
 {
   NR_TYPE	tas = GetSample(varp, 0);
+
+  // If we have a secondary tas, use it if primary is nan.
+  if (isnan(tas) && varp->ndep > 1)
+    tas = GetSample(varp, 1);
 
   if (tas <= 0.0)
     tas = 0.001;
