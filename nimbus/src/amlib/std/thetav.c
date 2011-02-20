923bbc1bdd756df9ea9f30ce2445eb7365f36f47
Chris Webster <cjw@ucar.edu>
2011-02-20 17:34:54 +0000

    New processing for THETAV (equivelant potential temperature), and re-introduction of TVIR (virtual temperature).
    THATEV now depends upon TVIR instead of computing it interanally.  This change in the DependTable will break processing of old projects.  Easy fix to DependTable and TVIR will need to be added to the project specific VarDB.
    
    This check in is per the processing algorithm review group.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5231 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/thetav.c
+++ nimbus/src/amlib/std/thetav.c
@@ -14,22 +14,11 @@ void sthetav(DERTBL *varp)
 {
   NR_TYPE	thetav = 0.0, atx, tvir, psxc, edpc, sphum;
 
-  atx	= GetSample(varp, 0);
+  tvir	= GetSample(varp, 0);
   psxc	= GetSample(varp, 1);
-  edpc	= GetSample(varp, 2);
-  sphum	= psxc - 0.378 * edpc;
-
-  if (sphum != 0.0)
-    sphum = 622.0 * edpc / sphum;
-
-  tvir = 1.0 - 0.6e-3 * sphum;
-
-  if (tvir != 0.0)
-    tvir = (atx + Kelvin) / tvir - Kelvin;
 
   if (psxc != 0.0)
-    thetav = (tvir + Kelvin) * pow( (double)(1000.0 / psxc), (double)0.28571);
+    thetav = (tvir + Kelvin) * pow((double)(1000.0 / psxc), (double)0.28571);
 
   PutSample(varp, thetav);
-
 }
