271dbe3dc1fb6f8730c371117948cb1f2e45dba6
Chris Webster <cjw@ucar.edu>
2011-03-03 18:34:27 +0000

    Update comment clock.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@5255 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/thetav.c
+++ src/amlib/std/thetav.c
@@ -1,24 +1,34 @@
-/*******       VIRTUAL POTENTIAL TEMPERATURE (K)                        THETAV
- 	Input:
- 		tvir - derived virtual temperature
- 		psxc - derived static pressure
- 	Output:
- 		thetav - derived virtual potential temperature
+/*
+-------------------------------------------------------------------------
+OBJECT NAME:	thetav.c
+
+FULL NAME:	Virtual Potential Temperature
+
+DESCRIPTION:	
+
+INPUT:		TVIR, PSXC
+
+OUTPUT:		THETAV deg_K
+
+REFERENCES:	
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 2011
+-------------------------------------------------------------------------
 */
- 
+
 #include "nimbus.h"
 #include "amlib.h"
 
 /* -------------------------------------------------------------------- */
 void sthetav(DERTBL *varp)
 {
-  NR_TYPE	thetav = 0.0, atx, tvir, psxc, edpc, sphum;
+  NR_TYPE	thetav = 0.0, tvir, psxc;
 
   tvir	= GetSample(varp, 0);
   psxc	= GetSample(varp, 1);
 
   if (psxc != 0.0)
-    thetav = (tvir + Kelvin) * pow((double)(1000.0 / psxc), (double)0.28571);
+    thetav = (tvir + Kelvin) * pow((double)(1000.0 / psxc), Rd_DIV_Cpd);
 
   PutSample(varp, thetav);
 }
