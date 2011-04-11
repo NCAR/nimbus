cd93bd6a72e7af4e9feeeee464274e42c1c98222
Chris Webster <cjw@ucar.edu>
2011-04-11 18:33:51 +0000

    Dew-Point & WaterVapor work.
    edpc.c
     - add initializer for EDPC
     - EDPC detects whether parent DP was calculated with MurphyKoop or
       GoffGratch and uses the same.
     - Add netCDF attribute for EDPC variable stating which algorithm was used.
    
    dp.c
     - Split GG & MK into separate DP functions.
     - Add netCDF attribute for DP variables stating which algorithm was used.
    
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5312 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/dpc.c
+++ nimbus/src/amlib/std/dpc.c
@@ -5,10 +5,10 @@ OBJECT NAME:	dpc.c
 FULL NAME:	Dew point
 
 ENTRY POINTS:	dpInit()
-		sdpc()
+		sdpc_GG()
+		sdpc_MK()
 
-STATIC FNS:	WaterVaporPressure*()
-		WaterVaporPressure()
+STATIC FNS:	none
 
 DESCRIPTION:	Computes the difference between dew point and frost point
 		derived from Murphy-Koop vapor pressure equations for plane
@@ -29,10 +29,13 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1992-2011
 
 #include "nimbus.h"
 #include "amlib.h"
+#include <map>
 
 #include <gsl/gsl_spline.h>
 
 double fw(double, double);	// See esubt.c
+
+std::map<std::string, bool> dp_method;
 
 static const size_t TableSize = 300;
 static double ttable[TableSize];
@@ -41,57 +44,51 @@ static double etable[TableSize];
 static gsl_interp_accel *acc;
 static gsl_interp *linear = 0;
 
+double WaterVaporPressure(double Tk);
+double WaterVaporPressureOfWater(double Tk);
+
 
 /* -------------------------------------------------------------------- */
-static double WaterVaporPressureOfIce(double Tk)
+void sdpc_GG(DERTBL *varp)	// Goff-Gratch.
 {
-//  static const double b0 = 9.550426;
-  static const double b1 = -5723.265;
-  static const double b2 = 3.53068;
-  static const double b3 = -0.00728332;	// K-1
-  static const double b0prime = 6.111536;	// hPa
+  double dp = GetSample(varp, 0);
 
-  return b0prime *
-	exp(b1 * (Kelvin - Tk) / (Kelvin * Tk) + b2 * log(Tk / Kelvin) + b3 * (Tk - Kelvin));
-}
+  if (dp < 0.0)
+    dp = 0.009109 + dp * (1.134055 + dp * 0.001038);
 
-static double WaterVaporPressureOfWater(double Tk)
-{
-  static const double e0 = 6.091888;	// hPa
-//  static const double e1 = 6.564725;
-  static const double e2 = -6763.22;	// K
-  static const double e3 = -4.210;
-  static const double e4 = 0.000367;	// K-1
-  static const double e5 = 0.0415;	// K-1
-  static const double e6 = -0.1525967;
-  static const double e7 = -1331.22;	// K
-  static const double e8 = -9.44523;
-  static const double e9 = 0.014025;	// K-1
-  static const double Tx = 218.8;	// K
-
-  double alpha = tanh(e5 * (Tk - Tx));
-
-  double d2 = e2 + alpha * e7;
-  double d3 = e3 + alpha * e8;
-  double d4 = e4 + alpha * e9;
-
-  return e0 *
-	exp((alpha - 1.0) * e6 + d2 * (Kelvin - Tk) / (Tk*Kelvin) + d3 * log(Tk/Kelvin) + d4 * (Tk - Kelvin));
+  PutSample(varp, dp);
 }
 
 /* -------------------------------------------------------------------- */
-static double WaterVaporPressure(double Tk)
+void sdpc_MK(DERTBL *varp)	// Murphy-Koop.
 {
-  // Murphy Koop.
-  if (Tk < Kelvin)
-    return WaterVaporPressureOfIce(Tk);
-  else
-    return WaterVaporPressureOfWater(Tk);
+  double dp = GetSample(varp, 0);
+  double psxc = GetSample(varp, 1);
+  double Tk = dp + Kelvin;
+  double e;
+
+  // fw() From Murphy and Koop, 2005.  Enhancement factor.  See esubt.c
+  e = WaterVaporPressure(Tk) * fw(Tk, psxc);
+  dp = gsl_interp_eval(linear, etable, ttable, e, acc);
+
+  PutSample(varp, (NR_TYPE)dp);
 }
 
 /* -------------------------------------------------------------------- */
 void dpInit(var_base *varp)
 {
+  if (((DERTBL *)varp)->ndep == 1)
+  {
+    dp_method[varp->name] = false;
+    ((DERTBL *)varp)->compute = (void (*)(void*))sdpc_GG;
+    AddToAttributes(varp->name, "Method", "Goff-Gratch");
+  }
+  else
+  {
+    dp_method[varp->name] = true;
+    AddToAttributes(varp->name, "Method", "Murphy-Koop");
+  }
+
   // Bail out if we've already initialized the interp stuff.  Only needed once.
   if (linear)
     return;
@@ -109,33 +106,4 @@ void dpInit(var_base *varp)
   acc = gsl_interp_accel_alloc();
   linear = gsl_interp_alloc(gsl_interp_linear, TableSize);
   gsl_interp_init(linear, etable, ttable, TableSize);
-}
-
-/* -------------------------------------------------------------------- */
-void sdpc(DERTBL *varp)
-{
-  double dp = GetSample(varp, 0);
-
-  /* Used prior to 2011.  We'll use the old algorithm if there is only one
-   * dependency in the DependTable, that will keep old projects running.
-   */
-  if (varp->ndep == 1)
-  {
-    if (dp < 0.0)
-      dp = 0.009109 + dp * (1.134055 + dp * 0.001038);
-
-    PutSample(varp, dp);
-    return;
-  }
-
-
-  double psxc = GetSample(varp, 1);
-  double Tk = dp + Kelvin;
-  double e;
-
-  // fw() From Murphy and Koop, 2005.  Enhancement factor.  See esubt.c
-  e = WaterVaporPressure(Tk) * fw(Tk, psxc);
-  dp = gsl_interp_eval(linear, etable, ttable, e, acc);
-
-  PutSample(varp, (NR_TYPE)dp);
 }
--- nimbus/src/amlib/std/esubt.c
+++ nimbus/src/amlib/std/esubt.c
@@ -7,14 +7,16 @@ FULL NAME:	Water Vapor Pressure
 ENTRY POINTS:	esubt()
 		sedpc()
 
-DESCRIPTION:	ESUBT function implements the Goff-Gratch formula ("Trans. Am.
-		Soc. Heat. Vent. Eng.," Vol. 52, pp. 95-121, 1946) for
-		computing vapor pressure over a plane wather surface.
+DESCRIPTION:	ESUBT function prior to 2011was based on the Goff-Gratch formula
+		("Trans. Am.  Soc. Heat. Vent. Eng.," Vol. 52, pp. 95-121, 1946)
+		for computing vapor pressure over a plane water surface.  After
+		2011 Murphy and Koop below is used.
 		
 		Prior to 2011, the code included the enhancement factor of Buck
 		("J. Appl. Meteorol.," Vol. 20, pp. 1527-1532, 1981).
-		After Feb 18, the code includes the enhancement factor of
-		Murphy and Koop ("Q. J. Roy. Met. Soc.", Vol. 131, pp. 1539-1565, 2005).
+
+		After Feb 18, the code includes the enhancement factor of Murphy
+		and Koop ("Q. J. Roy. Met. Soc.", Vol. 131, pp. 1539-1565, 2005).
 
 COPYRIGHT:	University Corporation for Atmospheric Research, 1992-2011
 -------------------------------------------------------------------------
@@ -22,8 +24,56 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 1992-2011
 
 #include "nimbus.h"
 #include "amlib.h"
+#include <map>
 
 static const double	Ts = 373.16;
+
+/* -------------------------------------------------------------------- */
+double WaterVaporPressureOfIce(double Tk)
+{
+//  static const double b0 = 9.550426;
+  static const double b1 = -5723.265;
+  static const double b2 = 3.53068;
+  static const double b3 = -0.00728332;	// K-1
+  static const double b0prime = 6.111536;	// hPa
+
+  return b0prime *
+        exp(b1 * (Kelvin - Tk) / (Kelvin * Tk) + b2 * log(Tk / Kelvin) + b3 * (Tk - Kelvin));
+}
+
+double WaterVaporPressureOfWater(double Tk)
+{
+  static const double e0 = 6.091888;	// hPa
+//  static const double e1 = 6.564725;
+  static const double e2 = -6763.22;	// K
+  static const double e3 = -4.210;
+  static const double e4 = 0.000367;	// K-1
+  static const double e5 = 0.0415;	// K-1
+  static const double e6 = -0.1525967;
+  static const double e7 = -1331.22;	// K
+  static const double e8 = -9.44523;
+  static const double e9 = 0.014025;	// K-1
+  static const double Tx = 218.8;	// K
+
+  double alpha = tanh(e5 * (Tk - Tx));
+
+  double d2 = e2 + alpha * e7;
+  double d3 = e3 + alpha * e8;
+  double d4 = e4 + alpha * e9;
+
+  return e0 *
+        exp((alpha - 1.0) * e6 + d2 * (Kelvin - Tk) / (Tk*Kelvin) + d3 * log(Tk/Kelvin) + d4 * (Tk - Kelvin));
+}
+
+/* -------------------------------------------------------------------- */
+double WaterVaporPressure(double Tk)
+{
+  // Murphy-Koop.
+  if (Tk < Kelvin)
+    return WaterVaporPressureOfIce(Tk);
+  else
+    return WaterVaporPressureOfWater(Tk);
+}
 
 /* -------------------------------------------------------------------- */
 double fw(double Tk, double pressure)
@@ -31,6 +81,8 @@ double fw(double Tk, double pressure)
   // From Murphy and Koop, 2005.  Enhancement factor.
   return 1.0 + 1.0e-5 * pressure * (4.923 - 0.0325 * Tk + 5.84e-5 * Tk * Tk);
 }
+
+static bool MurphyKoop = true;
 
 /* -------------------------------------------------------------------- */
 double esubt(double temperature, double pressure)
@@ -41,19 +93,38 @@ double esubt(double temperature, double pressure)
   if (Tk < 1.0)
     Tk = 1.0;
 
-  ew = pow(10.0, (
+  if (MurphyKoop)
+    ew = WaterVaporPressure(Tk);
+  else
+  {
+    ew = pow(10.0, (
 	-7.90298 * ((Ts / Tk) - 1.0)
 	+5.02808 * log10(Ts / Tk)
 	-1.3816e-7 * (pow(10.0, 11.344 * (1.0 - Tk / Ts)) - 1.0)
 	+8.1328e-3 * (pow(10.0, -3.49149 * (Ts / Tk - 1.0)) - 1.0))
 	+log10(StdPress) );
 
-  /* Arden Buck's pressure enhancement factor.  Used prior to 2011.
-   * double fw = 1.0007 + (3.46e-6 * pressure);
-   */
+    // Arden Buck's pressure enhancement factor.
+    double fw = 1.0007 + (3.46e-6 * pressure);
+    ew *= fw;
+  }
 
+  return ew;
+}
 
-  return ew * fw(Tk, pressure);
+/* -------------------------------------------------------------------- */
+void edpcInit(var_base *varp)
+{
+  extern std::map<std::string, bool> dp_method;
+  std::string dp(((DERTBL *)((DERTBL *)varp)->depends[0])->depend[0]);
+
+  if (dp_method.count(dp) > 0)
+    MurphyKoop = dp_method[dp];
+
+  if (MurphyKoop)
+    AddToAttributes(varp->name, "Method", "MurphyKoop");
+  else
+    AddToAttributes(varp->name, "Method", "GoffGratch");
 }
 
 /* -------------------------------------------------------------------- */
