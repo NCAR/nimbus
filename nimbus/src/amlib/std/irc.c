5e984c126bc615b218fa0d4e005e007cb5073691
Chris Webster <cjw@ucar.edu>
2011-11-10 14:50:12 +0000

    Retrieve DTT/DTB cals from Defaults file. Add DTB/DTT cals to netCDF file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5562 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/irc.c
+++ nimbus/src/amlib/std/irc.c
@@ -1,15 +1,17 @@
 /*                  CORRECTED PYRGEOMETER DATA MODULE                     *
  *                                                                        *
- ** CORRECTED PYRGEOMTER IRRADIANCE (WATTS/M2)
+ ** CORRECTED EPPLEY PYRGEOMTER IRRADIANCE (WATTS/M2)
         Input:
                 ir - raw pyrgeometer data
                 st - raw pyrg. sink data
                 dt - raw pyrg. dome data
+
+ ** CORRECTED CGR4 PYRGEOMTER IRRADIANCE (WATTS/M2)
+        Input:
+                ir - raw pyrgeometer data
+                dt - raw pyrg. dome data
         Output:
                 irc - derived pyrgeometer irradiance (w/m2)
-        Include:
-                irc.data - Silicon dome constants
-        Endspec:
 
 */
 
@@ -30,14 +32,25 @@
 
 static const double SBC = 5.6686E-8;
 
-std::map<std::string, std::vector<double> > cals;
+static std::vector<float> dome_temp_cal;
+
+static std::map<std::string, std::vector<float> > cals;
 
 /* -------------------------------------------------------------------- */
 void initPyrgeometer(var_base *varp)
 {
+  DERTBL *dp = (DERTBL *)varp;
+
+  /* If old Eppley processing, then leave.  Eppley's had two dependencies,
+   * sink tempearture and dome temperature.  The new CGR4 has just dome
+   * temperature.
+   */
+  if (dp->ndep > 2)	// If old Eppley processing, then leave.
+    return;
+
   float	*tmp;
   char	name[100];
-  std::vector<double> cal;
+  std::vector<float> cal;
 
   sprintf(name, "%s_CAL", varp->name);
   if ((tmp = GetDefaultsValue(name, varp->name)) == NULL)
@@ -54,6 +67,25 @@ void initPyrgeometer(var_base *varp)
   }
 
   cals[varp->name] = cal;
+
+  // Get cals for DTB/DTT.
+  cal.clear();
+  sprintf(name, "%s_CAL", dp->depend[1]);
+  if ((tmp = GetDefaultsValue(name, dp->depend[1])) == NULL)
+  {
+    sprintf(buffer, "irc.c: Pyrgeometer, no %s in defaults file.", name);
+    LogMessage(buffer);
+    cal.push_back(26.505);
+    cal.push_back(-61.487);
+    AddToDefaults(dp->depend[1], name, cal);
+  }
+  else
+  {
+    cal.push_back(tmp[0]);
+    cal.push_back(tmp[1]);
+  }
+
+  dome_temp_cal = cal;
 }
 
 /* -------------------------------------------------------------------- */
@@ -69,7 +101,7 @@ void sirc(DERTBL *varp)
      * create an interim variable, we will apply the cal here and back-write
      * DTT and DTB.
      */
-    dt = 26.505 + -61.487 * log10(dt);
+    dt = dome_temp_cal[0] + dome_temp_cal[1] * log10(dt);
     PutSample(varp->depends[1], dt);
     PutSample(varp, ((cals[varp->name])[0] * ir - (cals[varp->name])[1]) + SBC * pow(dt+Kelvin, 4.0));
   }
