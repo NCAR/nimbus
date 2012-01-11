3693be2a431df85dca030f3b2bfcd69b168d38f1
Chris Webster <cjw@ucar.edu>
2012-01-11 22:24:40 +0000

    Update to use new WaterVaporPressure() calculation, instead of esubt().
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@5616 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/rhum.c
+++ src/amlib/std/rhum.c
@@ -10,20 +10,29 @@
 #include "nimbus.h"
 #include "amlib.h"
 
-double esubt(double, double);
 
 /* -------------------------------------------------------------------- */
 void srhum(DERTBL *varp)
 {
-  NR_TYPE	atx, psxc, edpc, rhum;
+  NR_TYPE atx, psxc, e, rhum;
 
-  atx	= GetSample(varp, 0);
-  psxc	= GetSample(varp, 1);
-  edpc	= GetSample(varp, 2);
-  rhum	= (NR_TYPE)esubt(atx, psxc);
+  atx = GetSample(varp, 0);
+  if (varp->ndep == 3)	// Old method for computing RHUM.
+  {
+    double esubt(double, double);
+    psxc = GetSample(varp, 1);
+    e	= GetSample(varp, 2);
+    rhum = (NR_TYPE)esubt(atx, psxc);
+  }
+  else	// New method.
+  {
+    double WaterVaporPressure(double);
+    e = GetSample(varp, 1);
+    rhum = (NR_TYPE)WaterVaporPressure(atx);
+  }
 
   if (rhum != 0.0)
-    rhum = 100.0 * edpc / rhum;
+    rhum = 100.0 * e / rhum;
 
   PutSample(varp, rhum);
 
