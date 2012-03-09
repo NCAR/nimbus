72387d995a768d5d73445a0185e0af97f8ba601e
John Wasinger <wasinger@ucar.edu>
2012-03-09 21:25:36 +0000

    Control Research Data Generation
    Two toggle buttons are created to append '(NO|YES)CALIB' and
    '(NO|YES)RESCH' flags to the end of the IWG1 stream broadcast
    on the aircraft.  This is done by setting 
    
    Instruments shall use these flags to restrict calibration and
    gathering of research data.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5689 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/filter/brdcast.cc
+++ nimbus/src/filter/brdcast.cc
@@ -47,6 +47,52 @@ Broadcast::Broadcast() : UDP_Base(7071)
 }
 
 /* -------------------------------------------------------------------- */
+std::string Broadcast::extractPQString(PGresult *result, int tuple, int field)
+{
+  const char* pqstring = PQgetvalue(result, tuple, field);
+  if (! pqstring)
+    return "";
+
+  int len = strlen(pqstring);
+  while (len > 0 &&
+          isascii(pqstring[len-1]) &&
+          isspace(pqstring[len-1]))
+    len--;
+
+  return std::string(pqstring, len);
+}
+
+/* -------------------------------------------------------------------- */
+std::string Broadcast::getGlobalAttribute(std::string attr)
+{
+  PGconn *conn = PQconnectdb("host='acserver' dbname='real-time' user ='ads'");
+
+  /* check to see that the backend connection was successfully made
+   */
+  if (PQstatus(conn) == CONNECTION_BAD)
+  {
+    PQfinish(conn);
+    std::cerr << "PQconnectdb: Connection failed." << std::endl;
+    return "";
+  }
+  std::string query = "SELECT value FROM global_attributes WHERE key='";
+  query += attr + "'";
+  PGresult * res = PQexec(conn, query.c_str());
+
+  int ntuples = PQntuples(res);
+
+  if (ntuples == 0)
+  {
+    std::cerr << "No global attribute " << attr << "!\n";
+    return "";
+  }
+  std::string s = extractPQString(res, 0, 0);
+  PQclear(res);
+  PQfinish(conn);
+  return s;
+}
+
+/* -------------------------------------------------------------------- */
 void Broadcast::BroadcastData(const std::string & timeStamp) 
 {
   if (_varList.size() == 0)
@@ -71,6 +117,11 @@ void Broadcast::BroadcastData(const std::string & timeStamp)
         bcast << AveragedData[_varList[i]->LRstart];
     }
   }
+  // append ?CALIB, and ?RESCH flags to message
+//std::cout << (getGlobalAttribute("NoCalib") == "TRUE" ? "NO" : "YES") << "CALIB,";
+//std::cout << (getGlobalAttribute("NoResch") == "TRUE" ? "NO" : "YES") << "RESCH" << std::endl;
+  bcast << (getGlobalAttribute("NoCalib") == "TRUE" ? "NO" : "YES") << "CALIB,";
+  bcast << (getGlobalAttribute("NoResch") == "TRUE" ? "NO" : "YES") << "RESCH";
 
   bcast << "\r\n";
   for (size_t i = 0; i < _toList.size(); ++i)
--- nimbus/src/filter/psql.cc
+++ nimbus/src/filter/psql.cc
@@ -289,6 +289,8 @@ PostgreSQL::initializeGlobalAttributes()
   _sqlString << "INSERT INTO global_attributes VALUES ('wind_field', '" << cfg.WindFieldVariables() << "');";
   _sqlString << "INSERT INTO global_attributes VALUES ('landmarks', '" << readLandmarks() << "');";
   _sqlString << "INSERT INTO global_attributes VALUES ('DataRate', '" << cfg.GroundFeedDataRate() << "');";
+  _sqlString << "INSERT INTO global_attributes VALUES ('NoCalib', 'FALSE'";
+  _sqlString << "INSERT INTO global_attributes VALUES ('NoResch', 'FALSE'";
 
   submitCommand(_sqlString.str(), true);
 
