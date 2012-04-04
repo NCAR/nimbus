9a21f8a8a236f4afbf1b73c791e3b8e190e19d28
Chris Webster <cjw@ucar.edu>
2012-04-04 20:40:22 +0000

    Add copyright.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@5717 640d5228-2204-0410-8d71-8f46fa6850b4

--- GoogleEarth/acTrack2kml/acTrack2kml.cc
+++ GoogleEarth/acTrack2kml/acTrack2kml.cc
@@ -1,6 +1,8 @@
 /*
  * Program to read real-time flight database periodically (say 60 seconds)
  * and create a KML for google earth.
+ *
+ * COPYRIGHT: University Corporation for Atmospheric Research, 2005-2012
  */
 #include <libpq-fe.h>
 #include <string>
