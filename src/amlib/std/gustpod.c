f68439e6cf11f9fcae54d675274cd264b218b623
Chris Webster <cjw@ucar.edu>
2012-05-09 17:20:48 +0000

    Add reading of calibrations from Defaults file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@5823 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/gustpod.c
+++ src/amlib/std/gustpod.c
@@ -6,7 +6,7 @@ FULL NAME:	Gust Pod calculations
 
 DESCRIPTION:	Derived GustPod calcs.
 
-COPYRIGHT:	University Corporation for Atmospheric Research, 2010
+COPYRIGHT:	University Corporation for Atmospheric Research, 2010-2012
 -------------------------------------------------------------------------
 */
 
@@ -17,11 +17,45 @@ static size_t timeCount = 0;
 static NR_TYPE PSTcor, PSQcor;
 
 static NR_TYPE ak_coeff[] = { -0.0478, 0.1299 };
-static NR_TYPE ss_coeff[] = { -0.1564, 0.09218 };
+static NR_TYPE ss_coeff[] = { -0.1564, 0.09218 };	// Left Wing
+//static NR_TYPE ss_coeff[] = { 0.1662, 0.09218 };	// Right Wing
 
 static NR_TYPE recfx = 0.985;
 
 NR_TYPE tas(NR_TYPE, NR_TYPE, NR_TYPE);
+
+
+/* -------------------------------------------------------------------- */
+void GPinit(var_base *varp)
+{
+  float *tmp;
+
+  if ((tmp = GetDefaultsValue("GP_SS_COEFF", varp->name)) == NULL)
+  {
+    sprintf(buffer,"SS cals set to %f,%f in AMLIB function initGust.\n", ss_coeff[0], ss_coeff[1]);
+    LogMessage(buffer);
+  }
+  else
+    for (int i = 0; i < 2; ++i)
+      ss_coeff[i] = tmp[i];
+
+  if ((tmp = GetDefaultsValue("GP_RECOVERY", varp->name)) == NULL)
+  {
+    sprintf(buffer,"Recovery value set to %f in AMLIB function initGust.\n", recfx);
+    LogMessage(buffer);
+  }
+  else
+    recfx = tmp[0];
+
+  if ((tmp = GetDefaultsValue("GP_AK_COEFF", varp->name)) == NULL)
+  {
+    sprintf(buffer,"AK cals set to %f,%f in AMLIB function initGust.\n", ak_coeff[0], ak_coeff[1]);
+    LogMessage(buffer);
+  }
+  else
+    for (int i = 0; i < 2; ++i)
+      ak_coeff[i] = tmp[i];
+}
 
 /* -------------------------------------------------------------------- */
 void spsc_gp(DERTBL *varp)	// Static Pressure
