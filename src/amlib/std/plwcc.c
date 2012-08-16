393ea2cafc8dc62e62d4d3d4bb506db44116fee4
Chris Webster <cjw@ucar.edu>
2012-08-16 21:14:39 +0000

    Revamp of the King Liquid Water probe processing code.  Per processing algorithm review committee.  See document on raf-science wiki by Al Cooper for details.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@5975 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/plwcc.c
+++ src/amlib/std/plwcc.c
@@ -1,264 +1,247 @@
-/**************************** PLWCC *******************************  PLWCC
- ** Corrected PMS liquid water content (g/m3)
-   Input:
-     plwc - raw liquid water content
-     tasx - derived true air speed
-     atx  - derived ambient temperature
-     psxc - derived static pressure
-   Output:
-     plwcc - corrected PMS liquid water content
+/*
+-------------------------------------------------------------------------
+OBJECT NAME:	plwcc.c
+
+FULL NAME:	King Liquid Water Content
+
+ENTRY POINTS:	sfunc()
+
+STATIC FNS:	
+
+DESCRIPTION:	King Liquid Water content calculation.  Two algorithms, one
+		for C130 and one for the GV.  Difference is in number of
+		coefficients for Nusselt number.
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 1992-2012
+-------------------------------------------------------------------------
 */
 
 #include "nimbus.h"
 #include "amlib.h"
 
-struct _running_average
-{
-    int		nSamples[nFeedBackTypes];
-    NR_TYPE	*data_values[nFeedBackTypes];
-    int		buffIndex[nFeedBackTypes];
-    NR_TYPE	sum[nFeedBackTypes];
-    NR_TYPE	average[nFeedBackTypes];
-};
- 
-static const size_t baseline_lwc_seconds = 30;
-static const size_t baseline_conc_seconds = 2;
+static const int nLWC = 2;
 
-/*  Values from /home/local/proj/Defaults on 23 April 1998, RLR		*/
-static NR_TYPE  twire[3] = {155.0, 155.0, 155.0};	/* Wire temperature (C)	*/
-static NR_TYPE  tasFac[3] = {1.0, 1.0, 1.0};		/* True airspeed factor	*/
-static NR_TYPE  twireDiam[3] = {0.21, 0.21, 0.21};	/* Diameter	*/
-static NR_TYPE  cloud_conc_threshold = 0.01;		/* Cloud Concentration Baseline Threshold */
+static const double cond0 = 0.0238;
+static const double cond1 = 0.000071;
+static const double visc0 = 1.718e-5;
+static const double visc1 = 0.0049e-5;
 
-// Four running averages.
-struct _running_average lwc1, lwc2, conc1, conc2, lwcg, concg;
+// Coefficients to determine the boiling point.
+static const float bp[4] = { 0.03366503,1.34236135,-0.33479451,0.03519342 };
+
+// consts.c?
+static const double Lv0 = 2.501e6;
+
+//  Values that can be over-ridden from Defaults file.
+static NR_TYPE	tWire[nLWC] = {130.0, 130.0};	// Wire temperature (C)
+static NR_TYPE	dWire[nLWC] = {0.1805e-2, 0.1805e-2}; // Wire Diameter
+static NR_TYPE	lWire[nLWC] = {2.1e-2, 2.1e-2};	// Wire length (mm)
+static int	tau_Nu[nLWC] = {120, 120};		// Nusselt-number time constant.
+static NR_TYPE	cloud_conc_threshold = 0.25;		// Cloud Concentration Baseline Threshold
 
 
-NR_TYPE kinglwcc(NR_TYPE plwc, NR_TYPE tasx, NR_TYPE atx, NR_TYPE psxc, NR_TYPE twire, NR_TYPE diam);
+static NR_TYPE CooperLWC_GV(const NR_TYPE plwc, const NR_TYPE tasx, const NR_TYPE atx,
+	const NR_TYPE psxc, NR_TYPE concf, const int indx);
 
-/* -------------------------------------------------------------------- */
-/* Allocate structure for running average.  Each plwcc() below needs a
- * running average for concentration and liquid water.
- */
-static void allocate(struct _running_average * ra, size_t nSeconds)
-{
-  ra->nSamples[LOW_RATE_FEEDBACK] = nSeconds;
-  ra->nSamples[HIGH_RATE_FEEDBACK] = nSeconds * cfg.HRTRate();
+static NR_TYPE CooperLWC_C130(const NR_TYPE plwc, const NR_TYPE tasx, const NR_TYPE atx,
+	const NR_TYPE psxc, NR_TYPE concf, const int indx);
 
-  if (ra->data_values[LOW_RATE_FEEDBACK] == 0)
-    ra->data_values[LOW_RATE_FEEDBACK] = new NR_TYPE[ra->nSamples[LOW_RATE_FEEDBACK]];
-  if (ra->data_values[HIGH_RATE_FEEDBACK] == 0)
-    ra->data_values[HIGH_RATE_FEEDBACK] = new NR_TYPE[ra->nSamples[HIGH_RATE_FEEDBACK]];
+static NR_TYPE (*lwcFn)(const NR_TYPE, const NR_TYPE, const NR_TYPE, const NR_TYPE, NR_TYPE, const int);
 
-  memset(ra->data_values[LOW_RATE_FEEDBACK], 0, ra->nSamples[LOW_RATE_FEEDBACK] * sizeof(NR_TYPE));
-  memset(ra->data_values[HIGH_RATE_FEEDBACK], 0, ra->nSamples[HIGH_RATE_FEEDBACK] * sizeof(NR_TYPE));
-}
-
-/* Add a sample to a running average.
- */
-static void add_sample(struct _running_average * ra, NR_TYPE new_value)
-{
-  if (++ra->buffIndex[FeedBack] >= ra->nSamples[FeedBack])
-    ra->buffIndex[FeedBack] = 0;
-
-  NR_TYPE prev_value = ra->data_values[FeedBack][ra->buffIndex[FeedBack]];
-  ra->data_values[FeedBack][ra->buffIndex[FeedBack]] = new_value;
-  ra->sum[FeedBack] -= prev_value;
-  ra->sum[FeedBack] += new_value;
-  ra->average[FeedBack] = ra->sum[FeedBack] / ra->nSamples[FeedBack];
-}
 
 /* -------------------------------------------------------------------- */
 void plwccInit(var_base *varp)
 {
   float *tmp;
+  int probe_index = 0;
+  char defaultName[256];
 
-  if ((tmp = GetDefaultsValue("TWIRE_PMS", varp->name)) == NULL)
+  if ( strstr(varp->name, "1") )
+    probe_index = 1;
+
+  varp->ProbeCount = probe_index;
+
+  if (cfg.Aircraft() == Config::HIAPER)
+    lwcFn = CooperLWC_GV;
+  else
+    lwcFn = CooperLWC_C130;
+
+  // Check for obsolete / deprecated Defaults name.
+  if ( probe_index == 0 && (tmp = GetDefaultsValue("TWIRE_PMS", varp->name)) )
+    tWire[probe_index] = tmp[0];
+
+  if ( probe_index == 1 && (tmp = GetDefaultsValue("TWIRE_PMS1", varp->name)) )
+    tWire[probe_index] = tmp[0];
+
+
+  sprintf(defaultName, "KING%d_WIRE_TEMP", probe_index);
+  if ((tmp = GetDefaultsValue(defaultName, varp->name)) == NULL)
   {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twire[0]);
-    LogMessage(buffer);
+    std::vector<float> values;
+    values.push_back(tWire[probe_index]);
+    AddToDefaults(varp->name, defaultName, values);
   }
   else
-    twire[0] = tmp[0];
+    tWire[probe_index] = tmp[0];
 
-  if ((tmp = GetDefaultsValue("TWIRE_TASFAC", varp->name)) == NULL)
+  sprintf(defaultName, "KING%d_WIRE_DIAM", probe_index);
+  if ((tmp = GetDefaultsValue(defaultName, varp->name)) == NULL)
   {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", tasFac[0]);
-    LogMessage(buffer);
+    std::vector<float> values;
+    values.push_back(dWire[probe_index]);
+    AddToDefaults(varp->name, defaultName, values);
   }
   else
-    tasFac[0] = tmp[0];
+    dWire[probe_index] = tmp[0];
 
-  if ((tmp = GetDefaultsValue("TWIRE_DIAM", varp->name)) == NULL)
+  sprintf(defaultName, "KING%d_WIRE_LEN", probe_index);
+  if ((tmp = GetDefaultsValue(defaultName, varp->name)) == NULL)
   {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twireDiam[0]);
-    LogMessage(buffer);
+    std::vector<float> values;
+    values.push_back(lWire[probe_index]);
+    AddToDefaults(varp->name, defaultName, values);
   }
   else
-    twireDiam[0] = tmp[0];
+    lWire[probe_index] = tmp[0];
 
-  if ((tmp = GetDefaultsValue("CLOUD_CONC_THRESHOLD", varp->name)) == NULL)
+  sprintf(defaultName, "KING%d_TAU_NUSSELT", probe_index);
+  if ((tmp = GetDefaultsValue(defaultName, varp->name)) == NULL)
   {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", cloud_conc_threshold);
-    LogMessage(buffer);
+    std::vector<float> values;
+    values.push_back(tau_Nu[probe_index]);
+    AddToDefaults(varp->name, defaultName, values);
+  }
+  else
+    tau_Nu[probe_index] = tmp[0];
+
+  if ((tmp = GetDefaultsValue("KING_CLOUD_CONC_THRESHOLD", varp->name)) == NULL)
+  {
+    std::vector<float> values;
+    values.push_back(cloud_conc_threshold);
+    AddToDefaults(varp->name, defaultName, values);
   }
   else
     cloud_conc_threshold = tmp[0];
-
-  memset(&lwc1, 0, sizeof(_running_average));
-  memset(&conc1, 0, sizeof(_running_average));
-  allocate(&lwc1, baseline_lwc_seconds);
-  allocate(&conc1, baseline_lwc_seconds);
 
 }	/* END PLWCCINIT */
 
 /* -------------------------------------------------------------------- */
-void plwcc1Init(var_base *varp)
-{
-  float *tmp;
-
-  if ((tmp = GetDefaultsValue("TWIRE_PMS1", varp->name)) == NULL)
-  {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twire[1]);
-    LogMessage(buffer);
-  }
-  else
-    twire[1] = tmp[0];
-
-  if ((tmp = GetDefaultsValue("TWIRE_TASFAC1", varp->name)) == NULL)
-  {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", tasFac[1]);
-    LogMessage(buffer);
-  }
-  else
-    tasFac[1] = tmp[0];
-
-  if ((tmp = GetDefaultsValue("TWIRE_DIAM1", varp->name)) == NULL)
-  {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", twireDiam[1]);
-    LogMessage(buffer);
-  }
-  else
-    twireDiam[1] = tmp[0];
-
-  if ((tmp = GetDefaultsValue("CLOUD_CONC_THRESHOLD", varp->name)) == NULL)
-  {
-    sprintf(buffer,"Value set to %f in AMLIB function plwccInit.\n", cloud_conc_threshold);
-    LogMessage(buffer);
-  }
-  else
-    cloud_conc_threshold = tmp[0];
-
-  memset(&lwc2, 0, sizeof(_running_average));
-  memset(&conc2, 0, sizeof(_running_average));
-  allocate(&lwc2, baseline_conc_seconds);
-  allocate(&conc2, baseline_conc_seconds);
-
-}	/* END PLWCC1INIT */
-
-/* -------------------------------------------------------------------- */
-void plwcgInit(var_base *varp)
-{
-  memset(&lwcg, 0, sizeof(_running_average));
-  memset(&concg, 0, sizeof(_running_average));
-  allocate(&lwcg, baseline_conc_seconds);
-  allocate(&concg, baseline_conc_seconds);
-
-}	/* END PLWCCGINIT */
-
-/* -------------------------------------------------------------------- */
 void splwcc(DERTBL *varp)
 {
-  NR_TYPE plwc, tasx, atx, psxc, concf;
+  NR_TYPE plwc, tasx, atx, psxc, concf = floatNAN;
   NR_TYPE plwcc;
 
-  plwc  = GetSample(varp, 0);
-  tasx  = GetSample(varp, 1);
-  atx   = GetSample(varp, 2);
-  psxc  = GetSample(varp, 3);
+  plwc  = GetSample(varp, 0);	// Raw Liquid Water measurement
+  tasx  = GetSample(varp, 1);	// True Airspeed
+  atx   = GetSample(varp, 2);	// Ambient Temperature
+  psxc  = GetSample(varp, 3);	// Static Pressure
  
+  if (plwc < 10.0 || isnan(plwc))
+  {
+    PutSample(varp, floatNAN);
+    return;
+  }
+
   if (tasx < 30.0)
   {
     PutSample(varp, 0.0);
     return;
   }
 
-  tasx *= tasFac[0];
-  plwcc = kinglwcc(plwc, tasx, atx, psxc, twire[0], twireDiam[0]);
-
-  /* Baseline to a another cloud instrument to remove drift.
-   */
   if (varp->ndep >= 5)
-  {
-    concf = GetSample(varp, 4);
+    concf = GetSample(varp, 4);	// FSSP (preferably) concntration
 
-    if (!isnan(concf))
-      add_sample(&conc1, concf);
+  plwcc = (*lwcFn)(plwc, tasx, atx, psxc, concf, varp->ProbeCount);
 
-    if (conc1.average[FeedBack] < cloud_conc_threshold && !isnan(plwcc))
-      add_sample(&lwc1, -plwcc);
-
-    plwcc += lwc1.average[FeedBack];
-  }
+  if (plwcc < 0.0)
+    plwcc = 0.0;
 
   PutSample(varp, plwcc);
 }
 
+
 /* -------------------------------------------------------------------- */
-void splwcc1(DERTBL *varp)
+NR_TYPE CooperLWC_GV(const NR_TYPE plwc, const NR_TYPE tasx, const NR_TYPE atx,
+	const NR_TYPE psxc, const NR_TYPE concf, const int indx)
 {
-  NR_TYPE plwc, tasx, atx, psxc, concf;
-  NR_TYPE plwcc;
+  static NR_TYPE Nu_a0 = 1.8679, Nu_b0 = 0.13506, Nu_c0 = 0.13289;
+  static const double Nu_a1 = 0.3433589;
+  static const double Nu_b1 = 0.638318;
+  static const double Nu_c1 = 0.3823;
 
-  plwc  = GetSample(varp, 0);
-  tasx  = GetSample(varp, 1);
-  atx   = GetSample(varp, 2);
-  psxc  = GetSample(varp, 3);
- 
-  if (tasx < 30.0)
+  NR_TYPE Nu;
+  NR_TYPE tm = (atx + tWire[indx]) / 2.0;		// Mean T
+  NR_TYPE cond = cond0 + cond1 * tm;			// thermal conductivity, air
+  NR_TYPE visc = visc0 + visc1 * tm;			// Viscosity of air
+  NR_TYPE dens = 100.0 * psxc / (Rd * (tm + Kelvin));	// air density
+  NR_TYPE Re = dens * tasx * dWire[indx] / visc;	// Reynolds number
+
+  if (isnan(Re))
+    return floatNAN;
+
+  // Update Nusselt-number coefficients
+  if (concf < cloud_conc_threshold && !isnan(concf))
   {
-    PutSample(varp, 0.0);
-    return;
+    Nu = plwc / (M_PI * lWire[indx] * cond * (tWire[indx] - atx));
+    if (tasx < 150.0)
+      { Nu_c0 += (Nu / pow(Re, Nu_c1) - Nu_c0) / tau_Nu[indx]; }
+    else
+    if (Re < 7244.0)
+      { Nu_a0 += (Nu / pow(Re, Nu_a1) - Nu_a0) / tau_Nu[indx]; }
+    else
+      { Nu_b0 += (Nu / pow(Re, Nu_b1) - Nu_b0) / tau_Nu[indx]; }
   }
-  
-  tasx *= tasFac[1];
-  plwcc = kinglwcc(plwc, tasx, atx, psxc, twire[1], twireDiam[1]);
 
-  /* Baseline to a another cloud instrument to remove drift.
-   */
-  if (varp->ndep >= 5)
-  { 
-    concf = GetSample(varp, 4);
-    
-    if (!isnan(concf))
-      add_sample(&conc2, concf);
+  if (tasx < 150.0)
+    { Nu = Nu_c0 * pow(Re, Nu_c1); }
+  else
+  if (Re < 7244.0)
+    { Nu = Nu_a0 * pow(Re, Nu_a1); }
+  else
+    { Nu = Nu_b0 * pow(Re, Nu_b1); }
 
-    if (conc2.average[FeedBack] < cloud_conc_threshold && !isnan(plwcc))
-      add_sample(&lwc2, -plwcc);
+  // Power required in dry air.
+  NR_TYPE Pdry = M_PI * lWire[indx] * cond * (tWire[indx] - atx) * Nu;
+  NR_TYPE xp = log10(psxc);
 
-    plwcc += lwc2.average[FeedBack];
-  }
-  
-  PutSample(varp, plwcc);
+  NR_TYPE Tbp = pow(10.0, (bp[0]+xp*(bp[1]+xp*(bp[2]+xp*bp[3]))));
+
+  // Liquid water content, convreted from MKS to g/m^3
+  return 1000.0 * (plwc - Pdry) / (lWire[indx] * dWire[indx] * tasx * ((Lv0+Lv*Tbp) + Cw * (Tbp - atx)));
 }
 
 /* -------------------------------------------------------------------- */
-void splwcg(DERTBL *varp)
+NR_TYPE CooperLWC_C130(	const NR_TYPE plwc, const NR_TYPE tasx, const NR_TYPE atx,
+			const NR_TYPE psxc, const NR_TYPE concf, const int indx)
 {
-  NR_TYPE plwc, concf;
+  static NR_TYPE Nu_a0 = 0.1179;
+  static const NR_TYPE Nu_a1 = 0.67452;
 
-  plwc  = GetSample(varp, 0);
-  concf = GetSample(varp, 1);
- 
-  /* Baseline to a another cloud instrument to remove drift.
-   */
-  if (!isnan(concf))
-    add_sample(&concg, concf);
+  NR_TYPE Nu;
+  NR_TYPE tm = (atx + tWire[indx]) / 2.0;		// Mean T
+  NR_TYPE cond = cond0 + cond1 * tm;			// thermal conductivity, air
+  NR_TYPE visc = visc0 + visc1 * tm;			// Viscosity of air
+  NR_TYPE dens = 100.0 * psxc / (Rd * (tm + Kelvin));	// air density
+  NR_TYPE Re = dens * tasx * dWire[indx] / visc;	// Reynolds number
 
-  if (concg.average[FeedBack] < cloud_conc_threshold && !isnan(plwc))
-    add_sample(&lwcg, -plwc);
+  if (isnan(Re))
+    return floatNAN;
 
-  plwc += lwcg.average[FeedBack];
-  
-  PutSample(varp, plwc);
+  // Update Nusselt-number coefficients
+  if (concf < cloud_conc_threshold && !isnan(concf))
+  {
+    Nu = plwc / (M_PI * lWire[indx] * cond * (tWire[indx] - atx));
+    Nu_a0 += (Nu / pow(Re, Nu_a1) - Nu_a0) / tau_Nu[indx];
+  }
+
+  Nu = Nu_a0 * pow(Re, Nu_a1);
+
+  // Power required in dry air.
+  NR_TYPE Pdry = M_PI * lWire[indx] * cond * (tWire[indx] - atx) * Nu;
+  NR_TYPE xp = log10(psxc);
+
+  NR_TYPE Tbp = pow(10.0, (bp[0]+xp*(bp[1]+xp*(bp[2]+xp*bp[3]))));
+
+  // Liquid water content, convreted from MKS to g/m^3
+  return 1000.0 * (plwc - Pdry) / (lWire[indx] * dWire[indx] * tasx * ((Lv0+Lv*Tbp) + Cw * (Tbp - atx)));
 }
