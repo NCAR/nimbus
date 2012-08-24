1b676235e20b812d165e625013658850e9816069
Tom Baltzer <tbaltzer@ucar.edu>
2012-08-24 17:14:08 +0000

    Break out the proxy as it's own entity.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6009 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ instruments/instctrl/proxy/proxy.h
@@ -0,0 +1,55 @@
+#ifndef PROXY_H
+#define PROXY_H
+
+#include <QtGui>
+#include <QtNetwork>
+
+/**
+ * The proxy client application for the remote instrument control project. Establishes
+ * a secure connection with the server application on eol-rt-data over port 80 and sets
+ * up a QUdpSocket to read datagrams sent from the local machine to a chosen port number.
+ * The datagrams are sent to the server through the secure connection. Status datagrams
+ * that match the entered instrument key are sent back to the Proxy from the server.
+ */
+
+class Proxy : public QDialog
+{
+	Q_OBJECT;
+
+public:
+	Proxy();
+
+private slots:
+	void connectToServer();
+	void clientConnected();
+	void clientEncrypted();
+	void clientDisconnected();
+	void readMode();
+	void sendMode();
+	void sendToServer();
+	void switchHostMode();
+	void quitSession();
+	void displayError(QAbstractSocket::SocketError socketError);
+	void displayError(const QList<QSslError> & errors);
+
+private:
+	QUdpSocket *udpInput_;
+	QSslSocket *sslSocket_;
+
+	QLabel *status_;
+	QLabel *connection_;
+	QLabel *portLabel_;
+	QLabel *clientLabel_;
+	QLabel *addressLabel_;
+
+	QPushButton *connectButton_;
+	QPushButton *switchButton_;
+	QPushButton *quitButton_;
+
+	QComboBox *port_;
+	QSet<QString> *uniquePorts_;
+	QLineEdit *address_;
+	QLineEdit *clientName_;
+};
+
+#endif /*PROXY_H*/
