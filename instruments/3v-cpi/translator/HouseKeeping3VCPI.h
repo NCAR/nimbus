1133e8527f1539b4a4f076c4a6e6f980d22ca501
Chris Webster <cjw@ucar.edu>
2012-11-09 21:39:32 +0000

    Add decoding of some of the housekeeping parameters into engineering units.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6112 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/3v-cpi/translator/HouseKeeping3VCPI.h
+++ instruments/3v-cpi/translator/HouseKeeping3VCPI.h
@@ -4,21 +4,56 @@
 namespace sp
 {
 
-
 	struct HouseKeeping3VCPI: public Packet
 	{
-		word		_data[83];
-	};
+		word	_data[83];
 
+		float true_airspeed()
+		{
+			float tas;
+			short *s = (short *)&_data[74];
+			short *d = (short *)&tas;
+			d[0] = s[1];
+			d[1] = s[0];
+			return tas;
+		}
+
+		float relative_humidity()
+		{
+			float rh = 0.002515185 * _data[27] - 28.02198;
+			return rh;
+		}
+
+		/// 48 bit timing word / clock counter.
+		long long fclk()
+		{
+			long long fclk = 0;
+			short *s = (short *)&_data[71];
+			fclk += s[0]; fclk <<= 16;
+			fclk += s[1]; fclk <<= 16;
+			fclk += s[2];
+			return fclk;
+		}
+
+		/// First 26 shorts of the housekeeping packet are all temperatures.
+		float temperature(int i)
+		{
+			if (i < 1 && i > 25)
+				return NAN;
+
+			float rt = 6.5536e9 * (1.0 - (float)_data[i]/65536.0) / (5.0*_data[i]);
+			float t = -273.15 + pow(1.1117024e-3 + 237.02702e-6*log(rt)+75.78814e-9*pow(log(rt),3.0), -1.0);
+			return t;
+		}
+	};
 
 
 	template<class T>
 	inline T&	operator >> (T& reader, HouseKeeping3VCPI& in)
 	{
-		reader.read(reinterpret_cast<byte*>(&in._data[0]),sizeof(in._data));
+		reader.read(reinterpret_cast<byte *>(&in._data[0]), sizeof(in._data));
 		in.PacketID = in._data[0];
 		return reader;
 	}
-
 
 }
