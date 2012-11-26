eea995cd91aae525af025c50bacc01832d3d2210
Charles Martin <martinc@ucar.edu>
2012-11-26 22:18:14 +0000

    Add port numbers to switch and proxy command lines (for now).
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6145 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/instctrl/Proxy/main.cpp
+++ instruments/instctrl/Proxy/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "Proxy.h"
 
 int main(int argc, char** argv)
 {
-    if (argc < 4) {
-        std::cout << "usage: " << argv[0] << " clientID client_key_file client_cert_file [extra_cert_file ... extra_cert_file]" << std::endl;
+    if (argc < 6) {
+        std::cout << "usage: " << argv[0] << " udp_port switch_port clientID client_key_file client_cert_file [extra_cert_file ... extra_cert_file]" << std::endl;
         exit(1);
     }
 
-    QCoreApplication app(argc, argv);
+	std::stringstream s;
+	s << argv[1];
+	int udpport;
+	s >> udpport;
+	s.clear();
+	s << argv[2];
+	int switchport;
+	s >> switchport;
 
-    std::string clientID(argv[1]);
-    std::string clientKeyPath(argv[2]);
-    std::string clientCertPath(argv[3]);
+    std::string clientID(argv[3]);
+    std::string clientKeyPath(argv[4]);
+    std::string clientCertPath(argv[5]);
 
     std::vector<std::string> caDatabase;
     // add the client certificate to the CA database
@@ -24,12 +32,14 @@ int main(int argc, char** argv)
         caDatabase.push_back(argv[i]);
     }
 
+    QCoreApplication app(argc, argv);
+
     Proxy proxy(
-    		12346,
+    		udpport,
     		clientKeyPath,
     		clientCertPath,
     		"localhost",
-    		12345,
+    		switchport,
     		caDatabase,
     		clientID);
 
--- instruments/instctrl/Switch/main.cpp
+++ instruments/instctrl/Switch/main.cpp
@@ -1,20 +1,24 @@
 #include <QtCore>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include "Switch.h"
 
 int main(int  argc, char** argv)
 {
 
-	if (argc < 3) {
-		std::cout << "usage: " << argv[0] << " private_key_file certificate_file [extra_cert_file ... extra_certFile]" << std::endl;
+	if (argc < 4) {
+		std::cout << "usage: " << argv[0] << " port private_key_file certificate_file [extra_cert_file ... extra_certFile]" << std::endl;
 		exit(1);
 	}
 
-	QCoreApplication app(argc, argv);
+	std::stringstream s;
+	s << argv[1];
+	int port;
+	s >> port;
 
-	std::string serverKey(argv[1]);
-	std::string serverCert(argv[2]);
+	std::string serverKey(argv[2]);
+	std::string serverCert(argv[3]);
 
 	std::vector<std::string> caDatabase;
 	// add the server certificate to the CA database
@@ -25,7 +29,9 @@ int main(int  argc, char** argv)
 		caDatabase.push_back(argv[i]);
 	}
 
-	Switch swtch(serverKey, serverCert, 12345, caDatabase);
+	QCoreApplication app(argc, argv);
+
+	Switch swtch(serverKey, serverCert, port, caDatabase);
 
 	return app.exec();
 
