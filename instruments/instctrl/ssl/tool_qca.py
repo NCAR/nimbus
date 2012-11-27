91b47d71e4fc709ac6e1b37dca5020c002976649
Charles Martin <martinc@ucar.edu>
2012-11-27 20:19:02 +0000

    Fix QCA references on Linux.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6149 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/instctrl/ssl/tool_qca.py
+++ instruments/instctrl/ssl/tool_qca.py
@@ -6,5 +6,11 @@ def qca(env):
 		env.AppendUnique(FRAMEWORKPATH=['/usr/local/lib',])
 		env.AppendUnique(FRAMEWORKS=['qca'])
 		env.AppendUnique(CPPPATH=['/usr/local/lib/qca.framework/Headers'])
-    
+	elif env['PLATFORM'] in ['posix','linux', 'linux2']:
+		env.AppendUnique(CPPPATH=['/usr/local/include/QtCrypto'])
+		env.AppendUnique(LIBPATH=['/usr/local/lib'])
+		env.AppendUnique(LIBS=['qca'])
+	elif env['PLATFORM'] == 'win32':
+		pass
+
 Export('qca')
