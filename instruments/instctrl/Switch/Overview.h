61b3e90bc1e483e637d799273650452e57d1997b
Charles Martin <martinc@ucar.edu>
2012-12-31 18:03:25 +0000

    Add more documentation on configuration.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6213 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/instctrl/Switch/Overview.h
+++ instruments/instctrl/Switch/Overview.h
@@ -4,7 +4,7 @@
 /// - Instrument - The device which is being remotely monitored and controlled. The command and
 /// status information is transmitted to and from the instruement using UDP datagrams.
 /// - User Control - A user control application, which sends and receives the
-/// datagrams from the instrument.
+/// datagrams from the instrument. Usually referred to as the 'user'.
 /// - Proxy - An application or component which can intercept and relay the instrument
 /// datagrams, for networking and security purposes.
 /// - Switch - An application which is deployed in pairs to consolidate messages into
@@ -12,8 +12,8 @@
 /// proxies, aggregate or fan out messages to/from the instruments and user controls.
 ///
 /// A proxy may be remote, and communicate with the switch via SSL. Alternatively, it may
-/// be embedded in a switch. The latter allows a a switch and its proxies to be deployed as
-/// a single application in cases where SSL is not required.
+/// be embedded within a switch. The latter allows a switch and its proxies to be deployed as
+/// a single application in cases where SSL is not required; e.g. on the aircraft.
 ///
 /// Because SSL is implemented with network sockets, the switch can contain an SSL server (SslServer),
 /// to manage the SSL connections to remote proxies. For consistency, a corresponding server
@@ -21,11 +21,11 @@
 ///
 /// The typical configuration will have one switch on a ground server, taking SSL connections from
 /// remote proxies. This switch will communicate with an aircraft based switch. The aircraft switch
-/// contains embedded proxies.However, it is feasible to any combination of remote proxies, SSL
+/// contains embedded proxies. However, it is feasible to any combination of remote proxies, SSL
 /// proxy switches, and embedded proxy switches.
 ///
 /// @section RICConfiguration Configuration
-/// The switch and proxy applications are configured via .ini configuration files. The location
+/// The switch and proxy applications are configured via configuration files. The location
 /// of the configuration file can be specified on the command line. If the path is
 /// not so specified, it defaults to the standard QSettings location (~/.config on Linux and OSX).
 /// It's a bad idea to use the default location. If the configuration file does not exist when
@@ -34,19 +34,19 @@
 ///
 /// The configuration file format is based on the Qt QSettings class, which implements a fairly
 /// generic .ini format. The format is pretty straightforward, but the QSettings
-/// documentation can provide some illumination. The configuration files can be edited by hand,
+/// documentation can provide some illumination. The configuration files may be edited by hand,
 /// but it is easy to make subtle mistakes. The files are really intended to be manipulated
 /// by configuration editor applications.
 ///
-/// @subsection RICGroundSwitchConfig Switch Configuration
+/// @subsection RICSwitchConfig Switch Configuration
 /// The switch can be configured in either an SSL proxy mode or an embedded proxy mode,
 /// as set byt the SslProxy parameter. There are some parameters, as described below, which are not
 /// relevant to a particular switch mode.
 /// @code
 ///[General]
-/// SwitchProxyPort=20001
-/// SwitchKeyFile=../ssl/test/test_certs/server.key
-/// SwitchCertFile=../ssl/test/test_certs/server.crt
+/// SSLProxyPort=20001
+/// SwitchSSLKeyFile=../ssl/test/test_certs/server.key
+/// SwitchSSLCertFile=../ssl/test/test_certs/server.crt
 /// SwitchLocalPort=10001
 /// SwitchRemotePort=10002
 /// SwitchRemoteIP=127.0.0.1
@@ -54,8 +54,8 @@
 /// SslProxy=true
 ///
 /// [ClientCerts]
-/// 1\ClientCertFile=../ssl/test/test_certs/EOL_AVAPS.crt
-/// 2\ClientCertFile=../ssl/test/test_certs/EOL_HSRL.crt
+/// 1\ClientSSLCertFile=../ssl/test/test_certs/EOL_AVAPS.crt
+/// 2\ClientSSLCertFile=../ssl/test/test_certs/EOL_HSRL.crt
 /// size=2
 ///
 /// [Instruments]
@@ -63,29 +63,29 @@
 /// 1\InstrumentFile=./HSRL.ini
 /// size=2
 /// @endcode
-/// @param SwitchProxyPort The port that will be used for SSL connections to SSL proxies. This parameter is
+/// @param [General]\SSLProxyPort The port that will be used for SSL connections to SSL proxies. This parameter is
 /// ignored  when the switch is configured for embedded proxy use (SslProxy=false).
-/// @param SwitchKeyFile The file containing the private key for the switch SSL certificate.  This parameter is
+/// @param [General]\SwitchSSLKeyFile The file containing the private key for the switch SSL certificate.  This parameter is
 /// ignored  when the switch is configured for embedded proxy use (SslProxy=false).
-/// @param SwitchCertFile The file containing the public SSL certificate for the switch. This will be transmitted to the SSL proxies
+/// @param [General]\SwitchSSLCertFile The file containing the public SSL certificate for the switch. This will be transmitted to the SSL proxies
 /// during the SSL handshake. If the sertificate is self igned, the client must already have the certificate so that
 /// the self-signed certificate is accepted.   This parameter is
 /// ignored  when the switch is configured for embedded proxy use (SslProxy=false).
-/// @param SwitchLocalPort The local port for the switch-to-switch datagram communication.
-/// @param SwitchRemotePort The remote port for the switch-to-switch datagram communication.
-/// @param SwitchRemoteIP The IP or host name of the remote switch for the switch-to-switch datagram communication.
-/// @param SwitchCipherKey The path to a file containing the symmetric encryption key for the switch-to-switch
+/// @param [General]\SwitchLocalPort The local port for the switch-to-switch datagram communication.
+/// @param [General]\SwitchRemotePort The remote port for the switch-to-switch datagram communication.
+/// @param [General]\SwitchRemoteIP The IP or host name of the remote switch for the switch-to-switch datagram communication.
+/// @param [General]\SwitchCipherKey The path to a file containing the symmetric encryption key for the switch-to-switch
 /// datagram communication.
-/// @param SslProxy Set to "true" if the switch connects to SSL proxies. Set to false when configured for embedded proxy
+/// @param [General]\SslProxy Set to "true" if the switch connects to SSL proxies. Set to false when configured for embedded proxy
 /// usage.
-/// @param [ClientCerts]/ClientCertFile The path to a file containing an authorized SSL proxy client. There will be one entry for
+/// @param [ClientCerts]\ClientSSLCertFile The path to a file containing an authorized SSL proxy client. There will be one entry for
 /// each authorized proxy. This same file will be given to the user who will be running that proxy. A useful
 /// convention is to name the certificate file after the user and instrument. This is not
 /// used and is ignored in an embedded proxy switch configuration.
-/// @param [ClientCerts]/size Size specifies the number of entries in the ClientCerts array.
-/// @param [Instruments]/InstrumentFile The path to a file containing an instrument definition.
+/// @param [ClientCerts]\size Size specifies the number of entries in the ClientCerts array.
+/// @param [Instruments]\InstrumentFile The path to a file containing an instrument definition.
 /// See below. This is not used and is ignored in an SSL proxy switch configuration.
-/// @param [Instruments]/size Size specifies the number of entries in the Instruments array.
+/// @param [Instruments]\size Size specifies the number of entries in the Instruments array.
 ///
 /// @subsection RICProxyConfiguration SSL Proxy Configuration
 /// The SSL proxy application configuration file has the following format. Like the embedded proxy switch
@@ -96,23 +96,23 @@
 /// ProxyID=EOL
 /// ProxyKeyFile=../ssl/test/test_certs/client.key
 /// ProxyCertFile=../ssl/test/test_certs/client.crt
-/// SwitchProxyPort=20001
+/// SSLProxyPort=20001
 /// SwitchHostName=localhost
-/// SwitchCertFile=../ssl/test/test_certs/server.crt
+/// SwitchSSLCertFile=../ssl/test/test_certs/server.crt
 /// InstrumentFile=./AVAPS.ini
 /// @endcode
-/// @param ProxyID An identifier for this proxy. It is one of the authentication tokens that
+/// @param [General]\ProxyID An identifier for this proxy. It is one of the authentication tokens that
 /// enables communication with the switch.
-/// @param ProxyKeyFile The private key for the proxy.
-/// @param ProxyCertFile The public certificate that the proxy presents to the switch.
-/// @param SwitchProxyPort The port on the switch used for the SSL communications.
-/// @param SwitchHostName The host name or IP address of the switch.
-/// @param SwitchCertFile The path to a file containing the switch public certificate. The proxy
+/// @param [General]\ProxyKeyFile The private key for the proxy.
+/// @param [General]\ProxyCertFile The public certificate that the proxy presents to the switch.
+/// @param [General]\SSLProxyPort The port on the switch used for the SSL communications.
+/// @param [General]\SwitchHostName The host name or IP address of the switch.
+/// @param [General]\SwitchSSLCertFile The path to a file containing the switch public certificate. The proxy
 /// must have a copy of this certificate in order to accept the self-signed certificate from the switch.
-/// @param InstrumentFile The path to a configuration file describing the instrument that this proxy
+/// @param [General]\InstrumentFile The path to a configuration file describing the instrument that this proxy
 /// will be representing. See below.
 ///
-/// @subsection RICInstrumentConfiguration
+/// @subsection RICInstConfiguration Instrument Configuration
 /// The instrument configuration defines the instrument and user IP addresses for
 /// instrument interaction, as well as the message identifiers and processing
 /// parameters. The same instrument definition format is used by the proxy, SSL
@@ -122,28 +122,41 @@
 /// [General]
 /// InstName=AVAPS-GV
 /// InstIncomingPort=10120
-/// InstDestPort=10121
 /// InstHostName=127.0.0.1
+/// InstDestPort=10121
 ///
 /// [Messages]
-/// 1\Broadcast=true
 /// 1\ID=AVAPS-GV
+/// 1\Broadcast=true
 /// 1\RateLimit=0
 ///
-/// 2\Broadcast=false
 /// 2\ID=AVAPS-ENGR-GV
+/// 2\Broadcast=false
 /// 2\RateLimit=5
 ///
-/// 3\Broadcast=false
 /// 3\ID=AVAPS-LOG
+/// 3\Broadcast=false
 /// 3\RateLimit=5
 ///
-/// 4\Broadcast=false
 /// 4\ID=AGS
+/// 4\Broadcast=false
 /// 4\RateLimit=1
 ///
 /// size=4
 /// @endcode
+/// @param [General]\InstName The instrument name.
+/// @param [General]\InstIncomingPort The port to listen on for messages sent by the instrument or user. Used only by the proxies.
+/// @param [General]\InstHostName The IP or host name of the instrument or user. Instrument and user messages from
+/// the switch will be sent to this address. Used only by the proxies.
+/// @param [General]\InstDestPort The port number of the instrument or user. Instrument and user messages from the switch will be sent
+/// to this port. Used only by the proxies.
+/// @param [Messages]\1\ID An allowed message type. The ID will be extracted from the message and verified
+/// against the allowed IDs. Used by the proxies and switches.
+/// @param [Messages]\1\Broadcast If set, outgoing messages will be broadcast. Used only by the proxies.
+/// @param [Messages]\1\RateLimit A rate limit for this message, in seconds. Fractional values are allowed.
+/// Only one message will be allowed through the switch during this timer interval. Used only by the switches.
+/// @param [Messages]size The number of message definitions in the [Messages] array.
+///
 /// @section RICMessageHandling RIC Message Handling
 ///
 /// The following sections presents metacode describing the message flow from a user control program
