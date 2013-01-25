31ddaa13950300c9422eb4f04fe82762fc7d844e
Charles Martin <martinc@ucar.edu>
2013-01-25 16:18:53 +0000

    Generalize the top level error reporting of RIC proxy, for fatal startup errors.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6321 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/instctrl/proxy/main.cpp
+++ instruments/instctrl/proxy/main.cpp
@@ -59,122 +59,139 @@ parseCommandLine(int argc, char** argv,
 /// one user/instrument is supported.
 int main(int argc, char** argv)
 {
-	// Process command line options
-	std::string configFile;
-	parseCommandLine(argc, argv, configFile);
+    try {
+		// Process command line options
+		std::string configFile;
+		parseCommandLine(argc, argv, configFile);
 
-	// Get the configuration. Fetch from the specified file
-	// if it was provided, otherwise use the default location.
-	QtConfig* config;
-	if (configFile.size()) {
-		config = new QtConfig(configFile);
-	} else {
-		config = new QtConfig("NCAR", "Proxy");
-	}
+		// Get the configuration. Fetch from the specified file
+		// if it was provided, otherwise use the default location.
+		QtConfig* config;
+		if (configFile.size()) {
+			config = new QtConfig(configFile);
+		} else {
+			config = new QtConfig("NCAR", "Proxy");
+		}
 
+		// Get configuration values. Default values will be created in the
+		// configuration file if they don't exist, so that running the program
+		// for the first time will create a configuration template.
+		std::string proxyID        = config->getString("ProxyID",         "PROXY");
+		std::string proxyKeyFile    (config->getString("ProxyKeyFile",    "./proxy.key"));
+		std::string proxyCertFile   (config->getString("ProxyCertFile",   "./proxy.crt"));
+		int switchProxyPort        = config->getInt   ("SSLProxyPort", 0);
+		std::string switchHostName = config->getString("SwitchHostName",  "127.0.0.1");
+		std::string switchCertFile = config->getString("SwitchSSLCertFile",  "./switch.crt");
 
-	// Get configuration values. Default values will be created in the
-	// configuration file if they don't exist, so that running the program
-	// for the first time will create a configuration template.
-	std::string proxyID        = config->getString("ProxyID",         "PROXY");
-	std::string proxyKeyFile    (config->getString("ProxyKeyFile",    "./proxy.key"));
-	std::string proxyCertFile   (config->getString("ProxyCertFile",   "./proxy.crt"));
-	int switchProxyPort        = config->getInt   ("SSLProxyPort", 0);
-	std::string switchHostName = config->getString("SwitchHostName",  "127.0.0.1");
-	std::string switchCertFile = config->getString("SwitchSSLCertFile",  "./switch.crt");
+		// Get the instrument definition file. The proxy program will currently only support
+		// one instrument. This could be changed if necessary.
+		std::string instrumentFile;
+		instrumentFile = config->getString("InstrumentFile", "");
 
-	// Get the instrument definition file. The proxy program will currently only support
-	// one instrument. This could be changed if necessary.
-	std::string instrumentFile;
-	instrumentFile = config->getString("InstrumentFile", "");
+		// Get the configuration for the instrument.
+		InstConfig instConfig(instrumentFile);
 
-	// Get the configuration for the instrument.
-	InstConfig instConfig(instrumentFile);
+		// Get the messages for this instrument
+		std::vector<InstConfig::MessageInfo> instMsg = instConfig.messages();
 
-	// Get the messages for this instrument
-	std::vector<InstConfig::MessageInfo> instMsg = instConfig.messages();
+		// Build the message list for this proxy
+		std::map<std::string, SslProxy::InstMsgInfo> messages;
+		for (int i = 0; i < instMsg.size(); i++) {
+			// Configuration for one message
+			SslProxy::InstMsgInfo msg;
+			msg._instName              = instConfig.instrumentName();
+			msg._incomingPort          = instConfig.incomingPort();
+			msg._destPort              = instConfig.destPort();
+			msg._destIP                = instConfig.destIP();
+			msg._msgId                 = instMsg[i].msgID;
+			msg._broadcast             = instMsg[i].broadcast;
 
-	// Build the message list for this proxy
-    std::map<std::string, SslProxy::InstMsgInfo> messages;
-	for (int i = 0; i < instMsg.size(); i++) {
-		// Configuration for one message
-		SslProxy::InstMsgInfo msg;
-		msg._instName              = instConfig.instrumentName();
-		msg._incomingPort          = instConfig.incomingPort();
-		msg._destPort              = instConfig.destPort();
-		msg._destIP                = instConfig.destIP();
-		msg._msgId                 = instMsg[i].msgID;
-		msg._broadcast             = instMsg[i].broadcast;
+			// save the message info
+			messages[msg._msgId] = msg;
+		}
 
-		// save the message info
-		messages[msg._msgId] = msg;
-	}
+		// If the port number is 0, it indicates that the user has not configured
+		// the application yet. We wait until this point so that all of the default values
+		// will have been added to the configuration file. Force them to take a stab at
+		// configuration.
+		if (switchProxyPort == 0) {
+			std::cout << "Please create a usable configuration by editing " << config->fileName() << std::endl;
+			exit(1);
+		}
 
-    // If the port number is 0, it indicates that the user has not configured
-	// the application yet. We wait until this point so that all of the default values
-	// will have been added to the configuration file. Force them to take a stab at
-	// configuration.
-	if (switchProxyPort == 0) {
-		std::cout << "Please create a usable configuration by editing " << config->fileName() << std::endl;
-		exit(1);
-	}
+		// Create the Qt application
+		QApplication app(argc, argv);
 
-	// Create the Qt application
-	QApplication app(argc, argv);
+		// Get the certs
+		QList<QSslCertificate> certlist;
 
-	// Get the certs
-	QList<QSslCertificate> certlist;
+		certlist = QSslCertificate::fromPath(proxyCertFile.c_str());
+		if (certlist.size() == 0) {
+			std::string errmsg;
+			errmsg += "A valid certificate was not found at ";
+			errmsg += proxyCertFile;
+			throw errmsg;
+		}
+		QSslCertificate proxyCert(certlist[0]);
 
-	certlist = QSslCertificate::fromPath(proxyCertFile.c_str());
-	if (certlist.size() == 0) {
-		std::cerr << "A valid certificate was not found at " << proxyCertFile << std::endl;
-		exit(1);
-	}
-	QSslCertificate proxyCert(certlist[0]);
+		certlist = QSslCertificate::fromPath(switchCertFile.c_str());
+		if (certlist.size() == 0) {
+			std::string errmsg;
+			errmsg += "A valid certificate was not found at ";
+			errmsg += switchCertFile;
+			throw errmsg;
+		}
+		QSslCertificate switchCert(certlist[0]);
 
-	certlist = QSslCertificate::fromPath(switchCertFile.c_str());
-	if (certlist.size() == 0) {
-		std::cerr << "A valid certificate was not found at " << switchCertFile << std::endl;
-		exit(1);
-	}
-	QSslCertificate switchCert(certlist[0]);
+		// add the server certificate to the CA database
+		std::vector<QSslCertificate> extraCerts;
+		extraCerts.push_back(switchCert);
 
-	// add the server certificate to the CA database
-	std::vector<QSslCertificate> extraCerts;
-	extraCerts.push_back(switchCert);
+			// Create the SSL proxy. It will wait to connect with the switch,
+		// until requested via a call connectToServer().
+		SslProxy sslProxy(
+				proxyID,
+				proxyKeyFile,
+				proxyCert,
+				switchHostName,
+				switchProxyPort,
+				extraCerts,
+				messages);
 
-    // Create the SSL proxy. It will wait to connect with the switch,
-	// until requested via a call connectToServer().
-	SslProxy sslProxy(
-			proxyID,
-    		proxyKeyFile,
-    		proxyCert,
-    		switchHostName,
-    		switchProxyPort,
-    		extraCerts,
-    		messages);
+		// Create the Proxy user interface.
+		ProxyMainWindow proxyMainWindow(sslProxy, 0);
+		QString title("Proxy-");
+		title += SVNREVISION;
+		proxyMainWindow.setWindowTitle(title);
 
-	// Create the Proxy user interface.
-	ProxyMainWindow proxyMainWindow(sslProxy, 0);
-	QString title("Proxy-");
-	title += SVNREVISION;
-	proxyMainWindow.setWindowTitle(title);
+		// Connect signals from the interface to the SSL proxy.
+		proxyMainWindow.connect(&proxyMainWindow, SIGNAL(connectToServer()),
+				&sslProxy, SLOT(connectToServer()));
+		proxyMainWindow.connect(&proxyMainWindow, SIGNAL(disconnectFromServer()),
+				&sslProxy, SLOT(disconnectFromServer()));
 
-	// Connect signals from the interface to the SSL proxy.
-	proxyMainWindow.connect(&proxyMainWindow, SIGNAL(connectToServer()),
-			&sslProxy, SLOT(connectToServer()));
-	proxyMainWindow.connect(&proxyMainWindow, SIGNAL(disconnectFromServer()),
-			&sslProxy, SLOT(disconnectFromServer()));
+		// Connect the signals from the SSL proxy to the interface.
+		sslProxy.connect(&sslProxy, SIGNAL(switchMessage(std::string, bool)),
+				&proxyMainWindow, SLOT(switchMessageSlot(std::string, bool)));
+		sslProxy.connect(&sslProxy, SIGNAL(userMessage(std::string, bool)),
+				&proxyMainWindow, SLOT(userMessageSlot(std::string, bool)));
 
-	// Connect the signals from the SSL proxy to the interface.
-	sslProxy.connect(&sslProxy, SIGNAL(switchMessage(std::string, bool)),
-			&proxyMainWindow, SLOT(switchMessageSlot(std::string, bool)));
-	sslProxy.connect(&sslProxy, SIGNAL(userMessage(std::string, bool)),
-			&proxyMainWindow, SLOT(userMessageSlot(std::string, bool)));
+		// Show the user interface
+		proxyMainWindow.show();
 
-	// Show the user interface
-	proxyMainWindow.show();
+		// Hop on the merry-go-round.
+		return app.exec();
+    }
 
-    // Hop on the merry-go-round.
-	return app.exec();
+    catch (std::string& errmsg) {
+
+		// An error occurred. Pop up an error message box.
+
+        // Create the Qt application
+		QApplication app(argc, argv);
+
+		QMessageBox::critical(0, "RIC Proxy Error", errmsg.c_str(),  QMessageBox::Close);
+		return 1;
+
+    }
 }
