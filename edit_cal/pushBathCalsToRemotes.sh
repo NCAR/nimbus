e933f68bea4c031532aeb391c0f7a86ab51ad352
John Wasinger <wasinger@ucar.edu>
2013-01-30 05:09:27 +0000

    Test ping servers before attempting to connect.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6511 640d5228-2204-0410-8d71-8f46fa6850b4

--- edit_cal/pushBathCalsToRemotes.sh
+++ edit_cal/pushBathCalsToRemotes.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-pg_dump --insert -h shiraz -U ads calibrations | grep " 'bath', " | psql -h thumper -U ads -d calibrations
-pg_dump --insert -h shiraz -U ads calibrations | grep " 'bath', " | psql -h hyper.raf-guest.ucar.edu -U ads -d calibrations
-pg_dump --insert -h shiraz -U ads calibrations | grep " 'bath', " | psql -h hercules.raf-guest.ucar.edu -U ads -d calibrations
+
+for server in thumper hyper.raf-guest.ucar.edu hercules.raf-guest.ucar.edu; do
+  echo "Pushing bath cals to:" $server
+  ping -c 1 $server
+  if [[ $? -eq 0 ]] ; then
+    pg_dump --insert -h shiraz -U ads calibrations | grep " 'bath', " | psql -h $server -U ads -d calibrations
+    echo "Pushed bath cals to:" $server
+  else
+    echo "Failed to push bath cals to:" $server "(no connection)."
+  fi
+done
