1cede628f6fb05b992407df7494691cca331e6f8
John Wasinger <wasinger@ucar.edu>
2013-01-30 05:21:53 +0000

    Test to see if the master database server is up first.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6512 640d5228-2204-0410-8d71-8f46fa6850b4

--- edit_cal/pushBathCalsToRemotes.sh
+++ edit_cal/pushBathCalsToRemotes.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+master="shiraz"
+ping -c 1 $master
+if [[ $? -ne 0 ]] ; then
+  echo "Master calibration database server '"$master"' is offline."
+  exit
+fi
+
 for server in thumper hyper.raf-guest.ucar.edu hercules.raf-guest.ucar.edu; do
   echo "Pushing bath cals to:" $server
   ping -c 1 $server
   if [[ $? -eq 0 ]] ; then
-    pg_dump --insert -h shiraz -U ads calibrations | grep " 'bath', " | psql -h $server -U ads -d calibrations
+    pg_dump --insert -h $master -U ads calibrations | grep " 'bath', " | psql -h $server -U ads -d calibrations
     echo "Pushed bath cals to:" $server
   else
     echo "Failed to push bath cals to:" $server "(no connection)."
