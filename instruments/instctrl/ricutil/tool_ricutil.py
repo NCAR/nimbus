fc09415a17dc0fee40b7f5ab77e7e36cd6fe2925
Charles Martin <martinc@ucar.edu>
2013-02-08 16:32:41 +0000

    Add boost tool so that the boost library extension is supplied on Windows builds.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6428 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/instctrl/ricutil/tool_ricutil.py
+++ instruments/instctrl/ricutil/tool_ricutil.py
@@ -3,7 +3,7 @@ import sys
 ourDir = Dir('.').abspath
 ourName = 'ricutil'
 
-tools = ['doxygen', 'qt4', 'boost_date_time']
+tools = ['doxygen', 'qt4', 'boost', 'boost_date_time']
 env = Environment(tools = ['default'] + tools)
 env.EnableQt4Modules(['QtCore', 'QtNetwork'])
 
