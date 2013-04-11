4a6d268986684ae1fce8852ca8140bbc668e8977
Pavel Romashkin <pavel@ucar.edu>
2013-04-11 16:30:23 +0000

    Inserted valve logic check here, leaving valve function check below. Valve logic moves valve every x seconds, while function check simply reads position and moves valve by kbd command.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6646 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/ozone/c/op_check_dio.c
+++ instruments/ozone/c/op_check_dio.c
@@ -5,6 +5,9 @@
 #include "config_base.h"
 #include "config_extern.h"
 
+BYTE ValveAdvance ( BYTE secA, BYTE secB );
+static int nSkips=0, nStops=0, prev_position=1; // Define persistent valve operation flags.
+
 /* ************************************
 Usage:
 op_check_dio
@@ -12,10 +15,10 @@ No arguments. Typing r reads the valve state; f advances the valve; q exits the 
 Main DIO program for OP valve control and monitoring. */
 int main ()
 {
-	unsigned int nSkips=0, nStops=0, prev_position=0; // Define valve operation flags.
 	char action = 'r';
 	BYTE dio_conf = 1; //B is the output port; DIRCL is the input port; others are not used.
 	BYTE CurrPosition;
+	int i;
 	
 	if (ioperm( BASE_PROM_DAQ, 15, 1 )) //Set hardware port access if possible.
 	{
@@ -23,8 +26,23 @@ int main ()
 		return -1;
 	}
 	
-	DIO_Config( BASE_PROM_DAQ, dio_conf ); //First time, set up DIO ports: A,B,CH for output, CL for input.
+	prev_position = Valve_GetPosition( BASE_PROM_DAQ );
 	
+	for ( i=1; i<300; i++ )
+	{
+		printf("Expected valve pos.: %d; ", ValveAdvance ( 2, 3 )); // Advance the valve.
+		usleep ( 100000 ); // Less than 150 ms does not give valve enough time to reach position.
+		// Check valve position after sleeping. Hopefully valve has finished moving and its position is now accurate.
+		BYTE CurrPosition = Valve_GetPosition( BASE_PROM_DAQ );
+		printf("Prev. pos.: %d; Curr. pos: %d;\n", prev_position, CurrPosition);
+		prev_position = CurrPosition;
+		//Valve_Check( CurrPosition, &nSkips, &nStops, &prev_position ); // Valve_Check increments nStops every time it is called.
+		//printf("N-stops: %d; N-skips: %d, iteration: %d\n", nStops, nSkips, i);
+	}
+	return 0;
+
+	
+	DIO_Config( BASE_PROM_DAQ, dio_conf ); //First time, set up DIO ports: A,B,CH for output, CL for input.
 	// Action loop. Typing r reads the valve state; f advances the valve; q exits the program.
 	for ( ; action != 'q'; )
 	{
@@ -42,4 +60,38 @@ int main ()
 		action = getchar();
 	}
 	return 0;
+}
+
+/* ************************************
+Valve operation wrapper. Implements valve advancing and status read. Note: valve position read-out can be inaccurate
+if it occurs immediately after the valve moved. It takes some time for valve to rotate and for the sensor to output
+the position. If a read is performed in the meantime a value with several position bits set may result.
+Returns: expected valve position. May be the same as previous or advanced by one.
+Parameters:
+secA: number of seconds to flow ambient sample through cell A (valve pos. odd);
+secB: number of seconds to flow ambient sample through cell B (valve pos. even);
+cycle_number: cycle number of the main DAQ loop. */
+BYTE ValveAdvance ( BYTE secA, BYTE secB )
+{
+	BYTE dio_conf = 1; // B is the output port; DIRCL is the input port; others are not used.
+	BYTE expectedPos, currChannel;
+	static int isFirstRun=1, cntr=0;
+	
+	if ( isFirstRun )
+		isFirstRun = DIO_Config( BASE_PROM_DAQ, dio_conf ); //First time, set up DIO ports: A,B,CH for output, CL for input.
+	
+	currChannel = prev_position % 2 ? secA : secB; // Flowing ambient air, odd position, A; even position, B.
+	cntr++;
+	
+	//Move valve if cycle number is a multiple of channel wait time and timer loop period.
+	if ( (cntr % (MAIN_FREQ / DOWN_COUNTS * currChannel)) == 0 )
+	{
+		Valve_Fwd( BASE_PROM_DAQ, prev_position );
+		expectedPos = (prev_position % 4) + 1;
+		cntr = 0;
+	}
+	else
+		expectedPos = prev_position;
+	
+	return expectedPos;
 }
