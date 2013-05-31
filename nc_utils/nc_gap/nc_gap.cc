3b8c927ff892c46c62f8b79a58f2cb989850b4ec
Chris Webster <cjw@ucar.edu>
2013-05-31 13:32:16 +0000

    Program to display time gaps in a netCDF file by variable.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6732 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ nc_utils/nc_gap/nc_gap.cc
@@ -0,0 +1,176 @@
+/*
+-------------------------------------------------------------------------
+OBJECT NAME:	nc_gap.cc
+
+FULL NAME:	Locate netCDF Time Gaps
+
+DESCRIPTION:	
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 2011
+-------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <cstring>
+
+#include <netcdfcpp.h>
+
+using namespace std;
+
+void processArgs(char **argv);
+
+/* -------------------------------------------------------------------- */
+void
+usage()
+{
+  cerr << "netCDF to IWG1.\n  Usage: nc_gap file.nc\n";
+  cerr << "    Scan a netCDF file for time gaps and output gaps by variable.\n";
+  exit(1);
+}
+
+time_t
+startTime(NcVar *var)
+{
+  static time_t st = 0;
+
+  if (st == 0)
+  {
+    // Get start time from Time units attribute.
+    struct tm ft;
+    NcAtt *time_units = var->get_att("units");
+    strptime(time_units->as_string(0), "seconds since %F %T %z", &ft);
+    st = mktime(&ft);
+  }
+
+  return st;
+}
+
+string
+formatTime(NcVar *var, float *data, size_t index)
+{
+  char buffer[128];
+  // Get start time, add Time data offset, and generate an ascii timestamp.
+  time_t recordTime = startTime(var) + (time_t)data[index];
+  struct tm *rt = gmtime(&recordTime);
+  strftime(buffer, 128, "%H:%M:%S", rt);
+  return buffer;
+}
+
+
+void
+printStartTimes(NcFile & inFile, NcVar *timeVar, float *time_data)
+{
+  cout	<< "Start time data file : " << formatTime(timeVar, time_data, 0) << ", "
+	<< timeVar->num_vals() << " seconds long.\n";
+
+  // Get ground speed.
+  NcVar *gspdVar =  inFile.get_var("Time");
+  float *gspd_data = new float[gspdVar->num_vals()];
+  gspdVar->get(gspd_data, gspdVar->edges());
+
+  int spd_indx;
+  for (spd_indx = 0; spd_indx < gspdVar->num_vals(); ++spd_indx)
+  {
+    if (gspd_data[spd_indx] > 5.0)
+    {
+      cout << "Start taxiing : " << formatTime(timeVar, time_data, spd_indx) << endl;
+      break;
+    }
+  }
+
+  for (; spd_indx < gspdVar->num_vals(); ++spd_indx)
+  {
+    if (gspd_data[spd_indx] > 25.0)
+    {
+      cout << "Start take-off : " << formatTime(timeVar, time_data, spd_indx) << endl;
+      break;
+    }
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc < 2)
+    usage();
+
+  processArgs(argv);
+
+  NcFile inFile(argv[argc-1]);
+
+  if (!inFile.is_valid())
+  {
+    cerr << "nc_gap: Failed to open input file, exiting.\n";
+    return 1;
+  }
+
+  putenv((char *)"TZ=UTC");     // Perform all time calculations at UTC.
+  NcError * ncErr = new NcError(NcError::silent_nonfatal);
+
+  // Get time variable and data.
+  NcVar *timeVar =  inFile.get_var("Time");
+  float *time_data = new float[timeVar->num_vals()];
+  timeVar->get(time_data, timeVar->edges());
+
+  printStartTimes(inFile, timeVar, time_data);
+
+  for (int i = 0; inFile.num_vars(); ++i)
+  {
+    NcVar *var = inFile.get_var(i);
+
+    if (!var || !var->is_valid())
+      continue;
+
+    if (var->num_dims() > 1)	// Stick to time-series scalars for the time being.
+      continue;
+
+    if (strcmp(var->name(), "ATX") == 0)
+      break;
+
+    if (var->get_att("Dependencies"))	// Skip derived vars for now.
+      continue;
+
+
+    float *data = new float[var->num_vals()];
+    var->get(data, var->edges());
+
+    for (int j = 0; j < var->num_vals(); ++j)
+    {
+      if (data[j] == -32767.0)	// Get missing value first.
+      {
+        cout << left << setw(20) << var->name();
+        cout << formatTime(timeVar, time_data, j);
+        int start = j;
+        for (; j < var->num_vals() && data[j] == -32767.0; ++j)
+          ;
+
+        cout << " - " << formatTime(timeVar, time_data, (j-1)) << " : ";
+        if (j-start == var->num_vals())
+          cout << " whole flight.\n";
+        else
+          cout << setw(6) << j-start << " seconds\n";
+      }
+    }
+
+    delete [] data;
+  }
+
+}
+
+
+void
+processArgs(char **argv)
+{
+  while (*++argv)
+    if ((*argv)[0] == '-')
+      switch ((*argv)[1])
+        {
+        case 'h':
+          usage();
+          break;
+
+        }
+}
