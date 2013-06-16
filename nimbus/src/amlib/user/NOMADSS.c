ff8aadaf25d97e8eec7b4f80955fc921e76395e2
Tom Baltzer <tbaltzer@ucar.edu>
2013-06-16 22:53:35 +0000

    A fix to Jesse's RH for the cabin and a first go at Stuart's EW for the UVH.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6752 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/user/NOMADSS.c
+++ nimbus/src/amlib/user/NOMADSS.c
@@ -21,6 +21,9 @@ static NR_TYPE	esw(NR_TYPE T);
 
 
 /* -------------------------------------------------------------------- */
+// A calculation of cabin RH at 20 degrees celcius given to me by 
+// Jesse Ambrose of UofW during NOMADSS campaign - Tom Baltzer
+
 void srhcab(DERTBL *varp)
 {
   NR_TYPE	atx, rhum, rhum20cab, numerator;
@@ -28,7 +31,7 @@ void srhcab(DERTBL *varp)
   rhum = GetSample(varp, 0);
   atx = GetSample(varp, 1);
 
-  numerator = exp(17.67*(atx/(atx*243.5)));
+  numerator = exp(17.67*(atx/(atx+243.5)));
 
   rhum20cab = rhum * (numerator/denominator);
 
@@ -39,3 +42,28 @@ void srhcab(DERTBL *varp)
 }
 
 /* -------------------------------------------------------------------- */
+//  A calculation based on information from Stuart Beaton in an e-mail to
+//  Jorgen on 6/11/2013
+
+
+void sewuvh(DERTBL *varp)
+{
+  NR_TYPE	xsigv, xcelltemp, psx, xcellpres, n, ewcell, ewuvh;
+
+  xsigv = GetSample(varp, 0);
+  xcelltemp = GetSample(varp, 1);
+  psx = GetSample(varp, 1);
+  xcellpres = GetSample(varp, 1);
+
+  n = -2e17*log((xsigv - 0.34)/1.055);
+  ewcell = n * 1.38065e-23 * (xcelltemp+273.14)*10000;
+  ewuvh = ewcell*(psx/xcellpres);
+
+  //printf("rhcab calculation. %f*(%f/%f) = %f\n", rhum, numerator, denominator, rhum20cab);
+
+  PutSample(varp, ewuvh);
+
+}
+
+/* -------------------------------------------------------------------- */
+
