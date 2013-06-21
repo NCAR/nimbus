a57b4a121a54cf95ad7678ebc284883aa4ccd73f
Chris Webster <cjw@ucar.edu>
2013-06-21 22:58:16 +0000

    New UV Hygrometer.  - Migrate code to uvhyg.c  - break caclulation apart, want intermediary number density CONCH variable.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6756 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/user/uvhyg.c
+++ nimbus/src/amlib/user/uvhyg.c
@@ -17,8 +17,16 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2007
 #include "nimbus.h"
 #include "amlib.h"
 
+static NR_TYPE sigma = 1.406E+17;
+static NR_TYPE offset = 0.2662;
+static NR_TYPE gain = 1.0554;
+static NR_TYPE pathlength = 0.4318;
+
 
 /* -------------------------------------------------------------------- */
+/**
+ * UV Hygrometer pre-2013.
+ */
 void suvmr(DERTBL * varp)
 {
   NR_TYPE	xuvi, psx, atx;
@@ -32,3 +40,56 @@ void suvmr(DERTBL * varp)
   else	// VOCALS and later.
     PutSample(varp, -37.95 * ((atx + Kelvin) / psx) * (log((xuvi - 0.076) / 12.25) + 0.00045 * psx));
 }
+
+
+/* -------------------------------------------------------------------- */
+void uvhInit(var_base *varp)
+{
+  float *tmp;
+
+  if ((tmp = GetDefaultsValue("UV_SIGMA", varp->name)) == NULL)
+  {
+    sprintf(buffer, "UV_SIGMA set to %f in AMLIB function uvhyg.\n", sigma);
+    LogMessage(buffer);
+  }
+  else
+    sigma = tmp[0];
+
+  if ((tmp = GetDefaultsValue("UV_OFFSET", varp->name)) == NULL)
+  {
+    sprintf(buffer, "UV_OFFSET set to %f in AMLIB function uvhyg.\n", offset);
+    LogMessage(buffer);
+  }
+  else
+    offset = tmp[0];
+
+  if ((tmp = GetDefaultsValue("UV_GAIN", varp->name)) == NULL)
+  {
+    sprintf(buffer, "UV_GAIN set to %f in AMLIB function uvhyg.\n", gain);
+    LogMessage(buffer);
+  }
+  else
+    gain = tmp[0];
+
+  if ((tmp = GetDefaultsValue("UV_PATHLEN", varp->name)) == NULL)
+  {
+    sprintf(buffer, "UV_PATHLEN set to %f in AMLIB function uvhyg.\n", pathlength);
+    LogMessage(buffer);
+  }
+  else
+    pathlength = tmp[0];
+}
+
+/* -------------------------------------------------------------------- */
+void sconch(DERTBL *varp)
+{
+  NR_TYPE       xsigv, conc;
+
+  xsigv = GetSample(varp, 0);
+  conc = -sigma * pathlength * log((xsigv - offset) / gain);
+  if (conc <= 0.0)
+    conc = floatNAN;
+
+  PutSample(varp, conc);
+}
+
