14ba6849ccd04f9ef4511bd120202810e4f7a4f2
Chris Webster <cjw@ucar.edu>
2013-08-01 17:39:57 +0000

    Repair "Camera status not working" on status web page.
    Problem: multicast of status was going out default interface, which is eth2 on the aircraft.  When eth2 was down for satcom to come up, status would stop functioning.
    
    Repaired code to make 192.168.184 subnet as default multi-cast for camera capture.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6802 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/camera/capture/multicast.c
+++ instruments/camera/capture/multicast.c
@@ -6,67 +6,110 @@
 static long count=0;
 
 static struct sockaddr_in addr;
-static int fd;
+static int fd = -1;
 
-void multicast_status_init(status_t *status, camConf_t **camArray, int numCams) {
+
 /* This function allocates memory for all of the data contained in the status 
-	structure. The size of memory needed varies by the number of cameras on 
-	the bus as well as the current settings in the config file.
-*/
-	int i;
-	strcpy(status->clock, "0000-00-00 00:00:00");
-	status->health = 0;
-	status->html = 0;
-	status->numCams = numCams;
+ * structure. The size of memory needed varies by the number of cameras on 
+ * the bus as well as the current settings in the config file.
+ */
+void multicast_status_init(status_t *status, camConf_t **camArray, int numCams)
+{
+  int i;
+  strcpy(status->clock, "0000-00-00 00:00:00");
+  status->health = 0;
+  status->html = 0;
+  status->numCams = numCams;
 //	printf("numcams: %d", numCams);
 	
-	status->lastSize = malloc(sizeof(int) * numCams);
-	status->runningAvg = malloc(sizeof(float) * numCams);
-	status->cam_row = malloc(sizeof(char *) * numCams);
-	status->latest = malloc(sizeof(char *) * numCams);
+  status->lastSize = malloc(sizeof(int) * numCams);
+  status->runningAvg = malloc(sizeof(float) * numCams);
+  status->cam_row = malloc(sizeof(char *) * numCams);
+  status->latest = malloc(sizeof(char *) * numCams);
 
-	for	(i=0; i<numCams; i++) {
-		status->cam_row[i] = malloc(sizeof(char) * (
-			strlen(ROW_BASE) + strlen(camArray[i]->direction) + 16
-		));
-		sprintf(status->cam_row[i] ,ROW_BASE, camArray[i]->direction, camArray[i]->guid);
+  for	(i = 0; i < numCams; i++) {
+    status->cam_row[i] = malloc(sizeof(char) * (
+			strlen(ROW_BASE) + strlen(camArray[i]->direction) + 16));
+    sprintf(status->cam_row[i] ,ROW_BASE, camArray[i]->direction, camArray[i]->guid);
 
-		status->latest[i] = malloc(sizeof(char) * 100);
-		status->lastSize[i] = 0;
-		status->runningAvg[i] = 0;
-	}
+    status->latest[i] = malloc(sizeof(char) * 100);
+    status->lastSize[i] = 0;
+    status->runningAvg[i] = 0;
+  }
 
-//	printf("%s\n", status->cam_row[0]);
+//  printf("%s\n", status->cam_row[0]);
 
-	/* create what looks like an ordinary UDP socket */
-	if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0) {
-		syslog(LOG_ERR, "failed to create Multicast socket");
-		return;
-	}
-	
-	/* set up destination address */
-	memset(&addr,0,sizeof(addr)); 	//zero out addr struct
-	addr.sin_family=AF_INET;		//set to inet socket (i.e. not UNIX)
-	addr.sin_addr.s_addr=inet_addr(MULTICAST_GROUP);	//set IP address (multicast group)
-	addr.sin_port=htons(MULTICAST_PORT);	//set port to send data on
+
+  // Obtain list of all network interfaces
+  struct ifaddrs *addrs;
+  if ( getifaddrs(&addrs) < 0 ) {
+    syslog(LOG_ERR, "failed to get network interface list");
+    return;
+  }
+
+
+  /* Loop through interfaces, selecting those AF_INET devices that support
+   * multicast, but aren't loopback or point-to-point
+   */
+  const struct ifaddrs *cursor;
+
+  for ( cursor = addrs; cursor != NULL && fd < 0; cursor = cursor->ifa_next )
+  {
+    if ( cursor->ifa_addr->sa_family == AF_INET
+            && !(cursor->ifa_flags & IFF_LOOPBACK)
+            && !(cursor->ifa_flags & IFF_POINTOPOINT)
+            &&  (cursor->ifa_flags & IFF_MULTICAST) )
+    {
+      char host[128];
+      getnameinfo(cursor->ifa_addr, sizeof(struct sockaddr_in), host, 128, NULL, 0, NI_NUMERICHOST);
+      if (strncmp(host, "192.168.184", 11))     // This is the interface we want.
+        continue;
+
+      // Create socket
+      if ((fd = socket(AF_INET,SOCK_DGRAM, 0)) < 0) {
+        syslog(LOG_ERR, "failed to create Multicast socket");
+        return;
+      }
+
+      // Make the socket transmit through this interface
+      struct ip_mreqn mreq;
+      memset(&mreq, 0, sizeof(mreq));
+      mreq.imr_ifindex = if_nametoindex(cursor->ifa_name);
+
+      if (setsockopt(fd, SOL_IP, IP_MULTICAST_IF, &mreq, sizeof(mreq)) != 0) {
+        syslog(LOG_ERR, "failed to setsockopt multicast");
+        return;
+      }
+    }
+  }
+
+  freeifaddrs(addrs);
+
+  /* set up destination address */
+  memset(&addr, 0, sizeof(addr)); 	//zero out addr struct
+  addr.sin_family = AF_INET;		//set to inet socket (i.e. not UNIX)
+  addr.sin_addr.s_addr = inet_addr(MULTICAST_GROUP);	//set IP address (multicast group)
+  addr.sin_port = htons(MULTICAST_PORT);	//set port to send data on
 }
 
-void multicast_send_final(status_t *status) {
 /* This function sends the final status multicast packet before the program terminates.
-	It sets health to 0, because the program is ending. Then it sends a packet with
-	'shut down' as the message. 
-*/
-	char *pkt;
-	pkt = malloc(sizeof(char) * (strlen(PACKET_BASE) + strlen(status->clock) + 11));
-	sprintf(pkt, PACKET_BASE, status->clock, 0, "shut down"); 
-	multicast_send_packet(pkt);
-	free(pkt);
+ * It sets health to 0, because the program is ending. Then it sends a packet with
+ * 'shut down' as the message. 
+ */
+void multicast_send_final(status_t *status)
+{
+  char *pkt;
+  pkt = malloc(sizeof(char) * (strlen(PACKET_BASE) + strlen(status->clock) + 11));
+  sprintf(pkt, PACKET_BASE, status->clock, 0, "shut down"); 
+  multicast_send_packet(pkt);
+  free(pkt);
 }
 
-void multicast_clean_up(status_t *status){
 /* This function sends a final multicast message, then cleans up memory that
-	was allocated by multicast_status_init()
-*/
+ * was allocated by multicast_status_init()
+ */
+void multicast_clean_up(status_t *status)
+{
 	int i;
 
 	status->health = 0;	
@@ -143,11 +186,8 @@ int multicast_send_status(status_t *status) {
 
 void multicast_send_packet(char *packet)
 {
-	/* now just sendto() our destination! */
-	if (sendto(fd,packet,strlen(packet),0,(struct sockaddr *) &addr, sizeof(addr)) < 0) {
-                count++;
-                if (count%1000 == 0)
-		    syslog(LOG_ERR, "failed to send Multicast status %ld times",count);
-		return;
-	}
+  if (sendto(fd, packet, strlen(packet), 0, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    if (count++ % 10 == 0)
+      syslog(LOG_ERR, "failed to send Multicast status %ld times",count);
+  }
 }
