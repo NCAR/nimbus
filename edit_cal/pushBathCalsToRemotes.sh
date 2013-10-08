7ae5ad66c180d3a822bf5bd2c5cfa8e0cb7908e9
John Wasinger <wasinger@ucar.edu>
2013-10-08 19:37:03 +0000

    Push only TT_BATH items to remote sites.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6839 640d5228-2204-0410-8d71-8f46fa6850b4

--- edit_cal/pushBathCalsToRemotes.sh
+++ edit_cal/pushBathCalsToRemotes.sh
@@ -11,7 +11,7 @@ for server in thumper hyper.raf-guest.ucar.edu hercules.raf-ext.ucar.edu; do
   echo "Pushing bath cals to:" $server
   ping -c 1 $server
   if [[ $? -eq 0 ]] ; then
-    pg_dump --insert -h $master -U ads calibrations | grep " 'bath', " | psql -h $server -U ads -d calibrations
+    pg_dump --insert -h $master -U ads calibrations | grep " 'TT_BATH', " | psql -h $server -U ads -d calibrations
     echo "Pushed bath cals to:" $server
   else
     echo "Failed to push bath cals to:" $server "(no connection)."
