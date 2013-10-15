01ebc68dfa85b08f4c623758b07d675714f7e028
Chris Webster <cjw@ucar.edu>
2013-10-15 13:27:40 +0000

    Cleanup.  Non-functional changes.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6849 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/xmach2.c
+++ nimbus/src/amlib/std/xmach2.c
@@ -1,17 +1,24 @@
-/*************************** XMACH2 *******************************  XMACH2
- ** primary mach number squared
-        Input:
-                qcxc - derived dynamic pressure
-                psxc - derived static pressure
-        Output:
-                xmach2 - derived mach number squared
-        Include:
-                xmac2 - mach number funtion
+/*
+-------------------------------------------------------------------------
+OBJECT NAME:	xmach2.c
+
+FULL NAME:	Mach Number
+
+DESCRIPTION:	Versions of nimbus prior to 2013 used XMACH2 or mach number
+		squared.  We now produce a mach number for each QC probe and
+		have a MACHX as the blessed reference.
+
+		Projects for ADS3 and later have all been retrofitted with
+		corrected depend tables.
+
+COPYRIGHT:	University Corporation for Atmospheric Research, 1990-2013
+-------------------------------------------------------------------------
 */
 
 #include "nimbus.h"
 #include "amlib.h"
-      
+
+
 /* -------------------------------------------------------------------- */
 void sxmach2(DERTBL *varp)
 {
