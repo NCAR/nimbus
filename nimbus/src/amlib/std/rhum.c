12cc16b652135f769bcc0a41a158b891f36c00ce
Chris Webster <cjw@ucar.edu>
2013-12-06 03:11:21 +0000

    Add RHUM wrt Ice (RHUMI).
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6951 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/rhum.c
+++ nimbus/src/amlib/std/rhum.c
@@ -10,29 +10,51 @@
 #include "nimbus.h"
 #include "amlib.h"
 
+double WaterVaporPressureOfWater(double);
+double WaterVaporPressureOfIce(double);
 
 /* -------------------------------------------------------------------- */
 void srhum(DERTBL *varp)
 {
-  NR_TYPE atx, psxc, e, esa, rhum;
+  NR_TYPE atx, e, esa, rhum = 0.0;
 
   atx = GetSample(varp, 0);
   if (varp->ndep == 3)	// Old method for computing RHUM.
   {
     double esubt(double, double);
-    psxc = GetSample(varp, 1);
+    NR_TYPE psxc = GetSample(varp, 1);
     e	= GetSample(varp, 2);
     esa = (NR_TYPE)esubt(atx, psxc);
   }
   else	// New method.
   {
-    double WaterVaporPressureOfWater(double);
     e = GetSample(varp, 1);
     esa = (NR_TYPE)WaterVaporPressureOfWater(atx + Kelvin);
   }
 
-  if (rhum != 0.0)
+  if (esa != 0.0)
     rhum = 100.0 * e / esa;
+
+  PutSample(varp, rhum);
+}
+
+/* -------------------------------------------------------------------- */
+void srhumi(DERTBL *varp)
+{
+  NR_TYPE rhum = 0.0;
+
+  NR_TYPE atx = GetSample(varp, 0) + Kelvin;
+
+  if (atx < Kelvin)
+  {
+    NR_TYPE e = GetSample(varp, 1);
+    NR_TYPE esa = (NR_TYPE)WaterVaporPressureOfIce(atx);
+
+    if (esa != 0.0)
+      rhum = 100.0 * e / esa;
+  }
+  else
+    rhum = floatNAN;
 
   PutSample(varp, rhum);
 
