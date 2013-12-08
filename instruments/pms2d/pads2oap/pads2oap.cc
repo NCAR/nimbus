1934ac3c96d3931a5f23fd96d00b941d814cfd8c
Chris Webster <cjw@ucar.edu>
2013-12-08 20:47:58 +0000

    2D data translator.  Translates (re-encapsulates) DMT 2D data from PADS into RAF OAP format.  Does not decompress the 4096 buffers.  That is still an option, TBD.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@6953 640d5228-2204-0410-8d71-8f46fa6850b4

---
+++ instruments/pms2d/pads2oap/pads2oap.cc
@@ -0,0 +1,253 @@
+/*
+ * Program to convert PADS 2D probe files into RAF OAP file format.  This does not uncompress
+ * the 4096 data block, rather it just encapsulates it differently.  Adds an XML header to the
+ * front of the file with useful meta-data and reworks the header in front of each 4096 PADS
+ * buffer.
+ */
+#include "pads2oap.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+
+// for byte swapping routines ntohs() & htons()
+#include <arpa/inet.h>
+
+
+void ProcessArgs(char *argv[], OAPfile& oap, std::string& pfn, std::string& ofn);
+void ConvertPADStoOAP(const PADS_rec&, OAP_rec&, uint16_t);
+std::string DateStamp(const PADS_rec&), TimeStamp(const PADS_rec&);
+
+
+/* ----------------------------------------------------------------------- */
+void defineProbes(OAPfile& oap)
+{
+  // Need a way to pass in probe list on command line....
+
+  // DYNAMO setup; P4, 100um, 64 diodes, guessing serial#.
+  oap.AddProbe(PMS2D_P4, 100, 64, "2DP100", "_RWI");
+
+  // IDEAS-4
+//  oap.AddProbe(PMS2D_C4, 25, 64, "F2DC001", "_LWI");
+//  oap.AddProbe(PMS2D_C6, 10, 64, "F2DC002", "_RWO");
+
+}
+
+/* ----------------------------------------------------------------------- */
+void usage()
+{
+  std::cerr << "pads2oap [-flight num] [-project name] [-platform name] -i pads_file -o oap_file\n";
+  std::cerr << "  -project : is for project name.  e.g. TORERO\n";
+  std::cerr << "  -platform : is for platform name.  e.g. GV_N677F or P3_N43RF\n";
+  std::cerr << "  -flight : is for flight number, this is a string, not necessarily a number.  e.g. rf03\n";
+  std::cerr << "  -i : input filename; PADS format.\n";
+  std::cerr << "  -o : output filename; will be OAP format.\n";
+  exit(1);
+}
+
+/* ----------------------------------------------------------------------- */
+int main(int argc, char *argv[])
+{
+  FILE *in;
+  OAPfile oap;
+  std::string pads_filename, oap_filename;
+
+  ProcessArgs(argv, oap, pads_filename, oap_filename);
+
+  if (pads_filename.length() == 0 || oap_filename.length() == 0)
+    usage();
+
+
+  if ((in = fopen(pads_filename.c_str(), "rb")) == NULL)
+  {
+    std::cerr << "Can't open input file " << argv[1] << std::endl;
+    exit(1);
+  }
+
+  oap.open(oap_filename);
+  defineProbes(oap);
+
+
+  int cnt = 0;
+  PADS_rec pads_rec;
+  OAP_rec oap_rec;
+
+  for (cnt = 0; fread(&pads_rec, sizeof(PADS_rec), 1, in); ++cnt)
+  {
+    ConvertPADStoOAP(pads_rec, oap_rec, PMS2D_P1);
+
+    if (cnt == 0)
+    {
+      std::string s = DateStamp(pads_rec);
+      oap.SetFlightDate(s);  // This was outside the loop, but here for auto-detect of date.
+      oap.WriteHeader();
+      std::cout << "First record " << DateStamp(pads_rec) << " " << TimeStamp(pads_rec) << std::endl;
+    }
+
+    oap.WriteRecord(oap_rec);
+  }
+
+  fclose(in);
+
+  std::cout << "Last record " << DateStamp(pads_rec) << " " << TimeStamp(pads_rec) << std::endl;
+  std::cout << cnt << " records transferred." << std::endl;
+}
+
+/* ----------------------------------------------------------------------- */
+void ConvertPADStoOAP(const PADS_rec& pads_rec, OAP_rec& oap_rec, uint16_t id)
+{
+  memset(&oap_rec, 0, sizeof(OAP_rec));
+  oap_rec.id	= htons(id);
+  oap_rec.year	= htons(pads_rec.year);
+  oap_rec.month	= htons(pads_rec.month);
+  oap_rec.day	= htons(pads_rec.day);
+  oap_rec.hour	= htons(pads_rec.hour);
+  oap_rec.minute= htons(pads_rec.minute);
+  oap_rec.second= htons(pads_rec.second);
+  oap_rec.msec	= htons(pads_rec.msec);
+  memcpy(oap_rec.data, pads_rec.data, TWOD_BUFF_SIZE);
+}
+
+/* ----------------------------------------------------------------------- */
+std::string DateStamp(const PADS_rec& pads_rec)
+{
+  std::string s;
+  char msg[1024];
+  sprintf(msg, "%04d/%02d/%02d", pads_rec.year, pads_rec.month, pads_rec.day);
+  s = msg;
+  return s;
+}
+
+/* ----------------------------------------------------------------------- */
+std::string TimeStamp(const PADS_rec& pads_rec)
+{
+  std::string s;
+  char msg[1024];
+  sprintf(msg, "%02d:%02d:%02d.%d", pads_rec.hour, pads_rec.minute, pads_rec.second, pads_rec.msec);
+  s = msg;
+  return s;
+}
+
+
+/* ----------------------------------------------------------------------- */
+void OAPfile::open(const std::string& filename)
+{
+  if (filename.length() == 0)
+    usage();
+
+  if ((_out = fopen(filename.c_str(), "w+b")) == NULL)
+  {
+    std::cerr << "Can't open output file " << filename << std::endl;
+    exit(1);
+  }
+}
+
+OAPfile::~OAPfile()
+{
+  if (_out)
+    fclose(_out);
+}
+
+void OAPfile::WriteRecord(const OAP_rec& oap_rec)
+{
+  if (!_out)
+    return;
+
+  if (fwrite(&oap_rec, sizeof(OAP_rec), 1, _out) != 1)
+  {
+    std::cerr << "Write error, exiting." << std::endl;
+    fclose(_out);
+    exit(1);
+  }
+}
+
+/* ----------------------------------------------------------------------- */
+void OAPfile::WriteHeader()
+{
+  std::stringstream header;
+
+  header
+	<< "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"
+  	<< "<OAP version=\"1\">\n"
+	<< " <Institution>NOAA Aircraft Operations Center</Institution>\n"
+	<< " <FormatURL>http://www.eol.ucar.edu/raf/Software/OAPfiles.html</FormatURL>\n";
+
+  if (_project.length())
+    header << " <Project>" << _project << "</Project>\n";
+
+  if (_platform.length())
+    header << " <Platform>" << _platform << "</Platform>\n";
+
+  if (_flightnumber.length())
+    header << " <FlightNumber>" << _flightnumber << "</FlightNumber>\n";
+
+  if (_flightdate.length())
+    header << " <FlightDate>" << _flightdate << "</FlightDate>\n";
+
+  for (int i = 0; i < _probelist.size(); ++i)
+  {
+    const char *id = (char *)&_probelist[i].id;
+    header << "  <probe id=\""
+	<< id[1] << id[0]  << "\" type=\"TwoDP\" resolution=\""
+	<< _probelist[i].resolution << "\" nDiodes=\""
+	<< _probelist[i].nDiodes << "\" serialnumber=\""
+	<< _probelist[i].serialnumber << "\" suffix=\""
+	<< _probelist[i].suffix << "\"/>\n";
+  }
+
+  header << "</OAP>\n";
+
+  if (_out)
+    fwrite(header.str().c_str(), header.str().length(), 1, _out);
+}
+
+/* ----------------------------------------------------------------------- */
+void OAPfile::AddProbe(uint16_t id, int resolution, int nDiodes, std::string serialnumber, std::string suffix)
+{
+  Probe p;
+
+  p.id = id;
+  p.resolution = resolution;
+  p.nDiodes = nDiodes;
+  p.serialnumber = serialnumber;
+  p.suffix = suffix;
+
+  _probelist.push_back(p);
+}
+
+/* ----------------------------------------------------------------------- */
+void ProcessArgs(char *argv[], OAPfile& oap, std::string& pfn, std::string& ofn)
+{
+  while (*++argv)
+  {
+    if (strcmp(*argv, "-project") == 0)
+    {
+      oap.SetProject(*++argv);
+    }
+    else
+    if (strcmp(*argv, "-platform") == 0)
+    {
+      oap.SetPlatform(*++argv);
+    }
+    else
+    if (strcmp(*argv, "-flight") == 0)
+    {
+      oap.SetFlightNumber(*++argv);
+    }
+    else
+    if (strcmp(*argv, "-i") == 0)
+    {
+      pfn = *++argv;
+    }
+    else
+    if (strcmp(*argv, "-o") == 0)
+    {
+      ofn = *++argv;
+    }
+    else
+      usage();
+  }
+}
+
---
+++ instruments/pms2d/pads2oap/pads2oap.h
@@ -0,0 +1,107 @@
+
+#ifndef _pads2oap_h_
+#define _pads2oap_h_
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#define TWOD_BUFF_SIZE	4096
+
+// Values currently in use for the 'id' field...at least by NCAR-RAF.
+#define PMS2D_C1         0x4331		// First PMS-2DC
+#define PMS2D_C2         0x4332		// Second PMS-2DC
+#define PMS2D_C4         0x4334         // First RAF 64 diode 25 um Fast 2DC
+#define PMS2D_C5         0x4335         // Second RAF 64 diode 25 um Fast 2DC
+#define PMS2D_C6         0x4336         // First RAF 64 diode 10 um Fast 2DC
+#define PMS2D_G1         0x4731		// First 2D Greyscale; unused to date
+#define PMS2D_H1         0x4831		// SPEC HVPS
+#define PMS2D_P1         0x5031		// First PMS-2DP
+#define PMS2D_P2         0x5032		// Second PMS-2DP
+#define PMS2D_P4         0x5034		// First RAF 64 diode 200 um Fast 2DP
+#define PMS2D_3V         0x3348		// 3V-CPI 2DS Vertical
+#define PMS2D_3H         0x3356		// 3V-CPI 2DS Horizontal
+
+
+struct _oap_rec {
+  int16_t id;				// 'P1','C1','P2','C2', H1, etc
+  int16_t hour;
+  int16_t minute;
+  int16_t second;
+  int16_t year;				// starting in 2007 w/ PACDEX
+  int16_t month;			// starting in 2007 w/ PACDEX
+  int16_t day;				// starting in 2007 w/ PACDEX
+  int16_t tas;				// true air speed, m/s
+  int16_t msec;				// msec of this record
+  int16_t overld;			// overload time, msec
+  unsigned char data[TWOD_BUFF_SIZE];	// image buffer
+  };
+typedef struct _oap_rec OAP_rec;
+
+struct _pads_rec {
+  uint16_t year;
+  uint16_t month;
+  uint16_t day;
+  uint16_t hour;
+  uint16_t minute;
+  uint16_t second;
+  uint16_t msec;
+  uint16_t wday;
+  unsigned char data[TWOD_BUFF_SIZE];
+  };
+typedef struct _pads_rec PADS_rec;
+
+
+class Probe
+{
+public:
+  Probe() : id(0), resolution(0), nDiodes(0) { }
+
+  uint16_t id;
+  int resolution;
+  int nDiodes;
+  std::string serialnumber;
+  std::string suffix;
+};
+
+
+
+class OAPfile
+{
+public:
+  OAPfile() : _out(0) { };
+  ~OAPfile();
+
+  void open(const std::string& file_name);
+
+  /**
+   * @param id Probe ID, used to distinguish between multiple probes in the same file.
+   * @param resolution probe resoultion in micrometers.
+   * @param nDiodes is the number of diodes.
+   * @param serialnumber is the probe serial number.  Strictly meta-data.
+   * @param suffix is used when merging data into a netCDF.  All generated variables
+   * will have this suffix.  See process2d(1) program
+   */
+  void AddProbe(uint16_t id, int resolution, int nDiodes, std::string serialnumber, std::string suffix);
+
+  void WriteHeader();
+  void WriteRecord(const OAP_rec&);
+
+  void SetProject(std::string s)	{ _project = s; }
+  void SetPlatform(std::string s)	{ _platform = s; }
+  void SetFlightNumber(std::string s)	{ _flightnumber = s; }
+  void SetFlightDate(std::string s)	{ _flightdate = s; }
+
+protected:
+  FILE *_out;
+
+  std::string _project;
+  std::string _platform;
+  std::string _flightnumber;
+  std::string _flightdate;
+
+  std::vector<Probe> _probelist;
+};
+
+
+#endif
