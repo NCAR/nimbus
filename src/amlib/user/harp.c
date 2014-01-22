5cebf1acaeeba94aee7dc81e83384b81f1ba9a8c
Gary Granger <granger@ucar.edu>
2014-01-22 06:22:10 +0000

    add sjbro derivation function, very similar to sjno2
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@6995 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/user/harp.c
+++ src/amlib/user/harp.c
@@ -16,6 +16,9 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2013
 static const double uvn382_cals[] = { 354.1, 1.56e-6 };    // Nadir
 static const double uvz382_cals[] = { 1337.6, 1.50e-6 };   // Zenith
 
+static const double uvn330_cals[] = { 185.285, 1.2570e-5 };    // Nadir
+static const double uvz330_cals[] = { 174.12, 1.1430e-5 };   // Zenith
+
 /* -------------------------------------------------------------------- */
 void sjno2(DERTBL *varp)
 {
@@ -33,3 +36,23 @@ void sjno2(DERTBL *varp)
 
   PutSample(varp, yn+yz);
 }
+
+
+void sjbro(DERTBL *varp)
+{
+  NR_TYPE	cnts_n, cnts_z;
+  static NR_TYPE yn, yz;
+
+  cnts_n = GetSample(varp, 0);
+  cnts_z = GetSample(varp, 1);
+
+  if (!isnan(cnts_n))
+    yn = (cnts_n - uvn330_cals[0]) * uvn330_cals[1];
+
+  if (!isnan(cnts_z))
+    yz = (cnts_z - uvz330_cals[0]) * uvz330_cals[1];
+
+  PutSample(varp, yn+yz);
+}
+
+
