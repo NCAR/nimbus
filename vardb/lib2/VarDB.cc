9f44510e1f2113b80f2e3ea8714b9a3fbf9a0704
Julian Quick <jquick@ucar.edu>
2014-07-21 18:52:40 +0000

    I accidently saved a corrupt xml, but this is now all clean and ok!
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7185 640d5228-2204-0410-8d71-8f46fa6850b4

--- vardb/lib2/VarDB.cc
+++ vardb/lib2/VarDB.cc
@@ -6,10 +6,10 @@ int main()
  vdb.open("VDB.xml"); 
  VDBVar zero = vdb.get_var("ZERO");
  cout<<zero.get_attribute("long_name")<<"\n";
- zero.set_attribute("unITs","queso");
+ zero.set_attribute("unITs","none");
  VDBVar s = vdb.get_var("2D-C");
  cout<<s.get_attribute("long_name")<<"\n";
- s.set_attribute("reference","cheese");
+ s.set_attribute("reference","false");
  vdb.close();
  //cout<<zero->get_attribute("units");
 }
