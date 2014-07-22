f73a1fb8aad312857766852d8c9d55497f634e5f
Chris Webster <cjw@ucar.edu>
2014-07-22 20:13:08 +0000

    Add test save command line option.  Julian to implement use.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7197 640d5228-2204-0410-8d71-8f46fa6850b4

--- vardb/lib2/vdb_test.cc
+++ vardb/lib2/vdb_test.cc
@@ -1,15 +1,20 @@
 #include "vardb.h"
 
-int main()
+int main(int argc, char *argv[])
 {
- printf("Hello Julian\n");
- VDBFile vdb;
- vdb.open("VDB.xml"); 
- VDBVar zero = vdb.get_var("ZERO");
- cout<<zero.get_attribute("long_name")<<"\n";
- zero.set_attribute("unITs","none");
- VDBVar s = vdb.get_var("2D-C");
- cout<<s.get_attribute("long_name")<<"\n";
- s.set_attribute("reference","false");
- vdb.close();
+  bool test_save = false;
+  VDBFile vdb;
+
+  if (strcmp(argv[1], "-s") == 0)
+    test_save = false;
+
+  printf("Hello Julian\n");
+  vdb.open("VDB.xml"); 
+  VDBVar zero = vdb.get_var("ZERO");
+  cout<<zero.get_attribute("long_name")<<"\n";
+  zero.set_attribute("unITs","none");
+  VDBVar s = vdb.get_var("2D-C");
+  cout<<s.get_attribute("long_name")<<"\n";
+  s.set_attribute("reference","false");
+  vdb.close();
 }
