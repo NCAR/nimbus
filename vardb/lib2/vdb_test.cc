8287ecd8992489e4d667963b4fb30caeb133b92b
Chris Webster <cjw@ucar.edu>
2014-07-23 22:23:25 +0000

    Add is_valid() method to test program.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7215 640d5228-2204-0410-8d71-8f46fa6850b4

--- vardb/lib2/vdb_test.cc
+++ vardb/lib2/vdb_test.cc
@@ -1,12 +1,22 @@
 #include "vardb.h"
 
+std::string fileName("VDB.xml");
+
 int main()
 {
-  VDBFile vdb("VDB.xml");
+  VDBFile vdb(fileName);
+
+  if (vdb.is_valid() == false)
+  {
+    std::cerr << "Failed to open " << fileName << endl;
+    return 1;
+  }
 
   VDBVar *zero = vdb.get_var("ZERO");
   if(zero->get_attribute("UnIts")=="none")
   {cout<<"Looks good!\n";
   }else{cout<<"Check configuration\n";}
+
   vdb.close();
+  return 0;
 }
