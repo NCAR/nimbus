629760798d6753070826f2184825bd0f416e09c7
Chris Webster <cjw@ucar.edu>
2014-08-04 21:38:49 +0000

    Turn qc_check into daemon for running passive checks.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7266 640d5228-2204-0410-8d71-8f46fa6850b4

--- nagios-qc/pi/compare.py
+++ nagios-qc/pi/compare.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import numpy
 import psycopg2
 import os
--- nagios-qc/pi/qc_check.sh
+++ nagios-qc/pi/qc_check.sh
@@ -4,39 +4,11 @@
 #When exiting this script, use ctrl+c, not ctrl+z
 #Author: Julian Quick, SUPER Intern 2014
 
-#Erase information from last execution
-rm commands
-sudo rm /var/log/nagios/nagios.log
-sudo rm /etc/nagios/raf/*
-sudo /sbin/service nagios stop
-sudo /sbin/service nagios start
-
-#Check data
-python pi.py
-
-#Create configuration files
-sudo python configMaker.py
-
-#Pipe data to nagios
-sh shellcmd.sh
-
-#scan nagios error log for missing configuration files, make them
-#sudo perl ../radar.pl
-#sleep 2
-cat /etc/nagios/raf/*
-
-#restart nagios with new configuration files
-#sudo /sbin/service nagios restart
-#sudo pkill -f radar
-
-#Pipe information to nagios again
-sh shellcmd.sh
-
 #This loop indefinatly compares psql data to vardb and pipes passive 
 #checks to nagios every 30 seconds
 while :
 do
-    python pi.py
-    sh shellcmd.sh
+    compare.py
+    piper.sh
     sleep 30
 done
