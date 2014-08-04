5a1f42400f2d3c22736f7cdd2769150286cef4b1
Chris Webster <cjw@ucar.edu>
2014-08-04 21:35:20 +0000

    Added custom hosts.  --Julian
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7264 640d5228-2204-0410-8d71-8f46fa6850b4

--- nagios-qc/pi/configMaker.py
+++ nagios-qc/pi/configMaker.py
@@ -1,3 +1,4 @@
+from lxml import etree
 def ServiceMaker(Host,Service,conf):
    conf.write('define service{\n')
    conf.write('   use                     generic-radar-service\n')
@@ -5,8 +6,26 @@ def ServiceMaker(Host,Service,conf):
    conf.write('   service_description     '+Service+'\n')
    conf.write('}\n\n')
 
-commands=open('commands')
+def HostMaker(Host,conf):
+   conf.write('define host{\n')
+   conf.write('   use                     generic-radar-host\n')
+   conf.write('   host_name               '+Host+'\n')
+   conf.write('   alias                   '+Host+'\n')
+   conf.write('   address                 0.0.0.0\n')
+   conf.write('}\n\n')
+
 configFile=open('/etc/nagios/raf/config.cfg','w')
-lines=commands.readlines()
-for line in lines:
-   ServiceMaker(line.split(';')[0],line.split(';')[1],configFile)
+HostName='RAF'
+HostMaker(HostName,configFile)
+
+#Using commands output file
+#-------------------------
+#commands=open('commands')
+#lines=commands.readlines()
+#for line in lines:
+#   ServiceMaker(line.split(';')[0],line.split(';')[1],configFile)
+
+#Using checks xml
+Checks=etree.parse('checks.xml')
+for Elm in Checks.getiterator('check'):
+  ServiceMaker(HostName,Elm.attrib['variable'],configFile)
