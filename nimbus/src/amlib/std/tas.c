8039253a937714bf93966559e42f2083568640b8
Chris Webster <cjw@ucar.edu>
2015-01-22 20:51:01 +0000

    Merge in tasf.c function.  Now a single tas function for all TAS.  Recovery factor removed and we expect and Ambient Temperature.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7589 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/tas.c
+++ nimbus/src/amlib/std/tas.c
@@ -4,15 +4,26 @@
 #include "amlib.h"
 
 
+extern NR_TYPE compute_tas(NR_TYPE, NR_TYPE);
+
 /* -------------------------------------------------------------------- */
-NR_TYPE compute_tas(NR_TYPE t, NR_TYPE r, NR_TYPE m)
+void stas(DERTBL *varp)
 {
-//  double term = (t + Kelvin) * (401.86 / (1.0 / xm2 + 0.2 * r));	// Using RTX
+  NR_TYPE mach	= GetSample(varp, 0);
+  NR_TYPE at	= GetSample(varp, 1);
+
+  NR_TYPE tas	= compute_tas(at, mach);
+
+  PutSample(varp, tas);
+}
+
+/* -------------------------------------------------------------------- */
+NR_TYPE compute_tas(NR_TYPE t, NR_TYPE m)
+{
   double term = m * sqrt((double)7.0 / 5.0 * Rd * (t + Kelvin));	// Using ATX
 
   if (term < 0.0)
     term = -term;
 
-//  return(sqrt(term));
   return term;
 }
