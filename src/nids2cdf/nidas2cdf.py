043e0bd3b401888fa31a723dce5296f278007f8f
Chris Webster <cjw@ucar.edu>
2015-06-05 17:32:55 +0000

    Change long list of numbers to a range.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/instruments/mtp@7860 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/nids2cdf/nidas2cdf.py
+++ src/nids2cdf/nidas2cdf.py
@@ -260,8 +260,8 @@ for s in dt:
 # numpy.loadtxt(sys.argv[1], delimiter=',', usecols=(0, 2), unpack=True)
 
 # Load temperature profile (MTPP data).
-alt_in  = numpy.loadtxt(sys.argv[2], dtype='f4', delimiter=',', usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34), ndmin=2)
-temp_in = numpy.loadtxt(sys.argv[2], dtype='f4', delimiter=',', usecols=(35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67), ndmin=2)
+alt_in  = numpy.loadtxt(sys.argv[2], dtype='f4', delimiter=',', usecols=(tuple(range(2,35))), ndmin=2)
+temp_in = numpy.loadtxt(sys.argv[2], dtype='f4', delimiter=',', usecols=(tuple(range(35,68))), ndmin=2)
 
 
 
