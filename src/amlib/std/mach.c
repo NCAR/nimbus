ae792da54c0fde454d2120fcfa4c451e94017337
Chris Webster <cjw@ucar.edu>
2015-06-19 21:01:02 +0000

    Replace dry mach, at, and tas with humidity corrected...these calculations were using constants for dry air.  Now using code that can do humidity correction (though that correction is currently 0).  This should produce no change in output in mach, at, and tas at this point.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@7885 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/mach.c
+++ src/amlib/std/mach.c
@@ -21,10 +21,22 @@ COPYRIGHT:	University Corporation for Atmospheric Research, 2013
 #include "amlib.h"
       
 /* -------------------------------------------------------------------- */
+double mach(double p, double q)
+{
+  return sqrt((2.0 * Cv() / R()) * (pow((p + q) / p, R() / Cp()) - 1.0));
+}
+
+/* -------------------------------------------------------------------- */
+double mach_dry(double p, double q)
+{
+  return sqrt(5.0 * (pow((q/p)+1.0, Rd_DIV_Cpd) - 1.0));
+}
+
+/* -------------------------------------------------------------------- */
 void smach(DERTBL *varp)
 {
   NR_TYPE q = GetSample(varp, 0);
   NR_TYPE p = GetSample(varp, 1);
 
-  PutSample(varp, sqrt(5.0 * (pow((q/p)+1.0, Rd_DIV_Cpd) - 1.0)));
+  PutSample(varp, mach(p, q));
 }
