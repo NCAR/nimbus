15232c3029207230c6738bec293941525bc60e48
Chris Webster <cjw@ucar.edu>
2015-08-17 20:47:31 +0000

    Change variable name from edpc to ew.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@7927 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/thetap.c
+++ src/amlib/std/thetap.c
@@ -25,7 +25,7 @@ static const double L1 = 1754.0;	// Jkg-1K-1
 static const double K2 = 1.137e+6;	// Jkg-1
 
 double theta(double Tk, double psxc);
-double Tlcl(double Tk, double edpc);
+double Tlcl(double Tk, double ew);
 
 
 /* -------------------------------------------------------------------- */
@@ -33,19 +33,19 @@ void sthetap(DERTBL *varp)
 {
   double Tk	= GetSample(varp, 0) + Kelvin;	// ATX
   double psxc	= GetSample(varp, 1);
-  double edpc	= GetSample(varp, 2);
+  double ew	= GetSample(varp, 2);
   double r	= GetSample(varp, 3) / 1000.0;	// MR
 
   if (Tk <= 0.0)
     Tk = 0.0001;
-  if (edpc <= 0.0)
-    edpc = 0.0001;
+  if (ew <= 0.0)
+    ew = 0.0001;
 
   // Temperature at LCL.  Bolton 1980, Eq 21.
-  double Tl = Tlcl(Tk, edpc);
+  double Tl = Tlcl(Tk, ew);
 
   // Potential Temperature of dry air at the LCL.
-  double thetaDL = theta(Tk, psxc-edpc) * pow((Tk / Tl), 0.28e-3 * r);
+  double thetaDL = theta(Tk, psxc-ew) * pow((Tk / Tl), 0.28e-3 * r);
 
   // Davies-Jones 2009.
   double thetap = thetaDL * exp( ((L0-L1 * (Tl - Kelvin) + K2 * r) * r) / (Cpd * Tl) );
@@ -58,14 +58,14 @@ void sthetaq(DERTBL *varp)
 {
   double Tk	= GetSample(varp, 0) + Kelvin;
   double psxc	= GetSample(varp, 1);
-  double edpc	= GetSample(varp, 2);
+  double ew	= GetSample(varp, 2);
   double mr	= GetSample(varp, 3);
   double lwcc	= GetSample(varp, 4);	// PLWCC, equation fails with no LWC.
 
   double rt = 0.001 * (mr + lwcc * Rd * Tk / (psxc * 100));
   double cpt = Cpd + rt * Cw;
   double lv = 2.501e6 + Lv * (Tk - Kelvin);
-  double thetaq = Tk * pow((1000.0 / (psxc - edpc)), Rd / cpt) * exp(lv * 0.001 * mr / (cpt * Tk));
+  double thetaq = Tk * pow((1000.0 / (psxc - ew)), Rd / cpt) * exp(lv * 0.001 * mr / (cpt * Tk));
 
   PutSample(varp, (NR_TYPE)thetaq);
 }
