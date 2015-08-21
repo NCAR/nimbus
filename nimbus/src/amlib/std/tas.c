a13ea523df7295e380dfb8bfacf68cf11e3ab506
Chris Webster <cjw@ucar.edu>
2015-08-21 17:07:16 +0000

    New humidity corrected TAS.  New variables TASFLG and TASDRY.  Old TAS vairable are now humidity corrected.  View TASDRY for comparisons with old data. TASFLG is binary, 0 or 1 and describes when the humidity corrected TAS is in affect.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@7936 640d5228-2204-0410-8d71-8f46fa6850b4

--- nimbus/src/amlib/std/tas.c
+++ nimbus/src/amlib/std/tas.c
@@ -4,7 +4,16 @@
 #include "amlib.h"
 
 
-static const double Yd = Cpd / Cvd;
+static const double Yd = Cpd / Cvd;	// d for dry constants.
+
+double mach(double p, double q);
+double unheatedRecoveryFactor(double mach);
+double heatedRecoveryFactor(double mach);
+double WaterVaporPressure(double Tk);
+
+void setEOP(double);
+
+static NR_TYPE dry_tas;
 
 /* -------------------------------------------------------------------- */
 NR_TYPE compute_tas(NR_TYPE t, NR_TYPE m)
@@ -24,18 +33,44 @@ void stas_flag(DERTBL *varp)
   NR_TYPE q = GetSample(varp, 0);
   NR_TYPE p = GetSample(varp, 1);
   NR_TYPE rt = GetSample(varp, 2);
-  NR_TYPE ew = GetSample(varp, 3);
+  NR_TYPE ewx = GetSample(varp, 3);
 
-  void setEOP(double);
-  setEOP(0.0);
+  setEOP(0.0);	// Set for dry constants.
 
-/*
-  if (!isnan(ew) && !isnan(p))
+  NR_TYPE dry_mach, recovery, dry_at, ews, ewt, retval = 0.0;
+  dry_mach = mach(p, q);
+
+  if (strncmp(varp->depend[2], "RTH", 3) == 0)	// Unheated will have an 'F' as 3rd character.
+    recovery = heatedRecoveryFactor(dry_mach);
+  else
+    recovery = unheatedRecoveryFactor(dry_mach);
+
+  dry_at = AMBIENT(rt, recovery, dry_mach*dry_mach);
+  dry_tas = compute_tas(dry_at, dry_mach);	// stash for later retrieval in stas_dry()
+
+  ews = WaterVaporPressure(dry_at+Kelvin);
+//printf("at=%f : mach=%f : ewx=%f : ews = %f\n", dry_at, dry_mach, ewx, ews);
+  if (!isnan(ewx) && ewx < 1.05 * ews)
   {
-    setEOP(ew/p);
+    ewt = ewx;
+    retval = 0.0;
   }
-*/
-  PutSample(varp, 0.0);
+  else
+  {
+    ewt = ews * 0.5;
+    retval = 1.0;
+  }
+
+  if (!isnan(p) && p > 0.0)
+    setEOP(ewt/p);
+
+  PutSample(varp, retval);
+}
+
+/* -------------------------------------------------------------------- */
+void stas_dry(DERTBL *varp)
+{
+  PutSample(varp, dry_tas);
 }
 
 /* -------------------------------------------------------------------- */
