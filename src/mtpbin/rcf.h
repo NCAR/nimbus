95e41bfbe58928676b36ad1ad4f1c1d5e610a132
Tom Baltzer <tbaltzer@ucar.edu>
2015-10-04 15:51:42 +0000

    Add a little class to this deal.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/instruments/mtp@7979 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/mtpbin/rcf.h
+++ src/mtpbin/rcf.h
@@ -8,12 +8,16 @@
  ** written in Visual Basic 6.0. 
  ** Retrieval Coeeficient Files (RCFs)
 */
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <arpa/inet.h>
+#ifndef _RCF_h
+#define _RCF_h
 
+#include <vector>
+#include <string> 
 
+// Structure and Union to Read the header record.  Note: For some reason the
+//  Header structure itself is not matching up with this (despite the VB6 
+//  code indicating that it "should").  For this reason another Union to get
+//  second half of the RCF header record is defined below.
 typedef struct {
   short RCformat;
   char  CreationDateTime[8];
@@ -47,9 +51,14 @@ typedef struct {
   float SmatrixN1[15][3][10];   // Linear term
   float SmatrixN2[15][3][10];   // Quadratic term
 } RCF_HDR;
-
+  
 typedef union{RCF_HDR Rcf_Hdr ; char Array[sizeof(RCF_HDR)];} My_Rcf_Hdr_Un;
-
+  
+// Structure and Union to obtain the 2nd half of the header structure.  Note 
+//  That this also only obtains part of the rest of the header with the 
+//  floating point values going to 0.00 too early into the reading of the 
+//  Sentitivey matrix linear and Quadratic terms.  A puzzle for
+//  another day.
 typedef struct {
   char SURC[4];                 // SU IFB used to calculate RCs (added 20050128)
   float CHnLSBloss[3];          // CHn LSB RF loss
@@ -77,18 +86,32 @@ typedef struct {
 } RC_FL_Read;
 
 typedef union{RC_FL_Read RC_read ; char Array[sizeof(RC_FL_Read)];} My_RC_FL_Un;
- 
-// Structure to hold each flight level retrieval coefficient information.
-typedef struct {
-  float sBP; 			//Flight level pressure altitude (hPa)
-  float sOBrms[30];             //1-sigma apriori observable errors
-  float sOBav[30];              //Archive Average observables
-  float sBPrl[33];              //Pressure at retrieval levels
-  float sRTav[33];              //Average T at retrieval levels
-  float sRMSa[33];              //Variance in T at retrieval levels
-  float sRMSe[33];              //Formal error in T at retrieval levels
-  float Src[33][30]; 	        //33 reztrieval levels, 30 observables
-  float Spare[67];
-} RC_Set_1FL;
 
+class RetrievalCoefficientFile 
+{
 
+public:
+  RetrievalCoefficientFile(std::string);
+
+  std::string _RCFFileName;
+   
+  // Structure to hold each flight level retrieval coefficient information.
+  typedef struct {
+    float sBP; 			//Flight level pressure altitude (hPa)
+    float sOBrms[30];             //1-sigma apriori observable errors
+    float sOBav[30];              //Archive Average observables
+    float sBPrl[33];              //Pressure at retrieval levels
+    float sRTav[33];              //Average T at retrieval levels
+    float sRMSa[33];              //Variance in T at retrieval levels
+    float sRMSe[33];              //Formal error in T at retrieval levels
+    float Src[33][30]; 	        //33 reztrieval levels, 30 observables
+    float Spare[67];
+  } RC_Set_1FL;
+
+  My_Rcf_Hdr_Un thisRcfHdr;
+  My_RC_FL_Un thisRcFl;
+  std::vector<My_RC_FL_Un> flightLevelRCInfoVec;
+  
+};
+
+#endif
