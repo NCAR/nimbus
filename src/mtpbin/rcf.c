d409dbec180f94f48ddca61a68aea0130208c7cf
Tom Baltzer <tbaltzer@ucar.edu>
2015-10-16 14:54:34 +0000

    Primary update: - Weighted averaging needs to be done based on altitude in km rather than PALT in hPa
    Also:
    - Put together full header information from RCF - except altitude in km since that's the part
      we're still not sucessfully decoding.  
    - Improve class definitions
    - Clean up some of the debugging couts
    
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/instruments/mtp@7987 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/mtpbin/rcf.c
+++ src/mtpbin/rcf.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h> 
 #include <iostream>
 #include <fstream>
 #include <arpa/inet.h>
@@ -37,12 +39,12 @@ cout << "RCFId:"<<_RCFId<<'\n';
       file.read (RcFlUn.Array, sizeof(RC_FL_Read));
       FlUn2FlRcSet(RcFlUn, &FlRcSet);
       flightLevelRCInfoVec.push_back(RcFlUn);
-cout<<"IN LOOP:"<<FlRcSet.Palt;
-      FlRcSetVec.push_back(FlRcSet);
-      if (i+1 == RcfHdr.Rcf_Hdr.NFL) FlRcSetVec.push_back(FlRcSet);
+      _FlRcSetVec.push_back(FlRcSet);
+      if (i+1 == RcfHdr.Rcf_Hdr.NFL) _FlRcSetVec.push_back(FlRcSet);
     }
-cout<<"\nAfter pushbacks:"<<FlRcSetVec.end()->Palt<<"\n";
-    FlRcSetVec.pop_back();
+    //
+cout<<"\nAfter pushbacks:"<<_FlRcSetVec.end()->Palt<<"\n";
+    _FlRcSetVec.pop_back();
 
     if (file)
       cout<< "all: "<<size<<" bytes were read.\n";
@@ -51,48 +53,51 @@ cout<<"\nAfter pushbacks:"<<FlRcSetVec.end()->Palt<<"\n";
 
     file.close();
 
-    cout << "\n";
-    cout << "Format    :"<<RcfHdr.Rcf_Hdr.RCformat << '\n';
-    RcfHdr.Rcf_Hdr.RAOBfilename[79] = '\n';
-    cout << RcfHdr.Rcf_Hdr.RAOBfilename << '\n';
-    RcfHdr.Rcf_Hdr.RCfilename[79] = '\n';
-    cout << RcfHdr.Rcf_Hdr.RCfilename << '\n';
-    cout << "Raobcount :" << RcfHdr.Rcf_Hdr.RAOBcount << '\n';
-    cout << "LR1       :"<<RcfHdr.Rcf_Hdr.LR1<<'\n';
-    cout << "zLRb      :"<<RcfHdr.Rcf_Hdr.zLRb<<'\n';
-    cout << "LR2       :"<<RcfHdr.Rcf_Hdr.LR2<<'\n';
-    cout << "RecordStep:"<<RcfHdr.Rcf_Hdr.LR2<<'\n';
-    cout << "RAOBmin   :"<<RcfHdr.Rcf_Hdr.RAOBmin<<'\n';
-    cout << "ExcessTamplitude:"<<RcfHdr.Rcf_Hdr.ExcessTamplitude<<'\n';
-    cout << "Nobs:"<<RcfHdr.Rcf_Hdr.Nobs<<'\n';
-    cout << "Nret:"<<RcfHdr.Rcf_Hdr.Nret<<'\n';
-    cout << "dZ:\n";
-    for (int i=0; i<32; i++) {cout << RcfHdr.Rcf_Hdr.dZ[i] <<", ";}
-    cout << RcfHdr.Rcf_Hdr.dZ[32]<<'\n';
-    cout << "NFL:"<<RcfHdr.Rcf_Hdr.NFL<<'\n';
-    cout << "Zr:\n";
-    for (int i=0; i<19; i++) {cout << RcfHdr.Rcf_Hdr.Zr[i] <<", ";}
-    cout << RcfHdr.Rcf_Hdr.Zr[19]<<'\n';
-    cout << "\n";
-    cout << "Nlo:"<<RcfHdr.Rcf_Hdr.Nlo<<'\n';
-    cout << "SURC:"<<RcfHdr.Rcf_Hdr.SURC<<'\n';
+    // Fill first part of our header
+    // NOTE: for some damn reason the header read screws up right
+    //   after NFL ... still don't know why, but have workaround
+    _RCFHdr.RCformat = RcfHdr.Rcf_Hdr.RCformat;
+    strncpy(_RCFHdr.CreationDateTime,RcfHdr.Rcf_Hdr.CreationDateTime,8);
+    strncpy(_RCFHdr.RAOBfilename, RcfHdr.Rcf_Hdr.RAOBfilename, 80);
+    strncpy(_RCFHdr.RCfilename, RcfHdr.Rcf_Hdr.RCfilename, 80);
+    _RCFHdr.RAOBcount=RcfHdr.Rcf_Hdr.RAOBcount;
+    _RCFHdr.LR1=RcfHdr.Rcf_Hdr.LR1;
+    _RCFHdr.zLRb=RcfHdr.Rcf_Hdr.zLRb;
+    _RCFHdr.LR2=RcfHdr.Rcf_Hdr.LR2;
+    _RCFHdr.RecordStep=RcfHdr.Rcf_Hdr.RecordStep;
+    _RCFHdr.RAOBmin=RcfHdr.Rcf_Hdr.RAOBmin;
+    _RCFHdr.ExcessTamplitude=RcfHdr.Rcf_Hdr.ExcessTamplitude;
+    _RCFHdr.Nobs=RcfHdr.Rcf_Hdr.Nobs;
+    _RCFHdr.Nret=RcfHdr.Rcf_Hdr.Nret;
+    for (int i=0; i<NUM_RETR_LVLS; i++)
+      _RCFHdr.dZ[i]=RcfHdr.Rcf_Hdr.dZ[i];
+    _RCFHdr.NFL=RcfHdr.Rcf_Hdr.NFL;
+    
+    cout << "Raobcount :" << _RCFHdr.RAOBcount << '\n';
+    cout << "NFL:"<<_RCFHdr.NFL<<'\n';
 
-
+    // Resynch header decoding at SURC
     My_EH EH;
     int i = 0;
     for (int j=1376; j<sizeof(END_HDR); j++,i++) EH.Array[i]=RcfHdr.Array[j];
-    cout << "SURC:" << EH.EH.SURC << '\n';
+  
+    // Finish filling our header record with what we can get.
+    strncpy(_RCFHdr.SURC, EH.EH.SURC, 4);
+    cout << "SURC:" << _RCFHdr.SURC << '\n';
     cout << "CHnLSBloss:\n";
-    for (int i=0; i<3;i++) {cout<< EH.EH.CHnLSBloss[i]<<',';}
+    for (int i=0; i<3;i++) 
+    {
+      _RCFHdr.CHnLSBloss[i] = EH.EH.CHnLSBloss[i];
+      cout<< _RCFHdr.CHnLSBloss[i]<<',';
+    }
     cout << '\n';
-    int j = 0;
-
+    _RCFHdr.RAOBbias=EH.EH.RAOBbias;
+    _RCFHdr.CH1LSBloss=EH.EH.CH1LSBloss;
     //Convert from column major storage for SmatrixN1
-    float SmatrixN1[15][3][10];
     int x=0,y=0,z=0;
     for (int i=0; i<450;i++) {
-      SmatrixN1[x][y][z] = EH.EH.Gmatrix[i];
-      //cout << x << " " << y << " " << z << '\n';
+      _RCFHdr.SmatrixN1[x][y][z] = EH.EH.Gmatrix[i];
+     //cout << x << " " << y << " " << z << '\n';
       x++;
       if (x > 14) { 
         x=0;
@@ -103,19 +108,24 @@ cout<<"\nAfter pushbacks:"<<FlRcSetVec.end()->Palt<<"\n";
         }
       }
     }
-/*
+
+/* - maybe at some point these debug statements will help resolve 
+ *   The decoding problems of the RCF header.
     for (int i=0; i<10;i++) {
-      cout << "SMATRIXN1:"<<SmatrixN1[14][2][i]<<'\n';
-      cout << "    SMATrIXN2:"<<EH.EH.SmatrixN2[14][2][i]<<'\n';
+      cout << "SMATRIXN1:"<<_RCFHdr.SmatrixN1[14][2][i]<<'\n';
     }
-    cout << "SmatrixN1[5][1][5]:"<<SmatrixN1[5][1][5]<<'\n';
+    cout << "SmatrixN1[5][1][5]:"<<_RCFHdr.SmatrixN1[5][1][5]<<'\n';
     for (int i = 450; i<460;i++) {cout<<" GM["<<i<<"]:"<<EH.EH.Gmatrix[i];}
     cout << "\n";
+*/
+
     //Convert from column major storage for SmatrixN2
-    float SmatrixN2[15][3][10];
-    x=0,y=0,z=0,j=0;
+    // NOTE: about 1/2 way through this, we loose data and it all turns to
+    // zeros... why?  Beats the heck out of me!  Gratefully I found where
+    // the flight level data starts and resynch to it above.
+    x=0,y=0,z=0;
     for (int i=450; i<900;i++) {
-      SmatrixN2[x][y][z] = EH.EH.Gmatrix[i];
+      _RCFHdr.SmatrixN2[x][y][z] = EH.EH.Gmatrix[i];
       x++;
       if (x > 14) { 
         x=0;
@@ -126,40 +136,23 @@ cout<<"\nAfter pushbacks:"<<FlRcSetVec.end()->Palt<<"\n";
         }
       }
     }
+
+/* - maybe at some point these debug statements will help resolve 
+ *   The decoding problems of the RCF header.
     for (int i=0; i<10;i++) {
-      cout << "SMATRIXN2:"<<SmatrixN2[0][0][i]<<'\n';
-      cout << "    SMATrIXN2:"<<SmatrixN2[14][2][i]<<'\n';
+      cout << "SMATRIXN2:"<<_RCFHdr.SmatrixN2[0][0][i]<<'\n';
     }
-    cout << "SmatrixN2[5][1][5]:"<<SmatrixN2[5][1][5]<<'\n';
-*/
-    //char SURC[4]; 
-    //for (int j=1376; j<1380; j++,i++) SURC[i]=RcfHdr.Array[j];
-    //cout << "NewSURC:"<<SURC<<"\n";
-/*
-    cout << "IFoff:\n";
-    for (int i=0; i<3; i++) {
-      for (int j=0; j<16; j++) {
-        cout << RcfHdr.Rcf_Hdr.IFoff[i][j] <<', ';
-      }
-      cout << '\n';
-    }
-    cout << "Nif:"<<RcfHdr.Rcf_Hdr.Nif<<'\n';
-    for (int i=0; i<10; i++) { 
-      for (int j=0; j<3; j++) { 
-        cout << RcfHdr.Rcf_Hdr.SmatrixN2[1][j][i] << ', ';
-      } 
-      cout <<'\n';
-    }
+    cout << "SmatrixN2[5][1][5]:"<<_RCFHdr.SmatrixN2[5][1][5]<<'\n';
 */
 
     cout << "PALT at Flight Levels:" << '\n';
-    vector<My_RC_FL_Un>::const_iterator it;
-    for(it=flightLevelRCInfoVec.begin(); it!=flightLevelRCInfoVec.end(); it+=1)
-    {
-      cout << it->RC_read.sBP <<'\n';
-    } 
+//    vector<My_RC_FL_Un>::const_iterator it;
+//    for(it=flightLevelRCInfoVec.begin(); it!=flightLevelRCInfoVec.end(); it+=1)
+//    {
+//      cout << it->RC_read.sBP <<'\n';
+//    } 
     vector<RC_Set_1FL>::const_iterator rit;
-    for (rit=FlRcSetVec.begin(); rit!=FlRcSetVec.end(); rit+=1)
+    for (rit=_FlRcSetVec.begin(); rit!=_FlRcSetVec.end(); rit+=1)
     {
       cout << rit->Palt<<'\n';
     }
@@ -174,57 +167,61 @@ std::string RetrievalCoefficientFile::getId()
   return _RCFId;
 }
 
-RC_Set_1FL RetrievalCoefficientFile::getRCAvgWt(float PAlt)
+RC_Set_1FL RetrievalCoefficientFile::getRCAvgWt(float PAltKm)
 {
   RC_Set_1FL RcSetAvWt;
 
-  // First check to see if PAlt is outside the range of Flight Level PAlts
+  // First check to see if PAltKm is outside the range of Flight Level PAltKms
   //  - if so then the weighted average observalbe will be the average
-  //    observable associated with the flight level whose PAlt is closest 
+  //    observable associated with the flight level whose PAltKm is closest 
   //    Assumption is that the Flight Level Retrieval Coefficient Set vector
   //    is stored in increasing Palt (decreasing aircraft altitude).
-std::vector<int>::size_type sz = FlRcSetVec.size();
-cout<<"In get avg: PAlt:"<<PAlt<<"  1st level:"<<FlRcSetVec.begin()->Palt<<"  last level:"<<FlRcSetVec.end()->Palt<<"  -1:"<<FlRcSetVec[sz-2].Palt<<"\n";
-  if (PAlt <= FlRcSetVec.begin()->Palt) 
+std::vector<int>::size_type sz = _FlRcSetVec.size();
+cout<<"In get avg: PAltKm:"<<PAltKm<<"  1st level:"<<_FlRcSetVec.begin()->Palt<<"  last level:"<<_FlRcSetVec.end()->Palt<<"  -1:"<<_FlRcSetVec[sz-2].Palt<<"\n";
+  if (PAltKm >= _RCFHdr.Zr[0]) 
   {
     for (int i = 0; i< NUM_OBSVBLS; i++) 
     {
-      RcSetAvWt.OBAvg[i] = FlRcSetVec.begin()->OBAvg[i];
-      RcSetAvWt.OBRms[i] = FlRcSetVec.begin()->OBRms[i];
+      RcSetAvWt.OBAvg[i] = _FlRcSetVec.begin()->OBAvg[i];
+      RcSetAvWt.OBRms[i] = _FlRcSetVec.begin()->OBRms[i];
       for (int j = 0; j < NUM_RETR_LVLS; j++) 
       {
-        RcSetAvWt.RC[j][i] = FlRcSetVec.begin()->RC[j][i];
+        RcSetAvWt.RC[j][i] = _FlRcSetVec.begin()->RC[j][i];
       }
     }
     return RcSetAvWt;
   }
-  if (PAlt >= FlRcSetVec.end()->Palt)
+  if (PAltKm <= _RCFHdr.Zr[_RCFHdr.NFL-1])
   {
     for (int i = 0; i< NUM_OBSVBLS; i++) 
     {
-      RcSetAvWt.OBAvg[i] = FlRcSetVec.end()->OBAvg[i];
-      RcSetAvWt.OBRms[i] = FlRcSetVec.end()->OBRms[i];
+      RcSetAvWt.OBAvg[i] = _FlRcSetVec.end()->OBAvg[i];
+      RcSetAvWt.OBRms[i] = _FlRcSetVec.end()->OBRms[i];
       for (int j = 0; j < NUM_RETR_LVLS; j++) 
       {
-        RcSetAvWt.RC[j][i] = FlRcSetVec.end()->RC[j][i];
+        RcSetAvWt.RC[j][i] = _FlRcSetVec.end()->RC[j][i];
       }
     }
     return RcSetAvWt;
   }
 
-  // Find two Flight Level Sets that are above and below the PAlt provided.
+  // Find two Flight Level Sets that are above and below the PAltKm provided.
   // Calculate the weight for averaging and identify the RC sets.
   vector<RC_Set_1FL>::const_iterator it, Botit, Topit;
   float BotWt,TopWt;
-  for(it=FlRcSetVec.begin(); it!=FlRcSetVec.end(); it+=1)
+  int i = 0;
+  for(it=_FlRcSetVec.begin(); it!=_FlRcSetVec.end(); it+=1)
   {
-    if (PAlt >= it->Palt and PAlt <= (it+1)->Palt)
+    if (PAltKm <= _RCFHdr.Zr[i] and PAltKm >= _RCFHdr.Zr[i+1])
     {
-      BotWt = 1 - ((PAlt - (it+1)->Palt)/(it->Palt - (it+1)->Palt));
+      BotWt = 1-((PAltKm-_RCFHdr.Zr[i+1])/(_RCFHdr.Zr[i]-_RCFHdr.Zr[i+1]));
       Topit = it;
       Botit = it+1;
-cout<< "TopAlt:"<<it->Palt<<"  BotAlt:"<<(it+1)->Palt<<"  BotWt:"<<BotWt<<"\n";
+cout<< "TopAlt:"<<_RCFHdr.Zr[i]<<"  BotAlt:"<<_RCFHdr.Zr[i+1]<<"  BotWt:"<<BotWt<<"\n";
+    } else {
+cout<< "i:"<<i<<"  iAlt:"<<_RCFHdr.Zr[i]<<"  i++Alt:"<<_RCFHdr.Zr[i+1]<<"  ChkAlt:"<<BotWt<<"\n";
     }
+    i++;
   }
   TopWt = 1-BotWt;
 
@@ -232,8 +229,8 @@ cout<< "TopAlt:"<<it->Palt<<"  BotAlt:"<<(it+1)->Palt<<"  BotWt:"<<BotWt<<"\n";
   // Calculate the Weighted averages 
   for (int i = 0; i < NUM_OBSVBLS; i++)
   {
-    RcSetAvWt.OBAvg[i] = Botit->OBAvg[i]*BotWt + Topit->OBAvg[i]*(1-BotWt);
-    RcSetAvWt.OBRms[i] = Botit->OBRms[i]*BotWt + Topit->OBRms[i]*(1-BotWt);
+    RcSetAvWt.OBAvg[i] = Botit->OBAvg[i]*BotWt + Topit->OBAvg[i]*TopWt;
+    RcSetAvWt.OBRms[i] = Botit->OBRms[i]*BotWt + Topit->OBRms[i]*TopWt;
     for (int j = 0; j < NUM_RETR_LVLS; j++)
     {
       RcSetAvWt.RC[j][i] = Botit->RC[j][i]*BotWt + Topit->RC[j][i]*(1-BotWt);
@@ -253,17 +250,34 @@ cout <<"\n\n";
 
 }
 
+bool RetrievalCoefficientFile::setFlightLevelsKm(float FlightLevels[], int Len)
+{
+  if (Len != _RCFHdr.NFL) 
+  {
+    cout<<"ERROR:Number of flight levels input is not equal to number in RCF\n";
+    return false;
+  } 
+  for (int i = 0; i<Len; i++)
+  {
+    if ((i+1<Len) && (FlightLevels[i] <= FlightLevels[i+1]))
+    {
+      cout<<"ERROR: Flight Levels are not in decreasing altitude\n";
+      return false;
+    }
+    _RCFHdr.Zr[i] = FlightLevels[i];
+  }
+
+  return true;
+}
+
 void RetrievalCoefficientFile::FlUn2FlRcSet(My_RC_FL_Un RcUn, RC_Set_1FL *RcSet)
 {
   RcSet->Palt = RcUn.RC_read.sBP;
-cout<<"Palt:"<<RcSet->Palt<<"  RMS vec:"<<'\n';
   for (int i=0; i<NUM_OBSVBLS; i++) 
   {
     RcSet->OBRms[i] = RcUn.RC_read.sOBrms[i];
-cout << " ["<<i<<"]: "<<RcSet->OBRms[i];
     RcSet->OBAvg[i] = RcUn.RC_read.sOBav[i];
   }
-cout<<'\n';
   for (int i=0; i<NUM_RETR_LVLS; i++) 
   {
     RcSet->PAltRl[i] = RcUn.RC_read.sBPrl[i];
