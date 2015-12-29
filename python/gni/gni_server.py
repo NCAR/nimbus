cd4e1ad3ddabbd40d875af0ebb6fa572af32ee73
Gary Granger <granger@ucar.edu>
2015-12-29 22:28:49 +0000

    refactor into classes
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@8086 640d5228-2204-0410-8d71-8f46fa6850b4

--- python/gni/gni_server.py
+++ python/gni/gni_server.py
@@ -5,78 +5,211 @@
 #
 
 import os
+import re
 import select
 import serial
 import socket
 import sys
 import time
+import logging
 
-UDP_IP = "192.168.84.1"
-UDP_PORT = 41002
+logger = logging.getLogger(__name__)
 
+# When the GNI is prompted with a string of "0\r\n", it echoes back
+# the 0 then prints the status line followed by the menu, but it does
+# it twice:
 
-sigCnt = 0
-timedExposure = 0.0
-statusElapsed = 0.0
+_example_menu = """
+0
+GNI,,c,9,c3,1,1
 
-# Connection to User Interface
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", UDP_PORT))
+Set In Program Mode
 
-# Connection to GNI.
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
-ser.nonblocking()
+0 = Status
+1 = Home Drives
+2 = Advance Carousel
+3 = Advance Slide
+4 = Load Slide
+5 = Expose Slide
+6 = Retract Slide
+7 = Advance, Load, Expose & Retract
+8 = Retract Slide Pickup
+9 = Home Shuttle
+10 = Home Slide Actuator
 
+GNI,,c,9,c3,1,1
 
-def readMenu(ser):
-  while True:
-    line = ser.readline()
-    if line.startswith('10 '):	# Last menu item starts with "10", read until then.
-      break
+Set In Program Mode
 
-def rqst_status():
-  print "requesting status"
-  ser.write("0")
+0 = Status
+1 = Home Drives
+2 = Advance Carousel
+3 = Advance Slide
+4 = Load Slide
+5 = Expose Slide
+6 = Retract Slide
+7 = Advance, Load, Expose & Retract
+8 = Retract Slide Pickup
+9 = Home Shuttle
+10 = Home Slide Actuator
+"""
 
-def send_status():
-  global sigCnt
-  s = "GNI,," + str(sigCnt)
-  sock.sendto(s, (UDP_IP, UDP_PORT))
-  sigCnt = sigCnt + 1
+class GNISerial:
 
+  def __init__(self):
+    "Serial connection to GNI."
+    self.sport = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
+    self.sport.nonblocking()
+    # This is a buffer of characters read from the serial port.  It
+    # should be replaced with a real buffer at some point.
+    self.cdata = ""
+    self.status = GNIStatus()
+    # Keep a record of when the full menu has been received from the
+    # GNI, as a sanity check and to make it possible to keep the
+    # message exchange more orderly.
+    self.gotmenu = False
+    self.gotstatus = False
 
-while True:
-  start = time.time()
-  readable, writable, exceptional = select.select([ser,sock,],[],[ser,sock,], 1.0)
-#  readable, writable, exceptional = select.select([sys.stdin,sock,],[],[], 1.0)
-  elapsed = time.time() - start
+  def getSerial(self):
+    return self.sport
 
-  if timedExposure:
-    exposeTime = exposeTime - elapsed
-    if exposeTime <= 0:
-      ser.write("6") # retract slide
-      timedExposure = 0
+  def requestStatus(self):
+    logger.debug("requesting status")
+    self.sport.write("0\r\n")
+    self.gotmenu = False
+    self.gotstatus = False
 
-  statusElapsed = statusElapsed + elapsed
-  if statusElapsed > 2.0:
-    rqst_status()
-    statusElapsed = 0.0
+  def retractSlide(self):
+    self.sport.write("6") # retract slide
+    self.gotmenu = False
 
-  print "elapse = " + str(elapsed)
-#  send_status()
+  def readData(self):
+    "Read data from the serial port and parse it for status updates."
+    self.cdata = self.cdata + self.sport.read(128)
+    self.handleData()
 
-  for s in readable:
-    if s is sock:
-      data = s.recv(1024)
-      print 'Read ' + data
-      ser.write(data)
+  def handleData(self):
+    # Only handle lines whose newline has been received.
+    lines = self.cdata.splitlines()
+    remainder = ""
+    if lines and not self.cdata.endswith("\n"):
+      remainder = lines[-1]
+      lines = lines[:-1]
+    self.cdata = remainder
+    for l in lines:
+      self.handleLine(l)
+
+  def handleLine(self, text):
+    if text.startswith("GNI"):
+      self.status.parseStatusLine(text)
+      self.gotstatus = True
+    elif text.startswith("10 = Home Slide Actuator"):
+      self.gotmenu = True
+    elif re.match("^\d\d? = ", text):
+      # Menu entry
+      pass
+    elif not text:
+      # Empty line
+      pass
     else:
-      n = s.inWaiting()
-#      readMenu(ser)
-      data = s.read(n)
-#      data = sys.stdin.readline()
-      print 'Sending ' + str(n) + ', [' + data + ']'
-      sock.sendto(data, (UDP_IP, UDP_PORT))
+      logger.error("Unrecognized data line from GNI: %s" % (text))
 
-ser.close()
-sock.close()
+  def getStatus(self):
+    return self.status.message
+
+  def close(self):
+    self.sport.close()
+
+
+class GNIStatus:
+  "Keep track of the current state of the GNI."
+
+  def __init__(self):
+    self.error = False
+    self.fields = None
+    self.message = None
+
+  def parseStatusLine(self, line):
+    "Parse lines of the form: GNI,,c,9,c3,1,1"
+    line = line.strip()
+    fields = line.split(',')
+    if len(fields) != 7 or fields[0] != "GNI":
+      raise Exception("Could not parse status line: %s" % (line))
+    self.message = line
+    self.fields = fields
+
+
+
+class GNIServer:
+
+  def __init__(self):
+    self.UDP_IP = "192.168.84.1"
+    self.UDP_PORT = 41002
+    self.sigCnt = 0
+    self.timedExposure = 0.0
+    self.statusElapsed = 0.0
+    self.exposeTime = 0.0
+
+    # Connection to the GNI serial interface
+    self.gni = None
+
+    # Connection to User Interface
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.sock.bind(("0.0.0.0", self.UDP_PORT))
+
+  def sendClient(self, message):
+    logger.debug("sending: %s" % (message))
+    self.sock.sendto(message, (self.UDP_IP, self.UDP_PORT))
+
+  def setSerialGNI(self, gni):
+    self.gni = gni
+
+  def sendStatus(self):
+    s = "GNI,," + str(self.sigCnt)
+    self.sendClient(s)
+    self.sigCnt += 1
+
+  def loop(self):
+    while True:
+      start = time.time()
+      ser = self.gni.getSerial()
+      ports = [ser, self.sock]
+      readable, writable, exceptional = select.select(ports, [], ports, 1.0)
+      elapsed = time.time() - start
+
+      if self.timedExposure:
+        self.exposeTime -= elapsed
+        if self.exposeTime <= 0:
+          self.gni.retractSlide()
+          self.timedExposure = 0
+
+      self.statusElapsed += elapsed
+      if self.statusElapsed > 2.0:
+        self.gni.requestStatus()
+        self.statusElapsed = 0.0
+
+      logger.debug("elapse = " + str(elapsed))
+    #  send_status()
+
+      if self.sock in readable:
+        data = self.sock.recv(1024)
+        print 'Read ' + data
+        ser.write(data)
+      if ser in readable:
+        self.gni.readData()
+        if self.gni.gotstatus:
+          self.sendClient(self.gni.getStatus())
+          self.gni.gotstatus = False
+
+  def close(self):
+    self.sock.close()
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+server = GNIServer()
+gni = GNISerial()
+server.setSerialGNI(gni)
+server.loop()
+server.close()
+gni.close()
