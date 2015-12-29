59ac00b83580771c1506fafbda6e31e8aa89bda7
Gary Granger <granger@ucar.edu>
2015-12-29 21:24:44 +0000

    proof-of-concept serial communication with gni using pyserial
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@8085 640d5228-2204-0410-8d71-8f46fa6850b4

--- python/gni/test_pyserial.py
+++ python/gni/test_pyserial.py
@@ -1,25 +1,32 @@
 
-
 import sys
 import serial
 import select
+import time
 
 def main(args):
     print("opening %s..." % (args[0]))
     ser = serial.Serial(args[0], 9600, timeout=0)
     ser.nonblocking()
+    now = time.time()
+    tprompt = now + 5
     while True:
-        print("entering select()...")
+        #print("entering select()...")
         # fds = [ser.fileno()]
         fds = [ser]
         (rlist, wlist, xlist) = select.select(fds, [], fds, 5)
-        print("returned from select().")
+        #print("returned from select().")
+	now = time.time()
+	if now >= tprompt:
+	    ser.write("0\r\n")
+	    tprompt = now + 5
         if rlist:
             data = ser.read(100)
-            print("data:", data)
+            sys.stdout.write(data)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
 
     
     
+
