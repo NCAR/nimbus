3b6bdbc8809e2027d07a5a031046ad17c9293e06
Tom Baltzer <tbaltzer@ucar.edu>
2016-01-19 22:44:55 +0000

    Pull new cals from new lab system.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@8148 640d5228-2204-0410-8d71-8f46fa6850b4

--- caledit/MainWindow.cc
+++ caledit/MainWindow.cc
@@ -163,7 +163,7 @@ void MainWindow::setupDatabase()
 */
     // List of remote sites that fill a calibration database.
     QStringList siteList;
-    siteList << "thumper.eol.ucar.edu";
+    siteList << "kilojoules.eol.ucar.edu";
     siteList << "hyper.raf-guest.ucar.edu";
     siteList << "hercules.raf-ext.ucar.edu";
 
