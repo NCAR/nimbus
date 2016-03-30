f0914e81dd6cd0fa3566cd1f2b5c51a0323977b6
Janine Aquino <janine@ucar.edu>
2016-03-30 19:52:40 +0000

    Not quite right. Command line params come before the file they apply to. Want rates to apply to output file.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk@8311 640d5228-2204-0410-8d71-8f46fa6850b4

--- instruments/camera/createMovies/combineCameras.pl
+++ instruments/camera/createMovies/combineCameras.pl
@@ -543,12 +543,12 @@ my $mp4BitRate = $keywords->{mp4BitRate};;
 my $outputFilename = "$flightNumber.$outputFileTimes.mp4";
 # First ffmpeg pass.
 #if (system "ffmpeg -passlogfile ~/ffmpeg_$flightNumber -r $outputFrameRate -b $mp4BitRate -y -title $projectNumber$flightNumber -author 'S. Beaton NCAR/RAF' -pass 1 -i $annotatedImageDirectory/%05d.jpg ~/$flightNumber.mp4") {die "Unable to create MPEG file $flightNumber.mp4, pass 1"};
-my $command = "/usr/bin/ffmpeg -r:$outputFrameRate -b:$mp4BitRate -y -i $annotatedImageDirectory/%05d.jpg -passlogfile ./ffmpeg_$flightNumber -pass 1".$keywords->{movieDirectory}."/$outputFilename";
+my $command = "/usr/bin/ffmpeg -i $annotatedImageDirectory/%05d.jpg -r $outputFrameRate -b:v $mp4BitRate -y -passlogfile ./ffmpeg_$flightNumber -pass 1 ".$keywords->{movieDirectory}."/$outputFilename";
 print "$command\n";
 if (system "$command") { die "Unable to create MPEG file $outputFilename, pass 1 using command $command"}; 
 # Second pass.
 #if (system "ffmpeg -passlogfile ~/ffmpeg_$flightNumber -r $outputFrameRate -b $mp4BitRate -y -title $projectNumber$flightNumber -author 'S. Beaton NCAR/RAF' -pass 2 -i $annotatedImageDirectory/%05d.jpg ~/$flightNumber.mp4") {die "Unable to create MPEG file $flightNumber.mp4, pass 2"};
-$command = "/usr/bin/ffmpeg -r:$outputFrameRate -b:$mp4BitRate -y -i $annotatedImageDirectory/%05d.jpg -passlogfile ./ffmpeg_$flightNumber -pass 2".$keywords->{movieDirectory}."/$outputFilename";
+$command = "/usr/bin/ffmpeg  -i $annotatedImageDirectory/%05d.jpg -r $outputFrameRate -b:v $mp4BitRate -y -passlogfile ./ffmpeg_$flightNumber -pass 2 ".$keywords->{movieDirectory}."/$outputFilename";
 print "$command\n";
 if (system "$command") {die "Unable to create MPEG file $outputFilename, pass 2 using command $command"};
 
