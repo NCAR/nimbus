57b9422b38b9b29b123269dbddf562ec5535aa73
Chris Webster <cjw@ucar.edu>
2016-10-11 18:02:29 +0000

    Actually put the word sizeof in the code.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@8555 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/asas.c
+++ src/amlib/std/asas.c
@@ -407,7 +407,7 @@ static void uhsasBinConsolidation(NR_TYPE *actual)
   while (dst < 100)
     uhsas_raw[dst++] = 0.0;
 
-  memcpy(actual, uhsas_raw, 100 * (NR_TYPE));
+  memcpy(actual, uhsas_raw, 100 * sizeof(NR_TYPE));
 }
 
 /* END ASAS.C */
