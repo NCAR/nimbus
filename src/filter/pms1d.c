640a1778e0d988a7caacde4da1d446757c4c73b3
Chris Webster <cjw@ucar.edu>
2017-01-23 20:31:34 +0000

    Set probe serial numbers for ARISTO2017.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@8641 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/filter/pms1d.c
+++ src/filter/pms1d.c
@@ -321,11 +321,19 @@ void PMS1D_SetupForADS3()
     else
       setSerialNumberAndProbeType("A1DC", "F2DC001", PROBE_PMS2D | PROBE_2DC);
   }
+  else
   if (cfg.ProjectName().compare("ARISTO2016") == 0)
   {
     setSerialNumberAndProbeType("AUHSAS_LWI", "UHSAS001", PROBE_PMS1D | PROBE_PCASP);
     setSerialNumberAndProbeType("AUHSAS_CAB", "UHSAS011", PROBE_PMS1D | PROBE_PCASP);	// Univ Wyoming
     setSerialNumberAndProbeType("ACDP", "CDP058", PROBE_PMS1D | PROBE_CDP);
+    setSerialNumberAndProbeType("A1DC", "F2DC002", PROBE_PMS2D | PROBE_2DC);
+  }
+  else
+  if (cfg.ProjectName().compare("ARISTO2017") == 0)
+  {
+    setSerialNumberAndProbeType("ACDP_RWIO", "CDP016", PROBE_PMS1D | PROBE_CDP);
+    setSerialNumberAndProbeType("ACDP_RWII", "CDP058", PROBE_PMS1D | PROBE_CDP);
     setSerialNumberAndProbeType("A1DC", "F2DC002", PROBE_PMS2D | PROBE_2DC);
   }
 }
