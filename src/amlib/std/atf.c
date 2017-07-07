5edcb1178a7f7399be7045cc6b4c926639abed44
Chris Webster <cjw@ucar.edu>
2017-07-07 17:48:45 +0000

    Add ability to put recovery factor constant C0 in Defaults file as HEATED_RF or UNHEATED_RF.
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@8806 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/std/atf.c
+++ src/amlib/std/atf.c
@@ -6,17 +6,26 @@ FULL NAME:      Ambient Temperature Fast/Unheated
 
 DESCRIPTION:    Ambient Temperature for Rosemount 102 Unheated:
 
-COPYRIGHT:      University Corporation for Atmospheric Research, 1992-2015
+COPYRIGHT:      University Corporation for Atmospheric Research, 1992-2017
 -------------------------------------------------------------------------
 */
 
 #include "nimbus.h"
 #include "amlib.h"
 
+static NR_TYPE RF_C0 = 0.9959;
+
 /* -------------------------------------------------------------------- */
 void atfInit(var_base *varp)
 {
-   AddToAttributes(varp->name, "RecoveryFactor", "0.9959 + 0.0283 log10(mach) + 0.0374 (log10(mach))^2 + 0.0762 (log10(mach))^3");
+  char buffer[1024];
+  float *tmp;
+
+  if ( (tmp = GetDefaultsValue("UNHEATED_RF", varp->name)) )
+    RF_C0 = (int)tmp[0];
+
+  sprintf(buffer, "%.4f + 0.0283 log10(mach) + 0.0374 (log10(mach))^2 + 0.0762 (log10(mach))^3", RF_C0);
+  AddToAttributes(varp->name, "RecoveryFactor", buffer);
 }
 
 /* -------------------------------------------------------------------- */
@@ -25,7 +34,7 @@ void atfInit(var_base *varp)
 double unheatedRecoveryFactor(double mach)
 {
   double logMach = log10(mach);
-  return 0.9959 + 0.0283 * logMach + 0.0374 * logMach * logMach + 0.0762 * logMach * logMach * logMach;
+  return RF_C0 + 0.0283 * logMach + 0.0374 * logMach * logMach + 0.0762 * logMach * logMach * logMach;
 }
 
 /* -------------------------------------------------------------------- */
