572ba83d5dbe023b5a99b1ed212e57282a761f5d
Chris Webster <cjw@ucar.edu>
2018-04-18 16:31:36 +0000

    Implement geopotential altitude function.
    Variable is GEOPHT.o
    
    Processing memorandum from Al Cooper Dec-7 2017.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/raf/trunk/nimbus@9103 640d5228-2204-0410-8d71-8f46fa6850b4

--- src/amlib/user/geopot.c
+++ src/amlib/user/geopot.c
@@ -4,23 +4,44 @@ OBJECT NAME:	geopt.c
 
 FULL NAME:	Geopotential Altitude
 
-ENTRY POINTS:	sGeopot()
-
-STATIC FNS:	
-
 DESCRIPTION:	Convert Mean Sea Level (MSL) Altitude to Geopotential
 		Altitude.
 
-COPYRIGHT:	University Corporation for Atmospheric Research, 1996
+COPYRIGHT:	University Corporation for Atmospheric Research, 1996-2018
 -------------------------------------------------------------------------
 */
 
 #include "nimbus.h"
 #include "amlib.h"
 
+static double g0 = 9.80665;
+static double g1 = 0.001931851;
+static double g2 = 0.006694380;
+static double k1 = 3.1570428706e-07;
+static double k2 = 2.1026896504e-09;
+static double k3 = 7.3745167729e-14;
+
 
 /* -------------------------------------------------------------------- */
-void sGeopot(DERTBL *varp)
+void sGeopot2(DERTBL *varp)	// Al Cooper equation, 2018.
+{
+  NR_TYPE       geop_alt;
+
+  double msl_alt = GetSample(varp, 0);		// ggalt
+  double lat = GetSample(varp, 1) * DEG_RAD;	// any latititude
+  double geoidht = GetSample(varp, 2);		// ellipsoid - geoid (m)
+
+  double sin_lat_2 = sin(lat) * sin(lat);
+  geop_alt =    GRAVITY * ((1.0 + g1 * sin_lat_2) / pow(1.0 - g2 * sin_lat_2, 0.5) ) *
+                ( msl_alt - ((pow(msl_alt + geoidht, 2.0) - pow(geoidht, 2.0)) / 2.0) *
+                (k1 - k2 * sin_lat_2) + ((pow(msl_alt + geoidht, 3.0) - pow(geoidht, 3.0)) / 3.0 * k3) );
+
+  PutSample(varp, geop_alt / g0);
+}
+
+
+/* -------------------------------------------------------------------- */
+void sGeopot(DERTBL *varp)	// This was implemented for NOAA/AOC in 1996.
 {
   NR_TYPE	msl_alt, geop_alt, lat;
 
