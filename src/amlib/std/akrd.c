78ec2d85c62604bc12b2c3458160d06df7500901
catdewey <cdewerd@ucar.edu>
2019-04-19 17:38:21 +0000

    Added OTREC_TEST and OTREC so akrd values will load for processing

--- src/amlib/std/akrd.c
+++ src/amlib/std/akrd.c
@@ -130,6 +130,8 @@ void initAKRD(var_base *varp)
   if (cfg.ProjectName().compare("CSET") == 0 ||
       cfg.ProjectName().compare("ORCAS") == 0 ||
       cfg.ProjectName().compare("SOCRATES") == 0)
+      cfg.ProjectName().compare("OTREC_TEST") == 0)
+      cfg.ProjectName().compare("OTREC") == 0)
   {
     low = load_AKRD_Default(varp, "AKRD_COEFF_LOW");
     mid = load_AKRD_Default(varp, "AKRD_COEFF_MID");
