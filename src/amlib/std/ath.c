c3569ee7bfec52a903815ac9c5297ad0e0e67e1f
Chris Webster <cjw@ucar.edu>
2021-03-18 06:41:44 -0600

    Fix two heatedRecoveryFactor() issues.
    Issue 1: Post OTREC Cory asked me to add support to have independant C0 for each element.  I added for ATH1 & ATH2, but failed to add ATH3 & ATH4.  Not noticed until I went back to reprocess CONTRAST.  No previous data is compromised.
    
    Issue 2: TASFLG generates its own temperature.  I used a test for heated vs unheated that always returned unheated.  In looking at HIPPO-4 it appears TASDRY & TASX were under-reported by 0.04-0.05 m/s.

--- src/amlib/std/ath.c
+++ src/amlib/std/ath.c
@@ -13,25 +13,26 @@ COPYRIGHT:      University Corporation for Atmospheric Research, 1992-2015
 #include "nimbus.h"
 #include "amlib.h"
 
-static NR_TYPE RF_C0[2] = { 0.986, 0.969 };
-static int sensor = 0;
+static NR_TYPE RF_C0[] = { 0.986, 0.969, 0.988, 0.988 };
 
 /* -------------------------------------------------------------------- */
 void athInit(var_base *varp)
 {
   char buffer[1024];
   float *tmp;
+  int sensor = varp->name[3] - '1';
 
 //  if ( (tmp = GetDefaultsValue("HEATED_RF", varp->name)) )
 //    RF_C0 = tmp[0];
 
-  sprintf(buffer, "%.4f + 0.053 log10(mach) + 0.090 (log10(mach))^2 + 0.091 (log10(mach))^3", RF_C0);
+  sprintf(buffer, "%.4f + 0.053 log10(mach) + 0.090 (log10(mach))^2 + 0.091 (log10(mach))^3",
+	RF_C0[sensor]);
   AddToAttributes(varp->name, "RecoveryFactor", buffer);
 
 }	/* END CONSTRUCTOR */
 
 /* -------------------------------------------------------------------- */
-double heatedRecoveryFactor(double mach)
+double heatedRecoveryFactor(double mach, int sensor)
 {
   double logMach = log10(mach);
   return RF_C0[sensor] + 0.053 * logMach + 0.090 * logMach * logMach + 0.091 * logMach * logMach * logMach;
@@ -40,10 +41,10 @@ double heatedRecoveryFactor(double mach)
 /* -------------------------------------------------------------------- */
 void sath(DERTBL *varp)
 {
-sensor = varp->name[3] - '1';
+  int sensor = varp->name[3] - '1';
   NR_TYPE rt = GetSample(varp, 0);
   NR_TYPE mach = GetSample(varp, 1);
-  NR_TYPE recovery = heatedRecoveryFactor(mach);
+  NR_TYPE recovery = heatedRecoveryFactor(mach, sensor);
 
   if (rt < -Kelvin)
     rt  = -Kelvin;
--- src/amlib/std/tas.c
+++ src/amlib/std/tas.c
@@ -6,14 +6,42 @@
 
 static const double Yd = Cpd / Cvd;	// d for dry constants.
 
+static int rt_sensor = -1;	// Default to unheated.
+
 double mach(double p, double q);
 double unheatedRecoveryFactor(double mach);
-double heatedRecoveryFactor(double mach);
+double heatedRecoveryFactor(double mach, int sensor);
 double WaterVaporPressure(double Tk);
 
 void setEOP(double);
 
 static NR_TYPE dry_tas;
+
+/* -------------------------------------------------------------------- */
+void tasFlgInit(var_base *varp)
+{
+  size_t i;
+  DERTBL *dp = (DERTBL *)varp;
+  DERTBL *rt = 0;
+
+  // Locate Recovry Temperature in depends list.
+  for (i = 0; i < dp->depends.size(); ++i)
+    if (strncmp(dp->depends[i]->name, "RT", 2) == 0)
+      rt = (DERTBL *)dp->depends[i];
+
+  if (rt == 0)
+  {
+    fprintf(stderr, "tas.c::tasFlgInit(), no RT dependency, fatal.\n");
+    exit(1);
+  }
+
+
+  // Now determine if heated or unheated.
+  if (strncmp(rt->depend[0], "RTH", 3) == 0)	// Unheated will have an 'F' as 3rd character.
+    rt_sensor = rt->depend[0][3] - '1';		// Get sensor number for (see recovery factor for why).
+
+  fprintf(stderr, "tas.c::tasFlgInit() found RT = [%s]\n", rt->depend[0]);
+}
 
 /* -------------------------------------------------------------------- */
 NR_TYPE compute_tas(NR_TYPE t, NR_TYPE m)
@@ -32,7 +60,7 @@ void stas_flag(DERTBL *varp)
 {
   NR_TYPE q = GetSample(varp, 0);
   NR_TYPE p = GetSample(varp, 1);
-  NR_TYPE rt = GetSample(varp, 2);
+  NR_TYPE rt = GetSample(varp, 2); // If this changes from index 2, then modify tasFlgInit() above.
   NR_TYPE ewx = GetSample(varp, 3);
 
   setEOP(0.0);	// Set for dry constants.
@@ -40,10 +68,10 @@ void stas_flag(DERTBL *varp)
   NR_TYPE dry_mach, recovery, dry_at, ews, ewt, retval = 0.0;
   dry_mach = mach(p, q);
 
-  if (strncmp(varp->depend[2], "RTH", 3) == 0)	// Unheated will have an 'F' as 3rd character.
-    recovery = heatedRecoveryFactor(dry_mach);
-  else
+  if (rt_sensor < 0)
     recovery = unheatedRecoveryFactor(dry_mach);
+  else
+    recovery = heatedRecoveryFactor(dry_mach, rt_sensor);
 
   dry_at = AMBIENT(rt, recovery, dry_mach*dry_mach);
   dry_tas = compute_tas(dry_at, dry_mach);	// stash for later retrieval in stas_dry()
