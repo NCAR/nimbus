8425bd59afbbddaa6f902a7b455926037e827a73
Chris Webster <cjw@ucar.edu>
2022-06-01 11:48:26 -0600

    Implement Josh's recovery factors.
    In addition, add a differing C1 between the probes.

--- src/amlib/std/ath.c
+++ src/amlib/std/ath.c
@@ -13,12 +13,16 @@ COPYRIGHT:      University Corporation for Atmospheric Research, 1992-2015
 #include "nimbus.h"
 #include "amlib.h"
 
+
 /* See Al Cooper memo titled "Study of the Recovery Factor" Sep 23, 2015.
+ * Josh updated June 1, 2022 with new C0 and a different C1 for each probe.
  */
-static NR_TYPE rosemount_C0[] = { 0.958, 0.957, 0.958, 0.957 };
+static NR_TYPE rosemount_C0[] = { 0.988, 0.988, 0.988, 0.988 };
+static NR_TYPE rosemount_C1[] = { 0.053, 0.053, 0.053, 0.053 };
 
 // Default to HARCO        RTH1   RTH2   RTH3   RTH4
-static NR_TYPE RF_C0[] = { 0.986, 0.969, 0.986, 0.969 };
+static NR_TYPE RF_C0[] = { 0.979, 0.979, 0.979, 0.979 };
+static NR_TYPE RF_C1[] = { 0.041, 0.041, 0.041, 0.041 };
 
 /* -------------------------------------------------------------------- */
 void athInit(var_base *varp)
@@ -27,14 +31,18 @@ void athInit(var_base *varp)
   float *tmp;
   int sensor = varp->name[3] - '1';
 
-  // Copy in Rosemount C0, if appropriate.
+  // We will use the HARCO variables regardless, so copy in Rosemount
+  // C0/C1, if appropriate.
   std::string s = ((DERTBL *)varp)->depends[0]->LongName;
   std::transform(s.begin(), s.end(), s.begin(), ::tolower);
   if ( s.find("rosemount") != std::string::npos )
+  {
     RF_C0[sensor] = rosemount_C0[sensor];
+    RF_C1[sensor] = rosemount_C1[sensor];
+  }
 
-  sprintf(buffer, "%.4f + 0.053 log10(mach) + 0.090 (log10(mach))^2 + 0.091 (log10(mach))^3",
-	RF_C0[sensor]);
+  sprintf(buffer, "%.3f + %.3f log10(mach) + 0.090 (log10(mach))^2 + 0.091 (log10(mach))^3",
+	RF_C0[sensor], RF_C1[sensor]);
   AddToAttributes(varp->name, "RecoveryFactor", buffer);
 
 }	/* END CONSTRUCTOR */
@@ -43,7 +51,7 @@ void athInit(var_base *varp)
 double heatedRecoveryFactor(double mach, int sensor)
 {
   double logMach = log10(mach);
-  return RF_C0[sensor] + 0.053 * logMach + 0.090 * logMach * logMach + 0.091 * logMach * logMach * logMach;
+  return RF_C0[sensor] + RF_C1[sensor] * logMach + 0.090 * logMach * logMach + 0.091 * logMach * logMach * logMach;
 }
 
 /* -------------------------------------------------------------------- */
