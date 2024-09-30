c818ae0e06d23708906d73f217a4597e120d746a
Chris Webster <cjw@ucar.edu>
2024-09-30 15:48:55 -0600

    For fixed ATTACK values during periods of ADIFR nan,
    The C130 should have a fixed value of 2.0, not the value of 3.0 for the
    GV.

--- src/amlib/std/reference.c
+++ src/amlib/std/reference.c
@@ -39,17 +39,22 @@ void sReferAttack(DERTBL *varp)
   static NR_TYPE attack[nFeedBackTypes] = { 0.0, 0.0 };
   static int attack_miss_cnt[nFeedBackTypes] = { 0, 0 };
   static const int ATTACK_MISS_MAX[nFeedBackTypes] = { 2, 2 * Config::HighRate };
+
   // Fallback value for Attack if it goes missing.
-  static const NR_TYPE ATTACK_DEFAULT_VALUE = 3.0;
+  static const NR_TYPE GV_ATTACK_DEFAULT_VALUE = 3.0;
+  static const NR_TYPE C130_ATTACK_DEFAULT_VALUE = 2.0;
 
 
   NR_TYPE aoa = GetSample(varp, 0);
 
   if (std::isnan(aoa))
   {
+    NR_TYPE substitute_attack = cfg.Aircraft() == Config::HIAPER ?
+			GV_ATTACK_DEFAULT_VALUE : C130_ATTACK_DEFAULT_VALUE;
+
     // Repeat prev attack up to two samples if missing, otherwise set to 3.0.
     if (++attack_miss_cnt[FeedBack] > ATTACK_MISS_MAX[FeedBack])
-      attack[FeedBack] = ATTACK_DEFAULT_VALUE;
+      attack[FeedBack] = substitute_attack;
   }
   else
   {
