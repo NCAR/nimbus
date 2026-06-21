9e01f598b2fa013a1757421ce74e1714be6c213d
Chris Webster <cjw@ucar.edu>
2026-06-21 23:02:51 +0000

    Update HVPS # bins.
    The original HVPS-1 was 256 bins.  The new HVPS-3 is 128 bins.  I doubt
    we ever processed an HVPS-1, so I am updating the nBins to 128.

--- src/nimbus/pms1d.c
+++ src/nimbus/pms1d.c
@@ -460,11 +460,8 @@ static int getCellSizes(const var_base * rp, float cellSize[])
   if (rp->ProbeType & PROBE_260X || thisIs2Dnot1D(rp->name) || strstr(rp->name, "PIP"))
     nBins = 64;
   else
-  if (rp->ProbeType & PROBE_2DS)
+  if (rp->ProbeType & PROBE_2DS || rp-ProbeType & PROBE_HVPS)
     nBins = 128;
-  else
-  if (rp->ProbeType & PROBE_HVPS)
-    nBins = 256;
   else
   if (rp->ProbeType & PROBE_PMS2D)
   {
